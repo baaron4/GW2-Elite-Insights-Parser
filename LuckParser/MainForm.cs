89571c3c8c1f3b30188d9fae9b7ef888468f84d7
Jekfer Bichon <jekfer.bichon@3ds.com>
2018-06-13 11:28:38 +0200

    real merge fix

--- LuckParser/MainForm.cs
+++ LuckParser/MainForm.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using LuckParser.Controllers;
 
@@ -172,7 +176,7 @@ namespace LuckParser
                     logger(i, "Working...", 20);
                     Controller1 control = new Controller1();
 
-                    if (fInfo.Extension.Equals(".evtc", StringComparison.OrdinalIgnoreCase) || 
+                    if (fInfo.Extension.Equals(".evtc", StringComparison.OrdinalIgnoreCase) ||
                         fInfo.Name.EndsWith(".evtc.zip", StringComparison.OrdinalIgnoreCase))
                     {
                         //Process evtc here
@@ -200,19 +204,16 @@ namespace LuckParser
                         {
                             result = "kill";
                         }
+
                         string outputType = Properties.Settings.Default.SaveOutHTML ? "html" : "csv";
                         string outputFile = Path.Combine(
-                            saveDirectory.FullName, 
-                            $"{fInfo.Name}_{control.GetLink(bossid + "-ext")}_{result}.{outputType}"
+                            saveDirectory.FullName,
+                            $"{fInfo.Name}_{HTMLHelper.GetLink(bossid + "-ext")}_{result}.{outputType}"
                         );
 
                         logger(i, "Creating File...", 60);
                         using (FileStream fs = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
                         {
-                            logger(i, "Creating CSV File...", 60);
-                            FileStream fcreate = File.Open(location + fileName + "_" + HTMLHelper.GetLink(bossid + "-ext") + "_" + result + ".csv", FileMode.Create);
-
-                            //return html string
                             logger(i, $"Writing {outputType.ToUpper()}...", 80);
                             using (StreamWriter sw = new StreamWriter(fs))
                             {
@@ -246,7 +247,7 @@ namespace LuckParser
                 System.Threading.Thread.CurrentThread.CurrentUICulture = before;
             }
         }
-        
+
         /// Worker job
         void m_oWorker_DoWork(object sender, DoWorkEventArgs e)
         {
@@ -296,7 +297,7 @@ namespace LuckParser
                 lvFileList.Items.Add(lvItem);
             }
         }
-       
+
         private void LvFileList_DragDrop(object sender, DragEventArgs e)
         {
             btnParse.Enabled = true;
---
+++ LuckParser/Models/PhaseData.cs
@@ -0,0 +1,34 @@
+﻿namespace LuckParser.Models.ParseModels
+{
+    public class PhaseData
+    {
+        public long start;
+        public long end;
+
+        public PhaseData(long start, long end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public long getDuration(string format)
+        {
+            switch (format)
+            {
+                case "m":
+                    return (end - start) / 60000;
+                case "s":
+                    return (end - start) / 1000;
+                case "ms":
+                default:
+                    return (end - start) / 1000;
+            }
+
+        }
+
+        public bool inInterval(long time, long offset = 0)
+        {
+            return start <= time - offset && time - offset <= end;
+        }
+    }
+}
