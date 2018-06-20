43b3ddcd351d7fdce28dd84273fb31f497731483
Jekfer Bichon <jekfer.bichon@3ds.com>
2018-06-20 15:50:16 +0200

    better agent cleaning

--- LuckParser/Models/ParseModels/Agents/AgentData.cs
+++ LuckParser/Models/ParseModels/Agents/AgentData.cs
@@ -77,9 +77,9 @@ namespace LuckParser.Models.ParseModels
 
         public void clean()
         {
-            gadget_agent_list = gadget_agent_list.Where(x => x.getInstid() != 0 && x.getLastAware() - x.getFirstAware() > 0).ToList();
-            NPC_agent_list = NPC_agent_list.Where(x => x.getInstid() != 0 && x.getLastAware() - x.getFirstAware() > 0).ToList();
-            all_agents_list = all_agents_list.Where(x => x.getInstid() != 0 && x.getLastAware() - x.getFirstAware() > 0).ToList();
+            gadget_agent_list = gadget_agent_list.Where(x => x.getInstid() != 0 && x.getLastAware() - x.getFirstAware() > 0 && x.getFirstAware() != 0 && x.getLastAware() != long.MaxValue).ToList();
+            NPC_agent_list = NPC_agent_list.Where(x => x.getInstid() != 0 && x.getLastAware() - x.getFirstAware() > 0 && x.getFirstAware() != 0 && x.getLastAware() != long.MaxValue).ToList();
+            all_agents_list = all_agents_list.Where(x => x.getInstid() != 0 && x.getLastAware() - x.getFirstAware() > 0 && x.getFirstAware() != 0 && x.getLastAware() != long.MaxValue).ToList();
         }
     }
 }
