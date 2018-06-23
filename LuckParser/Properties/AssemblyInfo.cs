92dbf4e3c6a9c0532eebb09e7d84a7067c269bd8
baaron4 <baaron4thewin@gmail.com>
2018-06-22 20:57:49 -0500

    Fixed some old assembly references

--- LuckParser/Properties/AssemblyInfo.cs
+++ LuckParser/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LuckParser")]
+[assembly: AssemblyTitle("EliteInsights")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LuckParser")]
+[assembly: AssemblyProduct("EliteInsights")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
