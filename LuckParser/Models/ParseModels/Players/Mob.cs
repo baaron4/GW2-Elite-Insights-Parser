57afaf04c9276eb0c295d5f9adc315308e85b6ed
Jekfer Bichon <jekfer.bichon@3ds.com>
2018-07-25 18:32:06 +0200

    started working on thrash mob icons

--- LuckParser/Models/ParseModels/Players/Mob.cs
+++ LuckParser/Models/ParseModels/Players/Mob.cs
@@ -36,7 +36,76 @@ namespace LuckParser.Models.ParseModels.Players
 
         protected override void setCombatReplayIcon(ParsedLog log)
         {
-            // nothing to do here, this will be managed by the boss-parent
+            // TODO
+            switch (agent.getID())
+            {
+                case (ushort)Boss.ThrashIDS.Seekers:
+                    break;
+                case (ushort)Boss.ThrashIDS.RedGuardian:
+                    break;
+                case (ushort)Boss.ThrashIDS.BlueGuardian:
+                    break;
+                case (ushort)Boss.ThrashIDS.GreenGuardian:
+                    break;
+                case (ushort)Boss.ThrashIDS.ChargedSoul:
+                    break;
+                case (ushort)Boss.ThrashIDS.Kernan:
+                    break;
+                case (ushort)Boss.ThrashIDS.Knuckles:
+                    break;
+                case (ushort)Boss.ThrashIDS.Karde:
+                    break;
+                case (ushort)Boss.ThrashIDS.Spirit:
+                    break;
+                case (ushort)Boss.ThrashIDS.BloodStone:
+                    break;
+                case (ushort)Boss.ThrashIDS.Olson:
+                    break;
+                case (ushort)Boss.ThrashIDS.Engul:
+                    break;
+                case (ushort)Boss.ThrashIDS.Faerla:
+                    break;
+                case (ushort)Boss.ThrashIDS.Caulle:
+                    break;
+                case (ushort)Boss.ThrashIDS.Henley:
+                    break;
+                case (ushort)Boss.ThrashIDS.Jessica:
+                    break;
+                case (ushort)Boss.ThrashIDS.Galletta:
+                    break;
+                case (ushort)Boss.ThrashIDS.Ianim:
+                    break;
+                case (ushort)Boss.ThrashIDS.Core:
+                    break;
+                case (ushort)Boss.ThrashIDS.Jade:
+                    break;
+                case (ushort)Boss.ThrashIDS.Guldhem:
+                    break;
+                case (ushort)Boss.ThrashIDS.Rigom:
+                    break;
+                case (ushort)Boss.ThrashIDS.Saul:
+                    break;
+                case (ushort)Boss.ThrashIDS.Thief:
+                    break;
+                case (ushort)Boss.ThrashIDS.Gambler:
+                    break;
+                case (ushort)Boss.ThrashIDS.GamblerClones:
+                    break;
+                case (ushort)Boss.ThrashIDS.Drunkard:
+                    break;
+                case (ushort)Boss.ThrashIDS.TormentedDead:
+                    break;
+                case (ushort)Boss.ThrashIDS.SurgingSoul:
+                    break;
+                case (ushort)Boss.ThrashIDS.Scythe:
+                    break;
+                case (ushort)Boss.ThrashIDS.Messenger:
+                    break;
+                case (ushort)Boss.ThrashIDS.Echo:
+                    break;
+                case (ushort)Boss.ThrashIDS.Enforcer:
+                    break;
+            }
         }
     }
 }
