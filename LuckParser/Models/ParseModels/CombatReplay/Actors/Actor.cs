f2c9c91622d8c4a2ef11b99e0af472e96109eda0
Jekfer Bichon <jekfer.bichon@3ds.com>
2018-08-08 13:43:17 +0200

    preparing LinkActors and DoughnutActors

---
+++ LuckParser/Models/ParseModels/CombatReplay/Actors/Actor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckParser.Models.ParseModels
+{
+    public abstract class Actor
+    {
+        private bool fill;
+        private Tuple<int, int> lifespan;
+        private string color;
+        private int growing;
+
+        public Actor(bool fill, int growing, Tuple<int, int> lifespan, string color)
+        {
+            this.lifespan = lifespan;
+            this.color = color;
+            this.fill = fill;
+            this.growing = growing;
+        }
+
+        public int getGrowing()
+        {
+            return growing;
+        }
+
+        public bool isFilled()
+        {
+            return fill;
+        }
+
+        public Tuple<int, int> getLifespan()
+        {
+            return lifespan;
+        }
+
+        public string getColor()
+        {
+            return "'" + color + "'";
+        }
+    }
+}
--- LuckParser/Models/ParseModels/CombatReplay/Actors/CircleActor.cs
+++ LuckParser/Models/ParseModels/CombatReplay/Actors/CircleActor.cs
@@ -6,46 +6,18 @@ using System.Threading.Tasks;
 
 namespace LuckParser.Models.ParseModels
 {
-    public abstract class CircleActor
+    public abstract class CircleActor : Actor
     {
-        private bool fill;
-        private int growing;
         private int radius;
-        private Tuple<int, int> lifespan;
-        private string color;
 
-        public CircleActor(bool fill, int growing, int radius, Tuple<int,int> lifespan, string color)
+        public CircleActor(bool fill, int growing, int radius, Tuple<int,int> lifespan, string color) : base(fill, growing,lifespan, color)
         {
-            this.fill = fill;
-            this.growing = growing;
             this.radius = radius;
-            this.lifespan = lifespan;
-            this.color = color;
-        }
-
-        public int getGrowing()
-        {
-            return growing;
-        }
-
-        public bool isFilled()
-        {
-            return fill;
         }
 
         public int getRadius()
         {
             return radius;
-        }
-
-        public Tuple<int,int> getLifespan()
-        {
-            return lifespan;
-        }
-
-        public string getColor()
-        {
-            return "'" + color + "'";
         }
 
         public abstract string getPosition(string id, CombatReplayMap map);
