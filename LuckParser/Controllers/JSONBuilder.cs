7abdf4eafb22dcf7bf9c52672e7e6562da9da9eb
Cord Bleibaum <cordbleibaum@icloud.com>
2018-08-25 10:13:29 +0200

    JSONBuilder stub

---
+++ LuckParser/Controllers/JSONBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LuckParser.Models.DataModels;
+
+namespace LuckParser.Controllers
+{
+    class JSONBuilder
+    {
+        readonly SettingsContainer _settings;
+
+        readonly ParsedLog _log;
+
+        readonly Statistics _statistics;
+        readonly StreamWriter _sw;
+
+        public static void UpdateStatisticSwitches(StatisticsCalculator.Switches switches)
+        {
+            switches.CalculateBoons = true;
+            switches.CalculateDPS = true;
+            switches.CalculateConditions = true;
+            switches.CalculateDefense = true;
+            switches.CalculateStats = true;
+            switches.CalculateSupport = true;
+            switches.CalculateCombatReplay = true;
+            switches.CalculateMechanics = true;
+        }
+
+        public JSONBuilder(StreamWriter sw, ParsedLog log, SettingsContainer settings, Statistics statistics)
+        {
+            _log = log;
+            _sw = sw;
+            _settings = settings;
+
+            _statistics = statistics;
+        }
+
+        //Creating JSON---------------------------------------------------------------------------------
+        public void CreateJSON()
+        {
+            // TODO
+        }
+    }
+}
