7318fad92b60d558794a4915ceda34d6f6bd3f2b
Jekfer Bichon <bichon.jekfer@gmail.com>
2018-08-25 17:10:41 +0200

    Removed boonToTrack from the signature of several methods. That list is now stocked on abstractMasterPlayer. Changed Boon.GetXXList() by their _statistics equivalent when possible. Changed some List<Boon> to HashSet<long> as they were used only for id lookups

--- LuckParser/Controllers/HTMLBuilder.cs
+++ LuckParser/Controllers/HTMLBuilder.cs
@@ -1132,13 +1132,13 @@ namespace LuckParser.Controllers
                     {
 
                         Dictionary<long, Statistics.FinalBoonUptime> boons = _statistics.SelfBoons[player][phaseIndex];
-                        Dictionary<long, Dictionary<int, string[]>> extraBoonData = player.GetExtraBoonData(_log, phases, listToUse);
+                        Dictionary<long, Dictionary<int, string[]>> extraBoonData = player.GetExtraBoonData(_log, phases);
                         List<string> boonArrayToList = new List<string>
                         {
                             player.GetGroup().ToString()
                         };
                         long fightDuration = phases[phaseIndex].GetDuration();
-                        Dictionary<long, long> boonPresence = player.GetBoonPresence(_log, phases, listToUse, phaseIndex);
+                        Dictionary<long, long> boonPresence = player.GetBoonPresence(_log, phases, phaseIndex);
                         int count = 0;
 
                         sw.Write("<tr>");
@@ -1671,15 +1671,7 @@ namespace LuckParser.Controllers
                                         }
                                         if (_statistics.PresentBoons.Count > 0)
                                         {
-                                            List<Boon> parseBoonsList = new List<Boon>();
-                                            parseBoonsList.AddRange(_statistics.PresentBoons);
-                                            parseBoonsList.AddRange(_statistics.PresentOffbuffs);
-                                            parseBoonsList.AddRange(_statistics.PresentDefbuffs);
-                                            if (_statistics.PresentPersonnalBuffs.ContainsKey(p.GetInstid()))
-                                            {
-                                                parseBoonsList.AddRange(_statistics.PresentPersonnalBuffs[p.GetInstid()]);
-                                            }
-                                            Dictionary<long, BoonsGraphModel> boonGraphData = p.GetBoonGraphs(_log, phases, parseBoonsList);
+                                            Dictionary<long, BoonsGraphModel> boonGraphData = p.GetBoonGraphs(_log, phases);
                                             foreach (BoonsGraphModel bgm in boonGraphData.Values.Reverse().Where(x => x.GetBoonName() != "Number of Conditions"))
                                             {
                                                 sw.Write("{");
@@ -1689,11 +1681,7 @@ namespace LuckParser.Controllers
                                                 sw.Write(" },");
 
                                             }
-                                            parseBoonsList = new List<Boon>();
-                                            parseBoonsList.AddRange(Boon.GetCondiBoonList());
-                                            parseBoonsList.AddRange(Boon.GetBoonList());
-                                            parseBoonsList.AddRange(Boon.GetBossBoonList());
-                                            boonGraphData = _log.GetBoss().GetBoonGraphs(_log, phases, parseBoonsList);
+                                            boonGraphData = _log.GetBoss().GetBoonGraphs(_log, phases);
                                             foreach (BoonsGraphModel bgm in boonGraphData.Values.Reverse().Where(x => x.GetBoonName() == "Compromised" || x.GetBoonName() == "Unnatural Signet"))
                                             {
                                                 sw.Write("{");
@@ -2192,7 +2180,7 @@ namespace LuckParser.Controllers
         {
             HashSet<long> usedIDs = new HashSet<long>();
             SkillData skillList = _log.GetSkillData();
-            HTMLHelper.WriteDamageDistTableCondi(sw, usedIDs, damageLogs, finalTotalDamage);
+            HTMLHelper.WriteDamageDistTableCondi(sw, usedIDs, damageLogs, finalTotalDamage, _statistics.PresentConditions);
             foreach (int id in damageLogs.Where(x => !usedIDs.Contains(x.GetID())).Select(x => x.GetID()).Distinct().ToList())
             {
                 SkillItem skill = skillList.Get(id);
@@ -2479,7 +2467,7 @@ namespace LuckParser.Controllers
                 sw.Write("<tbody>");
                 {
                     HashSet<long> usedIDs = new HashSet<long>();
-                    List<Boon> condiList = Boon.GetCondiBoonList();
+                    List<Boon> condiList = _statistics.PresentConditions;
                     foreach (Boon condi in condiList)
                     {
                         long condiID = condi.GetID();
@@ -2875,7 +2863,7 @@ namespace LuckParser.Controllers
             long fightDuration = phases[phaseIndex].GetDuration();
             Dictionary<long, Statistics.FinalBossBoon> conditions = _statistics.BossConditions[phaseIndex];
             bool hasBoons = false;
-            foreach (Boon boon in Boon.GetBoonList())
+            foreach (Boon boon in _statistics.PresentBoons)
             {
                 if (boon.GetName() == "Retaliation")
                 {
@@ -2887,10 +2875,7 @@ namespace LuckParser.Controllers
                     break;
                 }
             }
-            List<Boon> boonToTrack = Boon.GetCondiBoonList();
-            boonToTrack.AddRange(Boon.GetBoonList());
-            Dictionary<long, long> condiPresence = boss.GetCondiPresence(_log, phases, boonToTrack, phaseIndex);
-            Dictionary<long, long> boonPresence = boss.GetBoonPresence(_log, phases, boonToTrack, phaseIndex);
+            Dictionary<long, long> condiPresence = boss.GetCondiPresence(_log, phases, phaseIndex);
             double avgCondis = 0.0;
             foreach (long duration in condiPresence.Values)
             {
@@ -2907,7 +2892,7 @@ namespace LuckParser.Controllers
                     sw.Write("<tr>");
                     {
                         sw.Write("<th>Name</th>");
-                        foreach (Boon boon in Boon.GetCondiBoonList())
+                        foreach (Boon boon in _statistics.PresentConditions)
                         {
                             if (hasBoons && boon.GetName() == "Retaliation")
                             {
@@ -2925,7 +2910,7 @@ namespace LuckParser.Controllers
                     {
                         
                         sw.Write("<td style=\"width: 275px;\" data-toggle=\"tooltip\" title=\"Average number of conditions: " + Math.Round(avgCondis, 1) + "\">" + boss.GetCharacter() + " </td>");
-                        foreach (Boon boon in Boon.GetCondiBoonList())
+                        foreach (Boon boon in _statistics.PresentConditions)
                         {
                             if (hasBoons && boon.GetName() == "Retaliation")
                             {
@@ -2957,6 +2942,7 @@ namespace LuckParser.Controllers
             // Boon table if applicable
             if (hasBoons)
             {
+                Dictionary<long, long> boonPresence = boss.GetBoonPresence(_log, phases, phaseIndex);
                 sw.Write("<h3 align=\"center\"> Boon Uptime </h3>");
                 sw.Write("<script> $(function () { $('#boss_boon_table" + phaseIndex + "').DataTable({ \"order\": [[3, \"desc\"]]});});</script>");
                 sw.Write("<table class=\"display table table-striped table-hover compact mb-3\"  cellspacing=\"0\" width=\"100%\" id=\"boss_boon_table" + phaseIndex + "\">");
@@ -2966,7 +2952,7 @@ namespace LuckParser.Controllers
                         sw.Write("<tr>");
                         {
                             sw.Write("<th>Name</th>");
-                            foreach (Boon boon in Boon.GetBoonList())
+                            foreach (Boon boon in _statistics.PresentBoons)
                             {
                                 sw.Write("<th>" + "<img src=\"" + boon.GetLink() + " \" alt=\"" + boon.GetName() + "\" title =\" " + boon.GetName() + "\" height=\"18\" width=\"18\" >" + "</th>");
                             }
@@ -2979,7 +2965,7 @@ namespace LuckParser.Controllers
                         sw.Write("<tr>");
                         {
                             sw.Write("<td style=\"width: 275px;\">" + boss.GetCharacter() + " </td>");
-                            foreach (Boon boon in Boon.GetBoonList())
+                            foreach (Boon boon in _statistics.PresentBoons)
                             {
                                 if (boon.GetBoonType() == Boon.BoonType.Duration)
                                 {
@@ -3017,7 +3003,7 @@ namespace LuckParser.Controllers
                         sw.Write("<th>Sub</th>");
                         sw.Write("<th></th>");
                         sw.Write("<th>Name</th>");
-                        foreach (Boon boon in Boon.GetCondiBoonList())
+                        foreach (Boon boon in _statistics.PresentConditions)
                         {
                             if (boon.GetName() == "Retaliation")
                             {
@@ -3038,7 +3024,7 @@ namespace LuckParser.Controllers
                             sw.Write("<td>" + player.GetGroup().ToString() + "</td>");
                             sw.Write("<td>" + "<img src=\"" + HTMLHelper.GetLink(player.GetProf()) + "\" alt=\"" + player.GetProf() + "\" height=\"18\" width=\"18\" >" + "<span style=\"display:none\">" + player.GetProf() + "</span>" + "</td>");
                             sw.Write("<td>" + player.GetCharacter() + " </td>");
-                            foreach (Boon boon in Boon.GetCondiBoonList())
+                            foreach (Boon boon in _statistics.PresentConditions)
                             {
                                 if (boon.GetName() == "Retaliation")
                                 {
@@ -3115,11 +3101,7 @@ namespace LuckParser.Controllers
                                 }
                             }
                             //============================================
-                            List<Boon> parseBoonsList = new List<Boon>();
-                            parseBoonsList.AddRange(Boon.GetCondiBoonList());
-                            parseBoonsList.AddRange(Boon.GetBoonList());
-                            parseBoonsList.AddRange(Boon.GetBossBoonList());
-                            Dictionary<long, BoonsGraphModel> boonGraphData = _log.GetBoss().GetBoonGraphs(_log, phases, parseBoonsList);
+                            Dictionary<long, BoonsGraphModel> boonGraphData = _log.GetBoss().GetBoonGraphs(_log, phases);
                             foreach (BoonsGraphModel bgm in boonGraphData.Values.Reverse().Where(x => x.GetBoonName() != "Number of Boons"))
                             {
                                 sw.Write("{");
