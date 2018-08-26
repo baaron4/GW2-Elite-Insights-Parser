115827679f52dd6049a053c5b8f701f2a68b35e5
Jekfer Bichon <bichon.jekfer@gmail.com>
2018-08-27 00:08:55 +0200

    migrated Skorvald and MAMA to new mechanic constructor

--- LuckParser/Models/BossLogic/MAMA.cs
+++ LuckParser/Models/BossLogic/MAMA.cs
@@ -11,15 +11,15 @@ namespace LuckParser.Models
         {
             MechanicList.AddRange(new List<Mechanic>
             {
-            new Mechanic(37408, "Blastwave", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle',color:'rgb(255,200,0)',", "KB",0), //Blastwave (Spinning Knockback), KB Spin
-            new Mechanic(37103, "Blastwave", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle',color:'rgb(255,200,0)',", "KB",0), //Blastwave (Spinning Knockback), KB Spin
-            new Mechanic(37391, "Tantrum", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'star-diamond-open',color:'rgb(0,255,0)',", "Tntrm",4000), //Tantrum (Many rapid front spins), Spinning Tantrum
-            new Mechanic(37577, "Leap", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle',color:'rgb(255,0,0)',", "Jmp",0), //Leap (<33% only), Leap
-            new Mechanic(37437, "Shoot", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle-open',color:'rgb(200,150,0)',", "Shoot",0), //Shoot, Shoot
-            new Mechanic(37185, "Explosive Impact", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'diamond',color:'rgb(255,200,0)',", "ExplImp",0), // Explosive Impact, Explosive Impact
-            new Mechanic(37085, "Sweeping Strikes", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'square-open',color:'rgb(255,0,0)',", "Swp",200), //Double Swing (between Jump and Slams), Sweeping Strikes
-            new Mechanic(37217, "Nightmare Miasma", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle-open',color:'rgb(0,255,255)',", "Goo",0), //Nightmare Miasma (Poison Puddle), Poison Goo
-            new Mechanic(37180, "Grenade Barrage", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle-open',color:'rgb(255,0,0)',", "Balls",0), //Grenade Barrage (many projectiles in all directions), Ball Barrage
+            new Mechanic(37408, "Blastwave", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle',color:'rgb(255,200,0)',", "KB","Blastwave (Spinning Knockback)", "KB Spin",0),
+            new Mechanic(37103, "Blastwave", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle',color:'rgb(255,200,0)',", "KB","Blastwave (Spinning Knockback)", "KB Spin",0),
+            new Mechanic(37391, "Tantrum", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'star-diamond-open',color:'rgb(0,255,0)',", "Tntrm","Tantrum (Many rapid front spins)", "Spinning Tantrum",4000),
+            new Mechanic(37577, "Leap", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle',color:'rgb(255,0,0)',", "Jmp","Leap (<33% only)", "Leap",0),
+            new Mechanic(37437, "Shoot", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle-open',color:'rgb(200,150,0)',", "Shoot",0),
+            new Mechanic(37185, "Explosive Impact", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'diamond',color:'rgb(255,200,0)',", "ExplImp","Explosive Impact", "Explosive Impact",0),
+            new Mechanic(37085, "Sweeping Strikes", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'square-open',color:'rgb(255,0,0)',", "Swp","Double Swing (between Jump and Slams)", "Sweeping Strikes",200),
+            new Mechanic(37217, "Nightmare Miasma", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle-open',color:'rgb(0,255,255)',", "Goo","Nightmare Miasma (Poison Puddle)", "Poison Goo",0),
+            new Mechanic(37180, "Grenade Barrage", Mechanic.MechType.SkillOnPlayer, ParseEnum.BossIDS.MAMA, "symbol:'circle-open',color:'rgb(255,0,0)',", "Balls","Grenade Barrage (many projectiles in all directions)", "Ball Barrage",0),
             });
         }
 
