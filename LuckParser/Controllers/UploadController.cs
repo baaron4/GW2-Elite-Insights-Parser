fa412574ba21c392247926c17186ae16f1e0c986
baaron4 <baaron4thewin@gmail.com>
2018-09-07 12:24:35 -0500

    small code cleanup

--- LuckParser/Controllers/UploadController.cs
+++ LuckParser/Controllers/UploadController.cs
@@ -15,87 +15,12 @@ namespace LuckParser.Controllers
     {
         public string  UploadDPSReportsEI(FileInfo fi)
         {
-            string fileName = fi.Name;
-            byte[] fileContents = File.ReadAllBytes(fi.FullName);
-            Uri webService = new Uri(@"https://dps.report/uploadContent?generator=ei");
-            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, webService);
-            requestMessage.Headers.ExpectContinue = false;
-
-            MultipartFormDataContent multiPartContent = new MultipartFormDataContent("----MyGreatBoundary");
-            ByteArrayContent byteArrayContent = new ByteArrayContent(fileContents);
-            byteArrayContent.Headers.Add("Content-Type", "application/octet-stream");
-            multiPartContent.Add(byteArrayContent, "file", fileName);
-            //multiPartContent.Add(new StringContent("generator=ei"), "gen", "ei");
-            requestMessage.Content = multiPartContent;
-
-            HttpClient httpClient = new HttpClient();
-            try
-            {
-                Task<HttpResponseMessage> httpRequest = httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead, CancellationToken.None);
-                HttpResponseMessage httpResponse = httpRequest.Result;
-                HttpStatusCode statusCode = httpResponse.StatusCode;
-                HttpContent responseContent = httpResponse.Content;
-
-                if (responseContent != null)
-                {
-                    Task<String> stringContentsTask = responseContent.ReadAsStringAsync();
-                    String stringContents = stringContentsTask.Result;
-                    int first = stringContents.IndexOf('{');
-                    int length = stringContents.LastIndexOf('}') - first +1;
-                    String JSONFormat = stringContents.Substring(first,length);
-                    DPSReportsResponseItem item = JsonConvert.DeserializeObject<DPSReportsResponseItem>(JSONFormat);
-                    String logLink = item.permalink;
-                    return logLink;
-                }
-            }
-            catch (Exception ex)
-            {
-                return ex.Message;
-               // Console.WriteLine(ex.Message);
-            }
-            return "";
+            return UploadToDPSR(fi, "https://dps.report/uploadContent?generator=ei");
         }
         public string UploadDPSReportsRH(FileInfo fi)
         {
-            string fileName = fi.Name;
-            byte[] fileContents = File.ReadAllBytes(fi.FullName);
-            Uri webService = new Uri(@"https://dps.report/uploadContent?generator=rh");
-            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, webService);
-            requestMessage.Headers.ExpectContinue = false;
-
-            MultipartFormDataContent multiPartContent = new MultipartFormDataContent("----MyGreatBoundary");
-            ByteArrayContent byteArrayContent = new ByteArrayContent(fileContents);
-            byteArrayContent.Headers.Add("Content-Type", "application/octet-stream");
-            multiPartContent.Add(byteArrayContent, "file", fileName);
-            //multiPartContent.Add(new StringContent("generator=ei"), "gen", "ei");
-            requestMessage.Content = multiPartContent;
-
-            HttpClient httpClient = new HttpClient();
-            try
-            {
-                Task<HttpResponseMessage> httpRequest = httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead, CancellationToken.None);
-                HttpResponseMessage httpResponse = httpRequest.Result;
-                HttpStatusCode statusCode = httpResponse.StatusCode;
-                HttpContent responseContent = httpResponse.Content;
-
-                if (responseContent != null)
-                {
-                    Task<String> stringContentsTask = responseContent.ReadAsStringAsync();
-                    String stringContents = stringContentsTask.Result;
-                    int first = stringContents.IndexOf('{');
-                    int length = stringContents.LastIndexOf('}') - first + 1;
-                    String JSONFormat = stringContents.Substring(first, length);
-                    DPSReportsResponseItem item = JsonConvert.DeserializeObject<DPSReportsResponseItem>(JSONFormat);
-                    String logLink = item.permalink;
-                    return logLink;
-                }
-            }
-            catch (Exception ex)
-            {
-                return ex.Message;
-                // Console.WriteLine(ex.Message);
-            }
-            return "";
+            return UploadToDPSR(fi, "https://dps.report/uploadContent?generator=rh");
+           
         }
         public string UploadRaidar(FileInfo fi)
         {
@@ -142,7 +67,48 @@ namespace LuckParser.Controllers
         {
             public string permalink;
         }
+        private string UploadToDPSR(FileInfo fi,string URI)
+        {
+            string fileName = fi.Name;
+            byte[] fileContents = File.ReadAllBytes(fi.FullName);
+            Uri webService = new Uri(@URI);
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, webService);
+            requestMessage.Headers.ExpectContinue = false;
 
+            MultipartFormDataContent multiPartContent = new MultipartFormDataContent("----MyGreatBoundary");
+            ByteArrayContent byteArrayContent = new ByteArrayContent(fileContents);
+            byteArrayContent.Headers.Add("Content-Type", "application/octet-stream");
+            multiPartContent.Add(byteArrayContent, "file", fileName);
+            //multiPartContent.Add(new StringContent("generator=ei"), "gen", "ei");
+            requestMessage.Content = multiPartContent;
+
+            HttpClient httpClient = new HttpClient();
+            try
+            {
+                Task<HttpResponseMessage> httpRequest = httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead, CancellationToken.None);
+                HttpResponseMessage httpResponse = httpRequest.Result;
+                HttpStatusCode statusCode = httpResponse.StatusCode;
+                HttpContent responseContent = httpResponse.Content;
+
+                if (responseContent != null)
+                {
+                    Task<String> stringContentsTask = responseContent.ReadAsStringAsync();
+                    String stringContents = stringContentsTask.Result;
+                    int first = stringContents.IndexOf('{');
+                    int length = stringContents.LastIndexOf('}') - first + 1;
+                    String JSONFormat = stringContents.Substring(first, length);
+                    DPSReportsResponseItem item = JsonConvert.DeserializeObject<DPSReportsResponseItem>(JSONFormat);
+                    String logLink = item.permalink;
+                    return logLink;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+                // Console.WriteLine(ex.Message);
+            }
+            return "";
+        }
           
     }
     
