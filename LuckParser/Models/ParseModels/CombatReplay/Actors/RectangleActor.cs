a7e4496d71b53b4eee3d7acbd3758a0bde838522
Linus <no34rax@ph.tum.de>
2018-09-13 14:08:29 +0200

    Finished RectangleActor and modified Surging Souls
    Finished Implementation of the Rectangular actor class and modified the Walls (Surging Souls) at Soulless Horror to properly fit their dimensions. Cleaned up mess from previous RectangleActor commit

--- LuckParser/Models/ParseModels/CombatReplay/Actors/RectangleActor.cs
+++ LuckParser/Models/ParseModels/CombatReplay/Actors/RectangleActor.cs
@@ -7,19 +7,13 @@ namespace LuckParser.Models.ParseModels
         private readonly int _height;
         private readonly int _width;
 
-        public RectangleActor(bool fill, int growing, int height, int width, Tuple<int, int> lifespan, string color) : base(true, growing, lifespan, color, new MobileActor())
+        public RectangleActor(bool fill, int growing, int width, int height, Tuple<int, int> lifespan, string color) : base(true, growing, lifespan, color, new MobileActor())
         {
             _height = height;
             _width = width;
         }
 
-        public RectangleActor(bool fill, int growing, int height, int width, Tuple<int, int> lifespan, string color, Point3D position) : base(true, growing, lifespan, color, new ImmobileActor(position))
-        {
-            _height = height;
-            _width = width;
-        }
-
-        public RectangleActor(bool fill, int growing, int height, int width, Tuple<int, int> lifespan, string color, string iconLink) : base(true, growing, lifespan, color, new MobileActor())
+        public RectangleActor(bool fill, int growing, int width, int height, Tuple<int, int> lifespan, string color, Point3D position) : base(true, growing, lifespan, color, new ImmobileActor(position))
         {
             _height = height;
             _width = width;
