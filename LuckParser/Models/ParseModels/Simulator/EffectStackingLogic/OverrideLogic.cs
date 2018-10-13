04e6628a60c38b2bf5728ee654ed58add422f5e5
Jekfer Bichon <bichon.jekfer@gmail.com>
2018-10-13 13:46:22 +0200

    fixed a bug in queue logic

--- LuckParser/Models/ParseModels/Simulator/EffectStackingLogic/OverrideLogic.cs
+++ LuckParser/Models/ParseModels/Simulator/EffectStackingLogic/OverrideLogic.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using LuckParser.Models.DataModels;
+using static LuckParser.Models.ParseModels.BoonSimulator;
 
 namespace LuckParser.Models.ParseModels
 {
@@ -12,7 +13,18 @@ namespace LuckParser.Models.ParseModels
 
         public override bool StackEffect(ParsedLog log, BoonSimulator.BoonStackItem stackItem, List<BoonSimulator.BoonStackItem> stacks, List<BoonSimulationOverstackItem> overstacks)
         {
-            return StackEffect(0, log, stackItem, stacks, overstacks);
+            for (int i = 0; i < stacks.Count; i++)
+            {
+                if (stacks[i].BoonDuration < stackItem.BoonDuration)
+                {
+                    BoonStackItem stack = stacks[i];
+                    overstacks.Add(new BoonSimulationOverstackItem(stack.Src, stack.BoonDuration, stack.Start));
+                    stacks[i] = stackItem;
+                    Sort(log, stacks);
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
