6df2eb81b37028fe35c34797cc8bf9c8ea2c0a66
Jekfer Bichon <bichon.jekfer@gmail.com>
2018-10-21 18:24:44 +0200

    merge

---
+++ LuckParser/Controllers/CancellationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LuckParser
+{
+    public class CancellationException : Exception
+    {
+        public GridRow Row { get; }
+
+        public CancellationException(GridRow row)
+        {
+            Row = row;
+        }
+
+        public CancellationException(GridRow row, Exception inner) : base("", inner)
+        {
+            Row = row;
+        }
+    }
+}
