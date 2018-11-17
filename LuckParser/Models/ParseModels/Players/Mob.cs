7c4177274abfe3a132b9cd73e61e110fdf59bfea
Jekfer Bichon <bichon.jekfer@gmail.com>
2018-11-17 21:44:33 +0100

    removed invalid operation throws from mob.cs

--- LuckParser/Models/ParseModels/Players/Mob.cs
+++ LuckParser/Models/ParseModels/Players/Mob.cs
@@ -17,15 +17,12 @@ namespace LuckParser.Models.ParseModels
         //setters
         protected override void SetDamageLogs(ParsedLog log)
         {
-            throw new InvalidOperationException();
         }
         protected override void SetDamageTakenLogs(ParsedLog log)
         {
-            throw new InvalidOperationException();
         }
         protected override void SetCastLogs(ParsedLog log)
         {
-            throw new InvalidOperationException();
         }
 
         protected override void SetAdditionalCombatReplayData(ParsedLog log)
