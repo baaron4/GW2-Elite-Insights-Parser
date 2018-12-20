2261575700a511dc427013b2cd9378f508132d57
EliphasNUIT <bichon.jekfer@gmail.com>
2018-12-20 23:06:39 +0100

    added tooshort exception

---
+++ LuckParser/Exceptions/TooShortException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuckParser.Exceptions
+{
+    public class TooShortException : Exception
+    {
+        public TooShortException() : base("Fight is less than 1 second, aborted")
+        {
+        }
+
+    }
+}
