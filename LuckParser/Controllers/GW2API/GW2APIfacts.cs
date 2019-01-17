4bd32d28b109fb3b089186668ac2581d9f9bc4b5
Jekfer Bichon <jekfer.bichon@3ds.com>
2019-01-17 11:17:23 +0100

    some renaming

--- LuckParser/Controllers/GW2API/GW2APIfacts.cs
+++ LuckParser/Controllers/GW2API/GW2APIfacts.cs
@@ -2,7 +2,7 @@
 
 namespace LuckParser.Controllers
 {
-    public class GW2APIfacts
+    public class GW2APIFacts
     {
         public string Text { get; set; }
         public string Icon { get; set; }
@@ -16,14 +16,14 @@ namespace LuckParser.Controllers
         [JsonProperty(PropertyName = "field_type")]
         public string FieldType { get; set; }
         [JsonProperty(PropertyName = "finisher_type")]
-        public string Finisher_type { get; set; }
+        public string FinisherType { get; set; }
         public float Percent { get; set; }
         [JsonProperty(PropertyName = "hit_count")]
         public int HitCount { get; set; }
         [JsonProperty(PropertyName = "dmg_multiplier")]
         public float DmgMultiplier { get; set; }
         public int Distance { get; set; }
-        public GW2APIfacts Prefix { get; set; }
+        public GW2APIFacts Prefix { get; set; }
 
         public object Value { get; set; }
     }
