7ee1f45bee598d2b9a3b5fe32d7939ece541c57f
EliphasNUIT <bichon.jekfer@gmail.com>
2019-02-25 23:36:57 +0100

    updated version

--- LuckParser/Properties/AssemblyInfo.cs
+++ LuckParser/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 #if PROD    
-[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyVersion("2.7.1.0")]
 #else
 [assembly: AssemblyVersion("2.7.*")]
 #endif
