78653c7ef7280121b42ac8144a9a7ce77d4296ff
Jekfer Bichon <jekfer.bichon@3ds.com>
2019-03-11 18:31:40 +0100

    updated some damage modifiers

--- LuckParser/Models/ParseModels/DamageModifiers/DamageModifier.cs
+++ LuckParser/Models/ParseModels/DamageModifiers/DamageModifier.cs
@@ -92,9 +92,9 @@ namespace LuckParser.Models.ParseModels
             {
                 case "Druid":
                     return new List<ModifierSource> { ModifierSource.Ranger, ModifierSource.Druid };
-                case "Ranger":
-                    return new List<ModifierSource> { ModifierSource.Ranger, ModifierSource.Soulbeast };
                 case "Soulbeast":
+                    return new List<ModifierSource> { ModifierSource.Ranger, ModifierSource.Soulbeast };
+                case "Ranger":
                     return new List<ModifierSource> { ModifierSource.Ranger};
                 case "Scrapper":
                     return new List<ModifierSource> { ModifierSource.Engineer, ModifierSource.Scrapper };
@@ -182,15 +182,15 @@ namespace LuckParser.Models.ParseModels
             new BuffDamageModifier(Boon.GetBoonByName("Vicious Lacerations"), DamageSource.NoPets, 3.0, DamageType.Power, DamageType.All, ModifierSource.Revenant, ByStack),
             new BuffDamageModifier(Boon.GetBoonByName("Retaliation"), "Vicious Reprisal", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Revenant, ByPresence, "https://wiki.guildwars2.com/images/c/cf/Vicious_Reprisal.png"),
             new BuffDamageModifierTarget(Boon.GetBoonByName("Weakness"), "Dwarven Battle Training", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Revenant, ByPresence, "https://wiki.guildwars2.com/images/5/50/Dwarven_Battle_Training.png"),
-            new BuffDamageModifier(Boon.GetBoonByName("Number of Boons"), "Reinforced Potency", DamageSource.NoPets, 1.0, DamageType.Power, DamageType.All, ModifierSource.Revenant, ByStack, "https://wiki.guildwars2.com/images/0/0a/Envoy_of_Sustenance.png"),
+            new BuffDamageModifier(Boon.GetBoonByName("Number of Boons"), "Reinforced Potency", DamageSource.NoPets, 1.0, DamageType.Power, DamageType.All, ModifierSource.Herald, ByStack, "https://wiki.guildwars2.com/images/0/0a/Envoy_of_Sustenance.png"),
             new BuffDamageModifier(Boon.GetBoonByName("Fury"), "Ferocious Aggression", DamageSource.NoPets, 7.0, DamageType.All, DamageType.All, ModifierSource.Revenant, ByPresence, "https://wiki.guildwars2.com/images/e/ec/Ferocious_Aggression.png"),
-            new BuffDamageModifier(Boon.GetBoonByName("Kalla's Fervor"), DamageSource.NoPets, 2.0, DamageType.Condition, DamageType.All, ModifierSource.Revenant, ByStack),
-            new BuffDamageModifier(Boon.GetBoonByName("Improved Kalla's Fervor"), DamageSource.NoPets, 3.0, DamageType.Condition, DamageType.All, ModifierSource.Revenant, ByStack),
+            new BuffDamageModifier(Boon.GetBoonByName("Kalla's Fervor"), DamageSource.NoPets, 2.0, DamageType.Condition, DamageType.All, ModifierSource.Renegade, ByStack),
+            new BuffDamageModifier(Boon.GetBoonByName("Improved Kalla's Fervor"), DamageSource.NoPets, 3.0, DamageType.Condition, DamageType.All, ModifierSource.Renegade, ByStack),
             new BuffDamageModifierTarget(Boon.GetBoonByName("Vulnerability"), "Targeted Destruction", DamageSource.NoPets, 0.5, DamageType.Power, DamageType.All, ModifierSource.Revenant, ByStack, "https://wiki.guildwars2.com/images/e/ed/Targeted_Destruction.png"),
             new DamageLogDamageModifier("Swift Termination", DamageSource.NoPets, 20.0, DamageType.Power, DamageType.All, ModifierSource.Revenant,"https://wiki.guildwars2.com/images/b/bb/Swift_Termination.png", x => x.IsFifty, ByPresence),
             /// Warrior
             new BuffDamageModifier(Boon.GetBoonByName("Peak Performance"), DamageSource.NoPets, 20.0, DamageType.Power, DamageType.All, ModifierSource.Warrior, ByPresence),
-            new BuffDamageModifier(Boon.GetBoonByName("Always Angry"), DamageSource.NoPets, 7.0, DamageType.All, DamageType.All, ModifierSource.Warrior, ByStack),
+            new BuffDamageModifier(Boon.GetBoonByName("Always Angry"), DamageSource.NoPets, 7.0, DamageType.All, DamageType.All, ModifierSource.Berserker, ByStack),
             new BuffDamageModifierTarget(Boon.GetBoonByName("Weakness"), "Cull the Weak", DamageSource.NoPets, 7.0, DamageType.Power, DamageType.All, ModifierSource.Warrior, ByPresence, "https://wiki.guildwars2.com/images/7/72/Cull_the_Weak.png"),
             new BuffDamageModifier(Boon.GetBoonByName("Number of Boons"), "Empowered", DamageSource.NoPets, 1.0, DamageType.Power, DamageType.All, ModifierSource.Warrior, ByStack, "https://wiki.guildwars2.com/images/c/c2/Empowered.png"),
             new BuffDamageModifierTarget(Boon.GetBoonByName("Number of Boons"), "Destruction of the Empowered", DamageSource.NoPets, 3.0, DamageType.Power, DamageType.All, ModifierSource.Warrior, ByStack, "https://wiki.guildwars2.com/images/5/5c/Destruction_of_the_Empowered.png"),
@@ -200,10 +200,10 @@ namespace LuckParser.Models.ParseModels
             new BuffDamageModifierTarget(Boon.GetBoonByName("Burning"), "Fiery Wrath", DamageSource.NoPets, 7.0, DamageType.Power, DamageType.All, ModifierSource.Guardian, ByPresence, "https://wiki.guildwars2.com/images/7/70/Fiery_Wrath.png"),
             new BuffDamageModifier(Boon.GetBoonByName("Retaliation"), "Retribution", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Guardian, ByStack, "https://wiki.guildwars2.com/images/d/d7/Retribution_%28trait%29.png"),
             new BuffDamageModifier(Boon.GetBoonByName("Aegis"), "Unscathed Contender", DamageSource.NoPets, 20.0, DamageType.Power, DamageType.All, ModifierSource.Guardian, ByPresence, "https://wiki.guildwars2.com/images/b/b4/Unscathed_Contender.png"),
-            new BuffDamageModifier(Boon.GetBoonByName("Number of Boons"), "Power of the Virtuous", DamageSource.NoPets, 1.0, DamageType.Power, DamageType.All, ModifierSource.Guardian, ByStack, "https://wiki.guildwars2.com/images/b/b4/Unscathed_Contender.png"),
-            new BuffDamageModifierTarget(Boon.GetBoonByName("Crippled"), "Zealot's Aggression", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Guardian, ByPresence, "https://wiki.guildwars2.com/images/b/b4/Unscathed_Contender.png"),
+            new BuffDamageModifier(Boon.GetBoonByName("Number of Boons"), "Power of the Virtuous", DamageSource.NoPets, 1.0, DamageType.Power, DamageType.All, ModifierSource.Guardian, ByStack, "https://wiki.guildwars2.com/images/e/ee/Power_of_the_Virtuous.png"),
+            new BuffDamageModifierTarget(Boon.GetBoonByName("Crippled"), "Zealot's Aggression", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Dragonhunter, ByPresence, "https://wiki.guildwars2.com/images/7/7e/Zealot%27s_Aggression.png"),
             /// ENGINEER
-            new BuffDamageModifier(Boon.GetBoonByName("Laser's Edge"), DamageSource.NoPets, 15.0, DamageType.Power, DamageType.All, ModifierSource.Engineer, ByPresence),
+            new BuffDamageModifier(Boon.GetBoonByName("Laser's Edge"), DamageSource.NoPets, 15.0, DamageType.Power, DamageType.All, ModifierSource.Holosmith, ByPresence),
             new BuffDamageModifier(Boon.GetBoonByName("Thermal Vision"), DamageSource.NoPets, 5.0, DamageType.Condition, DamageType.All, ModifierSource.Engineer, ByPresence),
             new BuffDamageModifier(Boon.GetBoonByName("Vigor"), "Excessive Energy", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Engineer, ByPresence, "https://wiki.guildwars2.com/images/1/1f/Excessive_Energy.png"),
             new BuffDamageModifierTarget(Boon.GetBoonByName("Vulnerability"), "Shaped Charge", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Engineer, ByPresence, "https://wiki.guildwars2.com/images/f/f3/Explosive_Powder.png"),
@@ -211,42 +211,42 @@ namespace LuckParser.Models.ParseModels
             /// RANGER
             new BuffDamageModifier(Boon.GetBoonByName("Sic 'Em!"), DamageSource.NoPets, 40.0, DamageType.Power, DamageType.All, ModifierSource.Ranger, ByPresence),
             new BuffDamageModifier(Boon.GetBoonByName("Light on your Feet"), DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Ranger, ByPresence),
-            new BuffDamageModifier(Boon.GetBoonByName("Twice as Vicious"), DamageSource.NoPets, 5.0, DamageType.All, DamageType.All, ModifierSource.Ranger, ByPresence),
+            new BuffDamageModifier(Boon.GetBoonByName("Twice as Vicious"), DamageSource.NoPets, 5.0, DamageType.All, DamageType.All, ModifierSource.Soulbeast, ByPresence),
             new BuffDamageModifier(Boon.GetBoonByName("Number of Boons"), "Bountiful Hunter", DamageSource.All, 1.0, DamageType.Power, DamageType.All, ModifierSource.Ranger, ByStack, "https://wiki.guildwars2.com/images/2/25/Bountiful_Hunter.png"),
-            new BuffDamageModifier(Boon.GetBoonByName("Fury"), "Furious Strength", DamageSource.NoPets, 7.0, DamageType.Power, DamageType.All, ModifierSource.Ranger, ByStack, "https://wiki.guildwars2.com/images/c/ca/Furious_Strength.png"),
+            new BuffDamageModifier(Boon.GetBoonByName("Fury"), "Furious Strength", DamageSource.NoPets, 7.0, DamageType.Power, DamageType.All, ModifierSource.Soulbeast, ByStack, "https://wiki.guildwars2.com/images/c/ca/Furious_Strength.png"),
             // TODO Predator's Onslaught ? can daze and stun be tracked?
             /// THIEF
             new BuffDamageModifier(Boon.GetBoonByName("Lead Attacks"), DamageSource.NoPets, 1.0, DamageType.All, DamageType.All, ModifierSource.Thief, ByStack),
-            new BuffDamageModifier(Boon.GetBoonByName("Lotus Training"), DamageSource.NoPets, 10.0, DamageType.Condition, DamageType.All, ModifierSource.Thief, ByPresence),
-            new BuffDamageModifier(Boon.GetBoonByName("Bounding Dodger"), DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Thief, ByPresence),
+            new BuffDamageModifier(Boon.GetBoonByName("Lotus Training"), DamageSource.NoPets, 10.0, DamageType.Condition, DamageType.All, ModifierSource.Daredevil, ByPresence),
+            new BuffDamageModifier(Boon.GetBoonByName("Bounding Dodger"), DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Daredevil, ByPresence),
             new BuffDamageModifierTarget(Boon.GetBoonByName("Number of Conditions"), "Exposed Weakness", DamageSource.NoPets, 2.0, DamageType.Power, DamageType.All, ModifierSource.Thief, ByStack, "https://wiki.guildwars2.com/images/0/02/Exposed_Weakness.png"),
             new DamageLogDamageModifier("Executioner", DamageSource.NoPets, 20.0, DamageType.Power, DamageType.All, ModifierSource.Thief,"https://wiki.guildwars2.com/images/9/93/Executioner.png", x => x.IsFifty, ByPresence),
             new DamageLogDamageModifier("Ferocius Strikes", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Thief,"https://wiki.guildwars2.com/images/d/d1/Ferocious_Strikes.png", x => !x.IsFifty, ByPresence),
             //new BuffDamageModifierTarget(Boon.GetBoonByName("Crippled").ID, "Ankle Shots", DamageSource.NoPets, 5.0, DamageType.Power, DamageType.Power, ModifierSource.Thief, _byPresence, "https://wiki.guildwars2.com/images/b/b4/Unscathed_Contender.png"), // It's not always possible to detect the presence of pistol and the trait is additive with itself. Staff master is worse as we can't detect endurance at all
             new DamageLogDamageModifier("Twin Fangs", DamageSource.NoPets, 7.0, DamageType.Power, DamageType.All, ModifierSource.Thief,"https://wiki.guildwars2.com/images/d/d1/Ferocious_Strikes.png", x => x.IsNinety && x.Result == ParseEnum.Result.Crit, ByPresence),
-            new BuffDamageModifier(Boon.GetBoonByName("Number of Boons"), "Premeditation", DamageSource.NoPets, 1.0, DamageType.Power, DamageType.All, ModifierSource.Thief, ByStack, "https://wiki.guildwars2.com/images/d/d7/Premeditation.png"),
+            new BuffDamageModifier(Boon.GetBoonByName("Number of Boons"), "Premeditation", DamageSource.NoPets, 1.0, DamageType.Power, DamageType.All, ModifierSource.Deadeye, ByStack, "https://wiki.guildwars2.com/images/d/d7/Premeditation.png"),
             /// MESMER
             new BuffDamageModifier(Boon.GetBoonByName("Compounding Power"), DamageSource.NoPets, 2.0, DamageType.Power, DamageType.All, ModifierSource.Mesmer, ByStack),
             new BuffDamageModifierTarget(Boon.GetBoonByName("Vulnerability"), "Fragility", DamageSource.NoPets, 0.5, DamageType.Power, DamageType.All, ModifierSource.Mesmer, ByStack, "https://wiki.guildwars2.com/images/3/33/Fragility.png"),
             // Phantasmal Force would require activating buff tracking on minions, huge performance impact and some code impact
             // TOCHECK Superiority Complex
-            new BuffDamageModifierTarget(Boon.GetBoonByName("Slow"), "Danger Time", DamageSource.All, 10.0, DamageType.Power, DamageType.All, ModifierSource.Mesmer, ByPresence, "https://wiki.guildwars2.com/images/3/33/Fragility.png", (x => x.Result == ParseEnum.Result.Crit)),
+            new BuffDamageModifierTarget(Boon.GetBoonByName("Slow"), "Danger Time", DamageSource.All, 10.0, DamageType.Power, DamageType.All, ModifierSource.Chronomancer, ByPresence, "https://wiki.guildwars2.com/images/3/33/Fragility.png", (x => x.Result == ParseEnum.Result.Crit)),
             /// NECROMANCER     
             new BuffDamageModifierTarget(Boon.GetBoonByName("Number of Boons"), "Spiteful Talisman", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Necromancer, ByAbsence, "https://wiki.guildwars2.com/images/9/96/Spiteful_Talisman.png"),
             new BuffDamageModifierTarget(Boon.GetBoonByName("Fear"), "Dread", DamageSource.NoPets, 33.0, DamageType.Power, DamageType.All, ModifierSource.Necromancer, ByPresence, "https://wiki.guildwars2.com/images/e/e2/Unholy_Fervor.png"),
             new DamageLogDamageModifier("Close to Death", DamageSource.NoPets, 20.0, DamageType.Power, DamageType.All, ModifierSource.Necromancer,"https://wiki.guildwars2.com/images/b/b2/Close_to_Death.png", x => x.IsFifty, ByPresence),
-            new BuffDamageModifierTarget(Boon.GetBoonByName("Chilled"), "Cold Shoulder", DamageSource.NoPets, 15.0, DamageType.Power, DamageType.All, ModifierSource.Necromancer, ByPresence, "https://wiki.guildwars2.com/images/7/78/Cold_Shoulder.png"),
+            new BuffDamageModifierTarget(Boon.GetBoonByName("Chilled"), "Cold Shoulder", DamageSource.NoPets, 15.0, DamageType.Power, DamageType.All, ModifierSource.Reaper, ByPresence, "https://wiki.guildwars2.com/images/7/78/Cold_Shoulder.png"),
             /// ELEMENTALIST
-            new BuffDamageModifier(Boon.GetBoonByName("Harmonious Conduit"), DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Elementalist, ByPresence),
-            new BuffDamageModifier(Boon.GetBoonByName("Weaver's Prowess"), DamageSource.NoPets, 10.0, DamageType.Condition, DamageType.All, ModifierSource.Elementalist, ByPresence),
-            new BuffDamageModifier(Boon.GetBoonByName("Elements of Rage"), DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Elementalist, ByPresence),
+            new BuffDamageModifier(Boon.GetBoonByName("Harmonious Conduit"), DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Tempest, ByPresence),
+            new BuffDamageModifier(Boon.GetBoonByName("Weaver's Prowess"), DamageSource.NoPets, 10.0, DamageType.Condition, DamageType.All, ModifierSource.Weaver, ByPresence),
+            new BuffDamageModifier(Boon.GetBoonByName("Elements of Rage"), DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Weaver, ByPresence),
             new BuffDamageModifier(Boon.GetBoonByName("Fire Attunement"), "Pyromancer's Training", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Elementalist, ByPresence, "https://wiki.guildwars2.com/images/e/e6/Pyromancer%27s_Training.png"),
             new BuffDamageModifierTarget(Boon.GetBoonByName("Burning"), "Burning Rage", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, ModifierSource.Elementalist, ByPresence, "https://wiki.guildwars2.com/images/b/bd/Burning_Rage.png"),
             new DamageLogDamageModifier("Bolt to the Heart", DamageSource.NoPets, 20.0, DamageType.Power, DamageType.All, ModifierSource.Elementalist,"https://wiki.guildwars2.com/images/f/f8/Bolt_to_the_Heart.png", x => x.IsFifty, ByPresence),
             new BuffDamageModifierTarget(Boon.GetBoonByName("Bleeding"), "Serrated Stones", DamageSource.NoPets, 5.0, DamageType.Power, DamageType.All, ModifierSource.Elementalist, ByPresence, "https://wiki.guildwars2.com/images/6/60/Serrated_Stones.png"),
             new DamageLogDamageModifier("Aquamancer's Training", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.Power, ModifierSource.Elementalist,"https://wiki.guildwars2.com/images/8/81/Aquamancer%27s_Training.png", x => x.IsNinety, ByPresence),
             new BuffDamageModifier(Boon.GetBoonByName("Number of Boons"), "Bountiful Power", DamageSource.NoPets, 2.0, DamageType.Power, DamageType.All, ModifierSource.Elementalist, ByStack, "https://wiki.guildwars2.com/images/7/75/Bountiful_Power.png"),
-            new BuffDamageModifier(new BuffsTrackerMulti(Boon.GetBoonByName("Swiftness"), Boon.GetBoonByName("Superspeed")), "Swift Revenge", DamageSource.NoPets, 7.0, DamageType.Power, DamageType.All, ModifierSource.Elementalist, "https://wiki.guildwars2.com/images/9/94/Swift_Revenge.png")
+            new BuffDamageModifier(new BuffsTrackerMulti(Boon.GetBoonByName("Swiftness"), Boon.GetBoonByName("Superspeed")), "Swift Revenge", DamageSource.NoPets, 7.0, DamageType.Power, DamageType.All, ModifierSource.Weaver, "https://wiki.guildwars2.com/images/9/94/Swift_Revenge.png")
             // TODO Piercing Shards
         };
 
