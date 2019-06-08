e412b72b72b621089bec3e7f1541e574bab73a0c
EliphasNUIT <bichon.jekfer@gmail.com>
2019-06-08 14:49:09 +0200

    merge fix + refacto

--- LuckParser/Models/ParseModels/Mechanics/MechanicTypes/FirstHitSkillOnPlayerMechanic.cs
+++ LuckParser/Models/ParseModels/Mechanics/MechanicTypes/FirstHitSkillOnPlayerMechanic.cs
@@ -8,14 +8,16 @@ using System.Linq;
 namespace LuckParser.Models.ParseModels
 {
     
-    public class FirstHitSkillOnPlayerMechanic : Mechanic
+    public class FirstHitSkillOnPlayerMechanic : DamageMechanic
     {
 
-        public FirstHitSkillOnPlayerMechanic(long skillId, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, int internalCoolDown, List<MechanicChecker> conditions, TriggerRule rule) : this(skillId, inGameName, plotlySetting, shortName, shortName, shortName, internalCoolDown, conditions, rule)
+        private readonly Dictionary<AgentItem, AbstractDamageEvent> _firstHits = new Dictionary<AgentItem, AbstractDamageEvent>();
+
+        public FirstHitSkillOnPlayerMechanic(long skillId, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, int internalCoolDown, List<DamageChecker> conditions, TriggerRule rule) : this(skillId, inGameName, plotlySetting, shortName, shortName, shortName, internalCoolDown, conditions, rule)
         {
         }
 
-        public FirstHitSkillOnPlayerMechanic(long skillId, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown, List<MechanicChecker> conditions, TriggerRule rule) : base(skillId, inGameName, plotlySetting, shortName, description, fullName, internalCoolDown, conditions, rule)
+        public FirstHitSkillOnPlayerMechanic(long skillId, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown, List<DamageChecker> conditions, TriggerRule rule) : base(skillId, inGameName, plotlySetting, shortName, description, fullName, internalCoolDown, conditions, rule)
         {
         }
 
@@ -27,29 +29,34 @@ namespace LuckParser.Models.ParseModels
         {
         }
 
-        public override void CheckMechanic(ParsedLog log, Dictionary<Mechanic, List<MechanicLog>> mechanicLogs, Dictionary<ushort, DummyActor> regroupedMobs)
+        private AbstractDamageEvent GetFirstHit(AgentItem src, ParsedLog log)
+        {
+            if (!_firstHits.TryGetValue(src, out AbstractDamageEvent evt))
+            {
+                AbstractDamageEvent res = log.CombatData.GetDamageData(src).Where(x => x.SkillId == SkillId).FirstOrDefault();
+                _firstHits[src] = res;
+                return res;
+            }
+            return evt;
+        }
+
+        public override void CheckMechanic(ParsedLog log, Dictionary<Mechanic, List<MechanicEvent>> mechanicLogs, Dictionary<ushort, DummyActor> regroupedMobs)
         {
             CombatData combatData = log.CombatData;
             AgentData agentData = log.AgentData;
             foreach (Player p in log.PlayerList)
             {
-                List<CombatItem> combatitems = combatData.GetDamageTakenData(p.InstID, p.FirstAware, p.LastAware);
-                foreach (CombatItem c in combatitems)
+                List<AbstractDamageEvent> combatitems = combatData.GetDamageTakenData(p.AgentItem);
+                foreach (AbstractDamageEvent c in combatitems)
                 {
-                    if ( !(c.SkillID == SkillId) || (c.IsBuff == 0 && !c.ResultEnum.IsHit()) || (c.IsBuff > 0 && c.Result > 0) || !Keep(c, log))
+                    if ( !(c.SkillId == SkillId) || !c.HasHit || !Keep(c, log))
                     {
                         continue;
                     }
-                    List<CombatItem> hits = log.CombatData.AllCombatItems.Where(x =>
-                    x.IsStateChange == ParseEnum.StateChange.Normal && x.IsActivation == ParseEnum.Activation.None && x.IsBuffRemove == ParseEnum.BuffRemove.None
-                    && ((x.IsBuff != 0 && x.Value == 0) || (x.IsBuff == 0))
-                    && x.SrcInstid == c.SrcInstid
-                    && agentData.GetAgent(x.DstAgent, x.Time).Type == AgentItem.AgentType.Player
-                    ).ToList();
 
-                    if (hits.First() != c) continue;
+                    if (GetFirstHit(c.From, log) != c) continue;
 
-                    mechanicLogs[this].Add(new MechanicLog(log.FightData.ToFightSpace(c.Time), this, p));
+                    mechanicLogs[this].Add(new MechanicEvent(c.Time, this, p));
                 }
             }
         }
