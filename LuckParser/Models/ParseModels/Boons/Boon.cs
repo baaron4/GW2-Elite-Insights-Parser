21b0e06263ad564efc58137c7af8e6bcb001e41d
EliphasNUIT <bichon.jekfer@gmail.com>
2019-06-13 10:58:32 +0200

    added ion shield to boon simulatir

--- LuckParser/Models/ParseModels/Boons/Boon.cs
+++ LuckParser/Models/ParseModels/Boons/Boon.cs
@@ -246,6 +246,7 @@ namespace LuckParser.Models.ParseModels
                 new Boon("Not the Bees!", 34434, BoonSource.Enemy, BoonType.Duration, 1, BoonNature.GraphOnlyBuff, Logic.Override,"https://wiki.guildwars2.com/images/0/08/Throw_Jar.png"),
                 new Boon("Targeted", 34392, BoonSource.Enemy, BoonType.Duration, 1, BoonNature.GraphOnlyBuff, Logic.Override,"https://wiki.guildwars2.com/images/2/24/Targeted.png"),
                 new Boon("Pillar Pandemonium", 56204, BoonSource.Enemy, BoonType.Intensity, 99, BoonNature.GraphOnlyBuff, Logic.Override,"https://wiki.guildwars2.com/images/2/24/Targeted.png"),
+                new Boon("Ion Shield", 56100, BoonSource.Enemy, BoonType.Intensity, 99, BoonNature.GraphOnlyBuff, Logic.Override,"https://wiki.guildwars2.com/images/2/24/Targeted.png"),
         };
 
         private readonly static List<Boon> _revenant = new List<Boon>
