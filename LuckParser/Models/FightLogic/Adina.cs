862611e9ebe909ad8cd700bef6527ee0d31697fd
EliphasNUIT <bichon.jekfer@gmail.com>
2019-06-22 11:20:29 +0200

    mechanics polishing

--- LuckParser/Models/FightLogic/Adina.cs
+++ LuckParser/Models/FightLogic/Adina.cs
@@ -18,10 +18,10 @@ namespace LuckParser.Models.Logic
                 new PlayerBoonApplyMechanic(56440, "Eroding Curse", new MechanicPlotlySetting("cross","rgb(255,0,100)"), "Curse", "Stacking damage debuff from Hand of Erosion", "Eroding Curse", 0),
                 new HitOnPlayerMechanic(56648, "Boulder Barrage", new MechanicPlotlySetting("square","rgb(255,0,0)"), "Boulder", "Hit by boulder thrown during pillars", "Boulder Barrage", 0),
                 new HitOnPlayerMechanic(56390, "Perilous Pulse", new MechanicPlotlySetting("x","rgb(255,0,0)"), "Perilous Pulse", "Perilous Pulse", "Perilous Pulse", 0),
-                new HitOnPlayerMechanic(56141, "Stalagmites", new MechanicPlotlySetting("x","rgb(255,0,0)"), "Mines", "Hit by mines", "Mines", 0),
+                new HitOnPlayerMechanic(56141, "Stalagmites", new MechanicPlotlySetting("hexagon","rgb(255,0,0)"), "Mines", "Hit by mines", "Mines", 0),
                 new HitOnPlayerMechanic(56114, "Diamond Palisade", new MechanicPlotlySetting("x","rgb(255,150,0)"), "Eye", "Looked at Eye", "Looked at Eye", 0),
-                new HitOnPlayerMechanic(56035, "Quantum Quake", new MechanicPlotlySetting("circle","rgb(255,150,0)"), "S.Thrower", "Hit by rotating SandThrower", "SandThrower", 0),
-                new HitOnPlayerMechanic(56381, "Quantum Quake", new MechanicPlotlySetting("circle","rgb(255,150,0)"), "S.Thrower", "Hit by rotating SandThrower", "SandThrower", 0),
+                new HitOnPlayerMechanic(56035, "Quantum Quake", new MechanicPlotlySetting("diamond","rgb(255,150,0)"), "S.Thrower", "Hit by rotating SandThrower", "SandThrower", 0),
+                new HitOnPlayerMechanic(56381, "Quantum Quake", new MechanicPlotlySetting("diamond","rgb(255,150,0)"), "S.Thrower", "Hit by rotating SandThrower", "SandThrower", 0),
             });
             Extension = "adina";
             IconUrl = "https://wiki.guildwars2.com/images/a/a0/Mini_Earth_Djinn.png";
