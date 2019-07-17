8499572eaf40e6ee19feeabe1cd28733b5216b08
Jekfer Bichon <jekfer.bichon@3ds.com>
2019-07-17 16:10:40 +0200

    added possibility to track boons on players on damage modifier target

--- LuckParser/EIData/DamageModifiers/BuffDamageModifierTarget.cs
+++ LuckParser/EIData/DamageModifiers/BuffDamageModifierTarget.cs
@@ -8,6 +8,18 @@ namespace LuckParser.EIData
 {
     public class BuffDamageModifierTarget : BuffDamageModifier
     {
+        private readonly BuffsTracker _trackerPlayer = null;
+        private readonly GainComputer _gainComputerPlayer = null;
+
+        protected double ComputeGainPlayer(int stack, AbstractDamageEvent dl)
+        {
+            if (DLChecker != null && !DLChecker(dl))
+            {
+                return -1.0;
+            }
+            double gain = _gainComputerPlayer.ComputeGain(1.0, stack);
+            return gain > 0.0 ? 1.0 : -1.0;
+        }
 
         public BuffDamageModifierTarget(long id, string name, string tooltip, DamageSource damageSource, double gainPerStack, DamageType srctype, DamageType compareType, ModifierSource src, GainComputer gainComputer, string url, DamageLogChecker dlChecker = null) : base(id, name, tooltip, damageSource, gainPerStack, srctype, compareType, src, gainComputer, url, dlChecker)
         {
@@ -25,14 +37,46 @@ namespace LuckParser.EIData
         {
         }
 
+        public BuffDamageModifierTarget(long id, long playerId, string name, string tooltip, DamageSource damageSource, double gainPerStack, DamageType srctype, DamageType compareType, ModifierSource src, GainComputer gainComputer, GainComputer gainComputerPlayer, string url, DamageLogChecker dlChecker = null) : this(id, name, tooltip, damageSource, gainPerStack, srctype, compareType, src, gainComputer, url, dlChecker)
+        {
+            _trackerPlayer = new BuffsTrackerSingle(playerId);
+            _gainComputerPlayer = gainComputerPlayer;
+        }
+
+        public BuffDamageModifierTarget(long id, long[] playerIds, string name, string tooltip, DamageSource damageSource, double gainPerStack, DamageType srctype, DamageType compareType, ModifierSource src, GainComputer gainComputer, GainComputer gainComputerPlayer, string url, ulong minBuild, ulong maxBuild, DamageLogChecker dlChecker = null) : this(id, name, tooltip, damageSource, gainPerStack, srctype, compareType, src, gainComputer, url, minBuild, maxBuild, dlChecker)
+        {
+            _trackerPlayer = new BuffsTrackerMulti(new List<long>(playerIds));
+            _gainComputerPlayer = gainComputerPlayer;
+        }
+
+        public BuffDamageModifierTarget(long[] ids, long playerId, string name, string tooltip, DamageSource damageSource, double gainPerStack, DamageType srctype, DamageType compareType, ModifierSource src, GainComputer gainComputer, GainComputer gainComputerPlayer, string url, DamageLogChecker dlChecker = null) : this(ids, name, tooltip, damageSource, gainPerStack, srctype, compareType, src, gainComputer, url, dlChecker)
+        {
+            _trackerPlayer = new BuffsTrackerSingle(playerId);
+            _gainComputerPlayer = gainComputerPlayer;
+        }
+
+        public BuffDamageModifierTarget(long[] ids, long[] playerIds, string name, string tooltip, DamageSource damageSource, double gainPerStack, DamageType srctype, DamageType compareType, ModifierSource src, GainComputer gainComputer, GainComputer gainComputerPlayer, string url, ulong minBuild, ulong maxBuild, DamageLogChecker dlChecker = null) : this(ids, name, tooltip, damageSource, gainPerStack, srctype, compareType, src, gainComputer, url, minBuild, maxBuild, dlChecker)
+        {
+            _trackerPlayer = new BuffsTrackerMulti(new List<long>(playerIds));
+            _gainComputerPlayer = gainComputerPlayer;
+        }
+
 
         public override void ComputeDamageModifier(Dictionary<string, List<DamageModifierData>> data, Dictionary<Target, Dictionary<string, List<DamageModifierData>>> dataTarget, Player p, ParsedLog log)
         {
             List<PhaseData> phases = log.FightData.GetPhases(log);
+            Dictionary<long, BoonsGraphModel> bgmsP = p.GetBoonGraphs(log);
+            if (_trackerPlayer != null)
+            {
+                if (!_trackerPlayer.Has(bgmsP) && _gainComputerPlayer != ByAbsence)
+                {
+                    return;
+                }
+            }
             foreach (Target target in log.FightData.Logic.Targets)
             {
                 Dictionary<long, BoonsGraphModel> bgms = target.GetBoonGraphs(log);
-                if (!Tracker.Has(bgms))
+                if (!Tracker.Has(bgms) && GainComputer != ByAbsence)
                 {
                     continue;
                 }
@@ -48,7 +92,26 @@ namespace LuckParser.EIData
                     {
                         int totalDamage = GetTotalDamage(p, log, target, i);
                         List<AbstractDamageEvent> typedHits = GetDamageLogs(p, log, target, phases[i]);
-                        List<double> damages = typedHits.Select(x => ComputeGain(Tracker.GetStack(bgms, x.Time), x)).Where(x => x != -1.0).ToList();
+                        List<double> damages;
+                        if (_trackerPlayer != null)
+                        {
+                            damages = typedHits.Select(x =>
+                            {
+
+                                if (ComputeGainPlayer(_trackerPlayer.GetStack(bgmsP, x.Time), x) < 0.0)
+                                {
+                                    return -1.0;
+                                }
+                                return ComputeGain(Tracker.GetStack(bgms, x.Time), x);
+                            }).Where(x => x != -1.0).ToList();
+                        }
+                        else
+                        {
+                            damages = typedHits.Select(x =>
+                            {
+                                return ComputeGain(Tracker.GetStack(bgms, x.Time), x);
+                            }).Where(x => x != -1.0).ToList();
+                        }
                         extraDataList.Add(new DamageModifierData(damages.Count, typedHits.Count, damages.Sum(), totalDamage));
                     }
                     dict[Name] = extraDataList;
