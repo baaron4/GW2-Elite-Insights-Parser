28440abfa37c6c2cb657d64e2d0a11bf24509f1c
Jekfer Bichon <jekfer.bichon@3ds.com>
2019-11-04 13:14:50 +0100

    updated version

--- GW2EIParser/Properties/AssemblyInfo.cs
+++ GW2EIParser/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6135677e-6c0e-42de-925e-1995e52e5ac5")]
 #if !DEBUG
-[assembly: AssemblyVersion("2.18.1.0")]
+[assembly: AssemblyVersion("2.19.0.0")]
 #else
-[assembly: AssemblyVersion("2.18.*")]
+[assembly: AssemblyVersion("2.19.*")]
 #endif
 //[assembly: AssemblyFileVersion("1.8.10.34877")]
