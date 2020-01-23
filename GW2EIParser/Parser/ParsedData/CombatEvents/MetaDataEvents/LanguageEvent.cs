c8ab0e095e3680a491a6028ed764171d51ed58e9
EliphasNUIT <bichon.jekfer@gmail.com>
2020-01-24 00:59:04 +0100

    added some utilitiy to LanguageEvent

--- GW2EIParser/Parser/ParsedData/CombatEvents/MetaDataEvents/LanguageEvent.cs
+++ GW2EIParser/Parser/ParsedData/CombatEvents/MetaDataEvents/LanguageEvent.cs
@@ -2,7 +2,7 @@
 {
     public class LanguageEvent : AbstractMetaDataEvent
     {
-        public enum LanguageEnum : ulong { English = 0, Missing = 1, French = 2, Germah = 3, Spanish = 4, Unknown = 5 }
+        public enum LanguageEnum : ulong { English = 0, Missing = 1, French = 2, German = 3, Spanish = 4, Unknown = 5 }
 
         public LanguageEnum Language { get; }
 
@@ -10,6 +10,24 @@
         {
             Language = evtcItem.SrcAgent < (ulong)LanguageEnum.Unknown ? (LanguageEnum)evtcItem.SrcAgent
                 : LanguageEnum.Unknown;
+        }
+
+        public override string ToString()
+        {
+            switch (Language)
+            {
+                case LanguageEnum.English:
+                    return "English";
+                case LanguageEnum.Missing:
+                    return "Missing";
+                case LanguageEnum.French:
+                    return "French";
+                case LanguageEnum.German:
+                    return "German";
+                case LanguageEnum.Spanish:
+                    return "Spanish";
+            }
+            return "Unknown";
         }
 
     }
