9e28a8914287e7965a9c82498d2acb400e8db0a4
EliphasNUIT <bichon.jekfer@gmail.com>
2020-01-26 21:03:37 +0100

    improved exception message for new GetFIghtOffsetByFirstInvulFilter method

--- GW2EIParser/FightLogic/Fractals/FractalLogic.cs
+++ GW2EIParser/FightLogic/Fractals/FractalLogic.cs
@@ -58,7 +58,7 @@ namespace GW2EIParser.Logic
             AgentItem target = agentData.GetNPCsByID(targetID).FirstOrDefault();
             if (target == null)
             {
-                throw new InvalidOperationException("Main target of the fight not found");
+                throw new InvalidOperationException("Error Encountered: Main target of the fight not found");
             }
             // enter combat
             CombatItem invulLost = combatData.FirstOrDefault(x => x.DstAgent == target.Agent && x.IsStateChange == ParseEnum.StateChange.None && x.IsBuffRemove != ParseEnum.BuffRemove.None && x.SkillID == invulID);
