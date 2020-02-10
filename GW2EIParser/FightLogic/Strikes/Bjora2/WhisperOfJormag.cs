62087626d9bd40dd399be3d90760cbf0554dca3a
Jekfer Bichon <jekfer.bichon@3ds.com>
2020-02-10 16:31:32 +0100

    added mechanics for WoJ and correction Echo's id

--- GW2EIParser/FightLogic/Strikes/Bjora2/WhisperOfJormag.cs
+++ GW2EIParser/FightLogic/Strikes/Bjora2/WhisperOfJormag.cs
@@ -14,6 +14,16 @@ namespace GW2EIParser.Logic
         {
             MechanicList.AddRange(new List<Mechanic>
             {
+                new HitOnPlayerMechanic(59441, "Chains of Frost Hit", new MechanicPlotlySetting("diamond-tall","rgb(255,0,0)"), "H.Chains","Hit by Chains of Frost", "Chains of Frost",50),
+                new HitOnPlayerMechanic(59102, "Spreading Ice (Own)", new MechanicPlotlySetting("circle","rgb(255,125,0)"), "S.Ice","Hit by own spreading ice", "Spreading Ice (Own)",50),
+                new HitOnPlayerMechanic(59468, "Spreading Ice (Others)", new MechanicPlotlySetting("triangle","rgb(255,150,0)"), "S.Ice.O","Hit by other's spreading ice", "Spreading Ice (Others)",50),
+                new HitOnPlayerMechanic(59076, "Icy Slice", new MechanicPlotlySetting("hexagram","rgb(255,100,0)"), "I.Slice","Hit by Icy Slice", "Icy Slice",50),
+                new HitOnPlayerMechanic(59255, "Ice Tempest", new MechanicPlotlySetting("square","rgb(255,100,0)"), "I.Tornado","Hit by Ice Tornadoes", "Ice Tempest",50),
+                new PlayerBuffApplyMechanic(59120, "Chains of Frost", new MechanicPlotlySetting("circle","rgb(0,0,255)"), "F.Chains","Selected for Chains of Frost", "Chains of Frost",500),
+                new PlayerBuffRemoveMechanic(59054, "Teleport Back", new MechanicPlotlySetting("circle","rgb(0,125,255)"), "TP In","Teleported back to the arena", "Teleport Back",500),
+                new PlayerBuffRemoveMechanic(59223, "Teleport Out", new MechanicPlotlySetting("circle-open","rgb(0,125,255)"), "TP Out","Teleported outside of the arena", "Teleport Out",500),
+                new EnemyCastStartMechanic(59102, "Spreading Ice", new MechanicPlotlySetting("hexagram","rgb(255,0,125)"), "S.Ice.C","Cast Spreading Ice", "Cast Spreading Ice",0),
+                new EnemyCastStartMechanic(59159, "Chains of Frost", new MechanicPlotlySetting("hexagram","rgb(255,125,125)"), "F.Chains.C","Cast Chains of Frost", "Cast Chains of Frost",0),
             }
             );
             Extension = "woj";
