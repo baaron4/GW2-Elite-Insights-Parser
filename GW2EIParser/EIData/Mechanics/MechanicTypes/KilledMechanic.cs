6650442acdb2a7e9003a649816f875ca3e79d2c8
EliphasNUIT <bichon.jekfer@gmail.com>
2020-03-07 19:58:28 +0100

    reduced qadim mechanics table names + added a pyre guardian kill count

---
+++ GW2EIParser/EIData/Mechanics/MechanicTypes/KilledMechanic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GW2EIParser.Parser.ParsedData;
+using GW2EIParser.Parser.ParsedData.CombatEvents;
+
+namespace GW2EIParser.EIData
+{
+
+    public class KilledMechanic : Mechanic
+    {
+
+        public KilledMechanic(long skillId, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, int internalCoolDown) : this(skillId, inGameName, plotlySetting, shortName, shortName, shortName, internalCoolDown)
+        {
+        }
+
+        public KilledMechanic(long skillId, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown) : base(skillId, inGameName, plotlySetting, shortName, description, fullName, internalCoolDown)
+        {
+            IsEnemyMechanic = true;
+        }
+
+        public override void CheckMechanic(ParsedLog log, Dictionary<Mechanic, List<MechanicEvent>> mechanicLogs, Dictionary<ushort, AbstractSingleActor> regroupedMobs)
+        {
+            CombatData combatData = log.CombatData;
+            foreach (AgentItem a in log.AgentData.GetAgentByType(AgentItem.AgentType.NPC).Where(x => x.ID == SkillId))
+            {
+                if (!regroupedMobs.TryGetValue(a.ID, out AbstractSingleActor amp))
+                {
+                    amp = log.FindActor(a, false);
+                    regroupedMobs.Add(amp.ID, amp);
+                }
+                foreach (DeadEvent devt in log.CombatData.GetDeadEvents(a))
+                {
+                    mechanicLogs[this].Add(new MechanicEvent(devt.Time, this, amp));
+                }
+            }
+        }
+    }
+}
