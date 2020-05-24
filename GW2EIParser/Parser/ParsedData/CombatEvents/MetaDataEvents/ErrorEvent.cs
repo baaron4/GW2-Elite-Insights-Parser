535b69a796880762d0adcd532d785e05821132dc
EliphasNUIT <bichon.jekfer@gmail.com>
2020-05-24 20:07:14 +0200

    fixed error message size, sizeof(char) between C and C# are not the same

--- GW2EIParser/Parser/ParsedData/CombatEvents/MetaDataEvents/ErrorEvent.cs
+++ GW2EIParser/Parser/ParsedData/CombatEvents/MetaDataEvents/ErrorEvent.cs
@@ -7,109 +7,30 @@ namespace GW2EIParser.Parser.ParsedData.CombatEvents
         public string Message { get; }
         public ErrorEvent(CombatItem evtcItem) : base(evtcItem)
         {
-            byte[] bytes = new byte[32 * sizeof(char)];
+            byte[] bytes = new byte[32 * 1]; // 32 of sizeof(char), char as in C not C#
             int offset = 0;
+            // 8 bytes
             foreach (byte bt in BitConverter.GetBytes(evtcItem.Time))
             {
                 bytes[offset++] = bt;
             }
+            // 8 bytes
             foreach (byte bt in BitConverter.GetBytes(evtcItem.SrcAgent))
             {
                 bytes[offset++] = bt;
             }
+            // 8 bytes
             foreach (byte bt in BitConverter.GetBytes(evtcItem.DstAgent))
             {
                 bytes[offset++] = bt;
             }
+            // 4 bytes
             foreach (byte bt in BitConverter.GetBytes(evtcItem.Value))
             {
                 bytes[offset++] = bt;
             }
+            // 4 bytes
             foreach (byte bt in BitConverter.GetBytes(evtcItem.BuffDmg))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.OverstackValue))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.SkillID))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.SrcInstid))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.DstInstid))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.SrcMasterInstid))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.DstMasterInstid))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes((byte)evtcItem.IFFEnum))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.IsBuff))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes((byte)evtcItem.IFFEnum))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.IsBuff))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.Result))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.IsActivation))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.IsBuffRemove))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.IsNinety))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.IsFifty))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.IsMoving))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.IsStateChange))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.IsFlanking))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.IsShields))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.IsOffcycle))
-            {
-                bytes[offset++] = bt;
-            }
-            foreach (byte bt in BitConverter.GetBytes(evtcItem.Pad))
             {
                 bytes[offset++] = bt;
             }
