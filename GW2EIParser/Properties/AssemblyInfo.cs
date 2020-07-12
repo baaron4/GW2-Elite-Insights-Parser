b2abc38f37fb144392c343333bc2c3906b2ffb9f
EliphasNUIT <16018765+EliphasNUIT@users.noreply.github.com>
2020-07-12 13:01:47 +0200

    Update AssemblyInfo.cs

--- GW2EIParser/Properties/AssemblyInfo.cs
+++ GW2EIParser/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6135677e-6c0e-42de-925e-1995e52e5ac5")]
 #if !DEBUG
-[assembly: AssemblyVersion("2.25.0.0")]
+[assembly: AssemblyVersion("2.26.0.0")]
 #else
-[assembly: AssemblyVersion("2.25.*")]
+[assembly: AssemblyVersion("2.26.*")]
 #endif
 //[assembly: AssemblyFileVersion("1.8.10.34877")]
