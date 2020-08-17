73a791af22d9d4ec6f9fe054c71c3ceacac09722
EliphasNUIT <bichon.jekfer@gmail.com>
2020-08-17 16:56:01 +0200

    fixed flame rush and flame surge

--- GW2EIEvtcParser/EIData/ProfHelpers/Guardian/FirebrandHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Guardian/FirebrandHelper.cs
@@ -10,10 +10,10 @@ namespace GW2EIEvtcParser.EIData
     {
         internal static readonly List<InstantCastFinder> FirebrandInstantCastFinders = new List<InstantCastFinder>()
         {
-            new DamageCastFinder(45082,46618,InstantCastFinder.DefaultICD), // Flame Rush
-            new DamageCastFinder(42924,46616,InstantCastFinder.DefaultICD), // Flame Surge
-            new DamageCastFinder(42360,42360,InstantCastFinder.DefaultICD), // Echo of Truth
-            new DamageCastFinder(44008,44008,InstantCastFinder.DefaultICD), // Voice of Truth
+            new DamageCastFinder(46618,46618,InstantCastFinder.DefaultICD), // Flame Rush
+            new DamageCastFinder(46616,46616,InstantCastFinder.DefaultICD), // Flame Surge
+            //new DamageCastFinder(42360,42360,InstantCastFinder.DefaultICD), // Echo of Truth
+            //new DamageCastFinder(44008,44008,InstantCastFinder.DefaultICD), // Voice of Truth
         };
     }
 }
