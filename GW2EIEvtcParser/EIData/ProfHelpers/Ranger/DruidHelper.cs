976de205710632aa56843cdd5462c784abfccee6
EliphasNUIT <bichon.jekfer@gmail.com>
2020-08-17 16:48:10 +0200

    added guardian instant cast

--- GW2EIEvtcParser/EIData/ProfHelpers/Ranger/DruidHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Ranger/DruidHelper.cs
@@ -8,6 +8,8 @@ namespace GW2EIEvtcParser.EIData
     {
         internal static readonly List<InstantCastFinder> DruidInstantCastFinders = new List<InstantCastFinder>()
         {
+            new BuffGainCastFinder(31869,31508,InstantCastFinder.DefaultICD), // Celestial Avatar
+            new BuffLossCastFinder(31411,31508,InstantCastFinder.DefaultICD), // Release Celestial Avatar
         };
     }
 }
--- GW2EIEvtcParser/EIData/ProfHelpers/Ranger/RangerHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Ranger/RangerHelper.cs
@@ -1,4 +1,5 @@
 ﻿using GW2EIEvtcParser.ParsedData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,7 +10,15 @@ namespace GW2EIEvtcParser.EIData
 
         internal static readonly List<InstantCastFinder> RangerInstantCastFinders = new List<InstantCastFinder>()
         {
+            new DamageCastFinder(12573,12573,InstantCastFinder.DefaultICD), // Hunter's Shot
+            new DamageCastFinder(12507,12507,InstantCastFinder.DefaultICD), // Crippling Shot
+            new BuffGiveCastFinder(33902,12633,InstantCastFinder.DefaultICD), // "Sic 'Em!"
+            new BuffGiveCastFinder(56923,12633,InstantCastFinder.DefaultICD), // "Sic 'Em!" PvP
+            new BuffGainCastFinder(12500,12543,InstantCastFinder.DefaultICD, (evt, combatData) => Math.Abs(evt.AppliedDuration - 6000) < ParserHelper.ServerDelayConstant), // Signet of Stone
+            new BuffGainCastFinder(42470,12543,InstantCastFinder.DefaultICD, (evt, combatData) => Math.Abs(evt.AppliedDuration - 5000) < ParserHelper.ServerDelayConstant), // Lesser Signet of Stone
+            new BuffGainCastFinder(12537,12536,InstantCastFinder.DefaultICD), // Sharpening Stone
         };
+
         public static void AttachMasterToRangerGadgets(List<Player> players, Dictionary<long, List<AbstractDamageEvent>> damageData, Dictionary<long, List<AbstractCastEvent>> castData)
         {
             var playerAgents = new HashSet<AgentItem>(players.Select(x => x.AgentItem));
