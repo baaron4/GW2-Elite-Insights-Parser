26de07e1e4802b67bc449c8fcfda2d84a33fbff4
EliphasNUIT <bichon.jekfer@gmail.com>
2020-09-08 15:58:33 +0200

    groundwork for sunqua peak

---
+++ GW2EIEvtcParser/EncounterLogic/Fractals/SunquaPeak/SunquaPeak.cs
@@ -0,0 +1,15 @@
+﻿using GW2EIEvtcParser.EIData;
+using GW2EIEvtcParser.ParsedData;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GW2EIEvtcParser.EncounterLogic
+{
+    internal abstract class SunquaPeak : FractalLogic
+    {
+        public SunquaPeak(int triggerID) : base(triggerID)
+        {
+
+        }
+    }
+}
