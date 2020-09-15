10ae476c3f5dbbe303583e8e5bb14db629488aff
EliphasNUIT <bichon.jekfer@gmail.com>
2020-09-15 14:12:41 +0200

    added Pure Strike damage mod for spellbreakers

--- GW2EIEvtcParser/EIData/ProfHelpers/Warrior/SpellbreakerHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Warrior/SpellbreakerHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using static GW2EIEvtcParser.ArcDPSEnums;
 using static GW2EIEvtcParser.EIData.Buff;
+using static GW2EIEvtcParser.EIData.DamageModifier;
 
 namespace GW2EIEvtcParser.EIData
 {
@@ -16,7 +17,8 @@ namespace GW2EIEvtcParser.EIData
 
         internal static readonly List<DamageModifier> DamageMods = new List<DamageModifier>
         {
-
+            new BuffDamageModifierTarget(NumberOfBoonsID, "Pure Strike (boons)", "7% crit damage", DamageSource.NoPets, 7.0, DamageType.Power, DamageType.All, ParserHelper.Source.Spellbreaker, ByPresence, "https://wiki.guildwars2.com/images/7/76/Pure_Strike_%28trait%29.png", DamageModifierMode.All, (dEvt) => dEvt.HasCrit),
+            new BuffDamageModifierTarget(NumberOfBoonsID, "Pure Strike (no boons)", "14% crit damage", DamageSource.NoPets, 14.0, DamageType.Power, DamageType.All, ParserHelper.Source.Spellbreaker, ByAbsence, "https://wiki.guildwars2.com/images/7/76/Pure_Strike_%28trait%29.png", DamageModifierMode.All, (dEvt) => dEvt.HasCrit),
         };
 
         internal static readonly List<Buff> Buffs = new List<Buff>
