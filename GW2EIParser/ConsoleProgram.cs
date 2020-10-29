ffe1bbc94f9d2933acc15b421ca56973fb171d92
EliphasNUIT <bichon.jekfer@gmail.com>
2020-10-29 15:13:08 +0100

    multi log parsing organize input files by size to evenly distribute tasks

--- GW2EIParser/ConsoleProgram.cs
+++ GW2EIParser/ConsoleProgram.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GW2EIParser.Exceptions;
@@ -8,22 +9,31 @@ namespace GW2EIParser
 {
     internal class ConsoleProgram
     {
-        // https://stackoverflow.com/questions/11463734/split-a-list-into-smaller-lists-of-n-size
-        private static IEnumerable<List<T>> SplitList<T>(List<T> locations, int nSize)
-        {
-            for (int i = 0; i < locations.Count; i += nSize)
-            {
-                yield return locations.GetRange(i, Math.Min(nSize, locations.Count - i));
-            }
-        }
 
         public ConsoleProgram(IEnumerable<string> logFiles)
         {
             if (Properties.Settings.Default.ParseMultipleLogs)
             {
-                var logList = new List<string>(logFiles);
-                int splitSize = (int)Math.Ceiling((double)logList.Count / ProgramHelper.GetMaxParallelRunning());
-                IEnumerable<List<string>> splitLogFiles = SplitList(logList, splitSize);
+                var splitLogFiles = new List<List<string>>();
+                var sizeSortedLogFiles = new List<string>(logFiles);
+                for (int i = 0; i < ProgramHelper.GetMaxParallelRunning(); i++)
+                {
+                    splitLogFiles.Add(new List<string>());
+                }
+                sizeSortedLogFiles.Sort((x, y) =>
+                {
+                    var fInfoX = new FileInfo(x);
+                    var xValue = fInfoX.Exists ? fInfoX.Length : 0;
+                    var fInfoY = new FileInfo(y);
+                    var yValue = fInfoY.Exists ? fInfoY.Length : 0;
+                    return xValue.CompareTo(yValue);
+                });
+                int index = 0;
+                foreach(string file in sizeSortedLogFiles)
+                {
+                    splitLogFiles[index].Add(file);
+                    index = (index + 1) % ProgramHelper.GetMaxParallelRunning();
+                }
                 Parallel.ForEach(splitLogFiles, files => {
                     foreach(string file in files)
                     {
