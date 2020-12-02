45e07a82ed0d39f6ecfa7f7bbd9d8c397052038d
EliphasNUIT <bichon.jekfer@gmail.com>
2020-12-03 00:34:49 +0100

    removed useless constructor

--- GW2EIParser/Exceptions/EncompassException.cs
+++ GW2EIParser/Exceptions/EncompassException.cs
@@ -5,10 +5,6 @@ namespace GW2EIParser.Exceptions
     public class EncompassException : Exception
     {
 
-        internal EncompassException() : base("Operation aborted")
-        {
-        }
-
         internal EncompassException(Exception inner) : base("Operation aborted", inner)
         {
         }
