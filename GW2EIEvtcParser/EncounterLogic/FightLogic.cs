a80d059a58efacff60948c10dc489496dff9c147
EliphasNUIT <bichon.jekfer@gmail.com>
2020-12-13 19:29:27 +0100

    added SpecialCastEventProcess to FightLogic

--- GW2EIEvtcParser/EncounterLogic/FightLogic.cs
+++ GW2EIEvtcParser/EncounterLogic/FightLogic.cs
@@ -340,6 +340,11 @@ namespace GW2EIEvtcParser.EncounterLogic
             }
         }
 
+        internal virtual List<AbstractCastEvent> SpecialCastEventProcess(Dictionary<AgentItem, List<AnimatedCastEvent>> animatedCastEvent, Dictionary<AgentItem, List<WeaponSwapEvent>> weaponSwapData, Dictionary<long, List<AbstractCastEvent>> _castDataById, SkillData skillData)
+        {
+            return new List<AbstractCastEvent>();
+        }
+
         internal virtual List<AbstractHealthDamageEvent> SpecialDamageEventProcess(Dictionary<AgentItem, List<AbstractHealthDamageEvent>> damageBySrc, Dictionary<AgentItem, List<AbstractHealthDamageEvent>> damageByDst, Dictionary<long, List<AbstractHealthDamageEvent>> damageById, SkillData skillData)
         {
             return new List<AbstractHealthDamageEvent>();
