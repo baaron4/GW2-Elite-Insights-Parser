e248aed5457b521f527b707ff7c5ae5b13ac9588
EliphasNUIT <bichon.jekfer@gmail.com>
2021-05-08 18:33:29 +0200

    removed useless condition

--- GW2EIEvtcParser/EIData/DamageModifiers/GainComputers/GainComputerByMultiplyingStack.cs
+++ GW2EIEvtcParser/EIData/DamageModifiers/GainComputers/GainComputerByMultiplyingStack.cs
@@ -11,10 +11,6 @@ namespace GW2EIEvtcParser.EIData
 
         public override double ComputeGain(double gainPerStack, int stack)
         {
-            if (stack == 0)
-            {
-                return 0.0;
-            }
             var pow = 100.0 * Math.Pow(1.0 + gainPerStack/100.0, stack) - 100.0;
             return pow / (100 + pow);
         }
