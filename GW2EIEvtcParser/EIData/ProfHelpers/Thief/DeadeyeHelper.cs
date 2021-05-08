f1a9606d1efd00c61d4d67c69c2adba15b30196c
EliphasNUIT <bichon.jekfer@gmail.com>
2021-05-08 17:20:31 +0200

    added iron sight to Deadeye

--- GW2EIEvtcParser/EIData/ProfHelpers/Thief/DeadeyeHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Thief/DeadeyeHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using GW2EIEvtcParser.ParsedData;
 using static GW2EIEvtcParser.EIData.Buff;
 using static GW2EIEvtcParser.EIData.DamageModifier;
 using static GW2EIEvtcParser.ParserHelper;
@@ -15,6 +17,15 @@ namespace GW2EIEvtcParser.EIData
         internal static readonly List<DamageModifier> DamageMods = new List<DamageModifier>
         {
             new BuffDamageModifier(NumberOfBoonsID, "Premeditation", "1% per boon",DamageSource.NoPets, 1.0, DamageType.Power, DamageType.All, Source.Deadeye, ByStack, "https://wiki.guildwars2.com/images/d/d7/Premeditation.png", DamageModifierMode.All),
+            new BuffDamageModifier(46333, "Iron Sight", "10% to marked target", DamageSource.NoPets, 10.0, DamageType.Power, DamageType.All, Source.Deadeye, ByPresence, "https://wiki.guildwars2.com/images/d/dd/Iron_Sight.png", DamageModifierMode.All, (x, log) => {
+                AgentItem src = x.From;
+                AbstractBuffEvent effectApply = log.CombatData.GetBuffData(46333).Where(y => y is BuffApplyEvent && y.To == src).LastOrDefault(y => y.Time <= x.Time);
+                if (effectApply != null)
+                {
+                    return x.To == effectApply.By;
+                }
+                return false;
+            }),
         };
 
 
