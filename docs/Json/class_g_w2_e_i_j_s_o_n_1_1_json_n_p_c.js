d6c955346e38b275922c145a7e9c2a12ed869b9b
EliphasNUIT <bichon.jekfer@gmail.com>
2021-06-26 15:32:36 +0200

    updated json doc

--- docs/Json/class_g_w2_e_i_j_s_o_n_1_1_json_n_p_c.js
+++ docs/Json/class_g_w2_e_i_j_s_o_n_1_1_json_n_p_c.js
@@ -2,6 +2,7 @@ var class_g_w2_e_i_j_s_o_n_1_1_json_n_p_c =
 [
     [ "BreakbarPercents", "class_g_w2_e_i_j_s_o_n_1_1_json_n_p_c.html#a6c433909653ce73f4349751b86ae6d44", null ],
     [ "Buffs", "class_g_w2_e_i_j_s_o_n_1_1_json_n_p_c.html#a1382a9372056cffa8a08a8b770590cf3", null ],
+    [ "EnemyPlayer", "class_g_w2_e_i_j_s_o_n_1_1_json_n_p_c.html#aaccb118342bb867a368256c5a8d90850", null ],
     [ "FinalHealth", "class_g_w2_e_i_j_s_o_n_1_1_json_n_p_c.html#af9285ee9e50a2236809e9eeba28a8ab2", null ],
     [ "FirstAware", "class_g_w2_e_i_j_s_o_n_1_1_json_n_p_c.html#a542d10d476c183c453814065da6f6223", null ],
     [ "HealthPercentBurned", "class_g_w2_e_i_j_s_o_n_1_1_json_n_p_c.html#ab0842d6f5fec2ca25764c9fc771bdaa6", null ],
