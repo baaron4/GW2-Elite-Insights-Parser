396d4a177da6d6d12eec2f21374aea1665282671
EliphasNUIT <bichon.jekfer@gmail.com>
2021-07-02 18:06:31 +0200

    added extension helper and abstract extension handler

---
+++ GW2EIEvtcParser/ParserHelpers/Extensions/ExtensionHandlers/AbstractExtensionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser.Extensions
+{
+    internal abstract class AbstractExtensionHandler
+    {
+        public uint Sig { get; }
+
+        protected AbstractExtensionHandler(uint sig)
+        {
+            Sig = sig;
+        }
+
+        public abstract bool HasTime(CombatItem c);
+        public abstract bool SrcIsAgent(CombatItem c);
+        public abstract bool DstIsAgent(CombatItem c);
+
+        public abstract void InsertEIExtensionEvent(CombatData combatData, CombatItem c);
+
+    }
+}
---
+++ GW2EIEvtcParser/ParserHelpers/Extensions/ExtensionHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GW2EIEvtcParser.Extensions
+{
+    internal static class ExtensionHelper
+    {
+        public static AbstractExtensionHandler GetExtensionHandler(CombatItem c)
+        {
+            if (c.IsStateChange != ArcDPSEnums.StateChange.Extension && c.Pad != 0)
+            {
+                return null;
+            }
+            // place holder for sig
+            switch (c.BuffDmg)
+            {
+                // TODO: based on sig, do a switch on rev
+                default:
+                    break;
+            }
+            return null;
+        }
+    }
+}
