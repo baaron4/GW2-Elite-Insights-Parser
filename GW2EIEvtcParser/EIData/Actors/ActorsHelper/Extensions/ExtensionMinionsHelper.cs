ff54810030cd33dee1da1f032af1744f49d33692
EliphasNUIT <bichon.jekfer@gmail.com>
2021-07-03 17:29:31 +0200

    added extension actor helpers

---
+++ GW2EIEvtcParser/EIData/Actors/ActorsHelper/Extensions/ExtensionMinionsHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GW2EIEvtcParser.Exceptions;
+using GW2EIEvtcParser.ParsedData;
+using static GW2EIEvtcParser.EIData.Buff;
+using static GW2EIEvtcParser.ParserHelper;
+
+namespace GW2EIEvtcParser.EIData
+{
+    public abstract class ExtensionMinionsHelper
+    {
+
+        internal Minions Minions { get; }
+
+        internal IReadOnlyList<NPC> MinionList => Minions.MinionList;
+
+        internal ExtensionMinionsHelper(Minions minions)
+        {
+            Minions = minions;
+        }
+    }
+}
