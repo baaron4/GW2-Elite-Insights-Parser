4135116f14d73e6684dae10faf2b682781a455ec
EliphasNUIT <bichon.jekfer@gmail.com>
2021-07-19 15:37:34 +0200

    artsariiv timing fixes

--- GW2EIEvtcParser/EncounterLogic/Fractals/ShatteredObservatory/ShatteredObservatory.cs
+++ GW2EIEvtcParser/EncounterLogic/Fractals/ShatteredObservatory/ShatteredObservatory.cs
@@ -33,7 +33,7 @@ namespace GW2EIEvtcParser.EncounterLogic
             {
                 return false;
             }
-            List<AbstractBuffEvent> invulsTarget = GetFilteredList(combatData, buffID, target, true);
+            List<AbstractBuffEvent> invulsTarget = GetFilteredList(combatData, buffID, target, true).Where(x => x.Time >= 0).ToList();
             if (invulsTarget.Count == count)
             {
                 AbstractBuffEvent last = invulsTarget.Last();
