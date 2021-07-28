a252675dadc554e006b7adbaef4cf00b04ddfe40
EliphasNUIT <bichon.jekfer@gmail.com>
2021-07-28 14:51:50 +0200

    chaged Water Blast Combo icd

--- GW2EIEvtcParser/EIData/ProfHelpers/ProfHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/ProfHelper.cs
@@ -14,7 +14,7 @@ namespace GW2EIEvtcParser.EIData
             new DamageCastFinder(9433, 9433, 500), // Earth Sigil
             new DamageCastFinder(9292, 9292, 500), // Air Sigil
             new DamageCastFinder(9428, 9428, 500), // Hydro Sigil
-            new EXTHealingCastFinder(12836, 12836, 500), // Water Blast Combo
+            new EXTHealingCastFinder(12836, 12836, EIData.InstantCastFinder.DefaultICD), // Water Blast Combo
         };
 
         internal static void AttachMasterToGadgetByCastData(CombatData combatData, HashSet<AgentItem> gadgets, List<long> castIDS, long castEndThreshold)
