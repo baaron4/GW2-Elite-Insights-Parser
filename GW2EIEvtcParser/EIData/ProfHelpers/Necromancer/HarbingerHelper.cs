9544a2e05b242847bd7e152ab31d4ada569d88f3
EliphasNUIT <bichon.jekfer@gmail.com>
2021-08-18 00:58:44 +0200

    updated harbinger instant cast, damage mods and buffs

--- GW2EIEvtcParser/EIData/ProfHelpers/Necromancer/HarbingerHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Necromancer/HarbingerHelper.cs
@@ -11,18 +11,21 @@ namespace GW2EIEvtcParser.EIData
 
         internal static readonly List<InstantCastFinder> InstantCastFinder = new List<InstantCastFinder>()
         {
+            new BuffGainCastFinder(62567, 59964, EIData.InstantCastFinder.DefaultICD), // Harbinger shroud
+            new BuffLossCastFinder(62540, 59964, EIData.InstantCastFinder.DefaultICD), // Harbinger shroud
         };
 
 
         internal static readonly List<DamageModifier> DamageMods = new List<DamageModifier>
         {
-            new BuffDamageModifier(-1, "Wicked Corruption", "1% per blight stack", DamageSource.NoPets, 1.0, DamageType.Strike, DamageType.All, Source.Harbinger, ByPresence, "", ulong.MaxValue, ulong.MaxValue, DamageModifierMode.All),
-            new BuffDamageModifier(-1, "Septic Corruption", "1% per blight stack", DamageSource.NoPets, 1.0, DamageType.Condition, DamageType.All, Source.Harbinger, ByPresence, "", ulong.MaxValue, ulong.MaxValue, DamageModifierMode.All),
+            new BuffDamageModifier(62653, "Wicked Corruption", "1% per blight stack", DamageSource.NoPets, 1.0, DamageType.Strike, DamageType.All, Source.Harbinger, ByPresence, "https://wiki.guildwars2.com/images/0/00/Wicked_Corruption.png", 118697, ulong.MaxValue, DamageModifierMode.All),
+            new BuffDamageModifier(62653, "Septic Corruption", "1% per blight stack", DamageSource.NoPets, 1.0, DamageType.Condition, DamageType.All, Source.Harbinger, ByPresence, "https://wiki.guildwars2.com/images/f/f7/Septic_Corruption.png", 118697, ulong.MaxValue, DamageModifierMode.All),
         };
 
         internal static readonly List<Buff> Buffs = new List<Buff>
         {
-                new Buff("Blight",-1, Source.Harbinger, BuffStackType.Stacking, 25, BuffNature.GraphOnlyBuff, "", ulong.MaxValue, ulong.MaxValue),
+                new Buff("Harbinger Shroud",59964, Source.Harbinger, BuffNature.GraphOnlyBuff, "https://wiki.guildwars2.com/images/thumb/a/ab/Harbinger_Shroud.png/48px-Harbinger_Shroud.png", 118697, ulong.MaxValue),
+                new Buff("Blight",62653, Source.Harbinger, BuffStackType.Stacking, 25, BuffNature.GraphOnlyBuff, "https://wiki.guildwars2.com/images/a/ac/Blight.png", 118697, ulong.MaxValue),
         };
     }
 }
