65f1919bb0e53969b3dfc70700ac700e99205ce9
EliphasNUIT <bichon.jekfer@gmail.com>
2021-09-22 00:13:38 +0200

    added icons for vindicator buffs

--- GW2EIEvtcParser/EIData/ProfHelpers/Revenant/VindicatorHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Revenant/VindicatorHelper.cs
@@ -10,6 +10,9 @@ namespace GW2EIEvtcParser.EIData
     {
         internal static readonly List<InstantCastFinder> InstantCastFinder = new List<InstantCastFinder>()
         {
+            new BuffGainCastFinder(-1, -1, EIData.InstantCastFinder.DefaultICD), // Legendary Alliance Stance
+            new DamageCastFinder(-1, -1, EIData.InstantCastFinder.DefaultICD), // Call of the Alliance
+            new BuffGainCastFinder(-1, -1, EIData.InstantCastFinder.DefaultICD), // Urn of Saint Viktor
         };
 
         internal static readonly List<DamageModifier> DamageMods = new List<DamageModifier>
@@ -18,8 +21,9 @@ namespace GW2EIEvtcParser.EIData
 
         internal static readonly List<Buff> Buffs = new List<Buff>
         {
-            new Buff("Urn of Saint Viktor", -1, Source.Vindicator, BuffNature.GraphOnlyBuff,"", ulong.MaxValue, ulong.MaxValue),
-            new Buff("Imperial Guard", -1, Source.Vindicator, BuffStackType.Stacking, 25, BuffNature.GraphOnlyBuff, "", ulong.MaxValue, ulong.MaxValue),
+            new Buff("Legendary Alliance Stance",-1, Source.Revenant, BuffNature.GraphOnlyBuff, "https://wiki.guildwars2.com/images/d/d6/Legendary_Alliance_Stance.png", 119939, ulong.MaxValue),
+            new Buff("Urn of Saint Viktor", -1, Source.Vindicator, BuffNature.GraphOnlyBuff,"https://wiki.guildwars2.com/images/f/ff/Urn_of_Saint_Viktor.png", 119939, ulong.MaxValue),
+            new Buff("Imperial Guard", -1, Source.Vindicator, BuffStackType.Stacking, 25, BuffNature.GraphOnlyBuff, "https://wiki.guildwars2.com/images/7/7f/Imperial_Guard.png", 119939, ulong.MaxValue),
         };
     }
 }
