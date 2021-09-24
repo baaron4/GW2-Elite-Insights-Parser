5de820e0a27eb80c226f02c9b7f92c58085f145e
EliphasNUIT <bichon.jekfer@gmail.com>
2021-09-24 10:39:01 +0200

    added vindicator ids

--- GW2EIEvtcParser/EIData/ProfHelpers/Revenant/VindicatorHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Revenant/VindicatorHelper.cs
@@ -11,25 +11,25 @@ namespace GW2EIEvtcParser.EIData
     {
         internal static readonly List<InstantCastFinder> InstantCastFinder = new List<InstantCastFinder>()
         {
-            new BuffGainCastFinder(62749, -1, EIData.InstantCastFinder.DefaultICD), // Legendary Alliance Stance
-            new BuffGainCastFinder(62891, -1, EIData.InstantCastFinder.DefaultICD), // Legendary Alliance Stance (UW)
+            new BuffGainCastFinder(62749, 62919, EIData.InstantCastFinder.DefaultICD), // Legendary Alliance Stance
+            new BuffGainCastFinder(62891, 62919, EIData.InstantCastFinder.DefaultICD), // Legendary Alliance Stance (UW)
             new DamageCastFinder(62705, 62705, EIData.InstantCastFinder.DefaultICD), // Call of the Alliance
-            new BuffGainCastFinder(62687, -1, EIData.InstantCastFinder.DefaultICD), // Urn of Saint Viktor
+            new BuffGainCastFinder(62687, 62864, EIData.InstantCastFinder.DefaultICD), // Urn of Saint Viktor
             //new EXTHealingCastFinder(-1, -1, EIData.InstantCastFinder.DefaultICD), // Redemptor's Sermon
         };
 
         internal static readonly List<DamageModifier> DamageMods = new List<DamageModifier>
         {
-            new BuffDamageModifier(-1, "Forerunner of Death", "15%", DamageSource.NoPets, 15.0, DamageType.Strike, DamageType.All, Source.Vindicator, ByPresence, "https://wiki.guildwars2.com/images/9/95/Forerunner_of_Death.png", 119939, ulong.MaxValue, DamageModifierMode.All),
+            new BuffDamageModifier(62811, "Forerunner of Death", "15%", DamageSource.NoPets, 15.0, DamageType.Strike, DamageType.All, Source.Vindicator, ByPresence, "https://wiki.guildwars2.com/images/9/95/Forerunner_of_Death.png", 119939, ulong.MaxValue, DamageModifierMode.All),
         };
 
         internal static readonly List<Buff> Buffs = new List<Buff>
         {
-            new Buff("Legendary Alliance Stance",-1, Source.Revenant, BuffNature.GraphOnlyBuff, "https://wiki.guildwars2.com/images/d/d6/Legendary_Alliance_Stance.png", 119939, ulong.MaxValue),
-            new Buff("Urn of Saint Viktor", -1, Source.Vindicator, BuffNature.GraphOnlyBuff,"https://wiki.guildwars2.com/images/f/ff/Urn_of_Saint_Viktor.png", 119939, ulong.MaxValue),
-            new Buff("Saint of zu Heltzer", -1, Source.Vindicator, BuffNature.GraphOnlyBuff,"https://wiki.guildwars2.com/images/3/36/Saint_of_zu_Heltzer.png", 119939, ulong.MaxValue),
-            new Buff("Forerunner of Death", -1, Source.Vindicator, BuffNature.GraphOnlyBuff,"https://wiki.guildwars2.com/images/9/95/Forerunner_of_Death.png", 119939, ulong.MaxValue),
-            new Buff("Imperial Guard", -1, Source.Vindicator, BuffStackType.Stacking, 25, BuffNature.GraphOnlyBuff, "https://wiki.guildwars2.com/images/7/7f/Imperial_Guard.png", 119939, ulong.MaxValue),
+            new Buff("Legendary Alliance Stance",62919, Source.Revenant, BuffNature.GraphOnlyBuff, "https://wiki.guildwars2.com/images/d/d6/Legendary_Alliance_Stance.png", 119939, ulong.MaxValue),
+            new Buff("Urn of Saint Viktor", 62864, Source.Vindicator, BuffNature.GraphOnlyBuff,"https://wiki.guildwars2.com/images/f/ff/Urn_of_Saint_Viktor.png", 119939, ulong.MaxValue),
+            new Buff("Saint of zu Heltzer", 62994, Source.Vindicator, BuffNature.GraphOnlyBuff,"https://wiki.guildwars2.com/images/3/36/Saint_of_zu_Heltzer.png", 119939, ulong.MaxValue),
+            new Buff("Forerunner of Death", 62811, Source.Vindicator, BuffNature.GraphOnlyBuff,"https://wiki.guildwars2.com/images/9/95/Forerunner_of_Death.png", 119939, ulong.MaxValue),
+            new Buff("Imperial Guard", 62819, Source.Vindicator, BuffStackType.Stacking, 5, BuffNature.GraphOnlyBuff, "https://wiki.guildwars2.com/images/7/7f/Imperial_Guard.png", 119939, ulong.MaxValue),
         };
 
         internal static HashSet<long> VindicatorDodges = new HashSet<long>()
