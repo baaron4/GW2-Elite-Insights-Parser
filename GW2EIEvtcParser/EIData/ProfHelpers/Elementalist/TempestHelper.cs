6096fbe79d1e0eec93a3635e69d1085b67f1de75
EliphasNUIT <bichon.jekfer@gmail.com>
2021-11-06 16:07:12 +0100

    moved static charge from ele to tempest + added heat sync

--- GW2EIEvtcParser/EIData/ProfHelpers/Elementalist/TempestHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Elementalist/TempestHelper.cs
@@ -24,6 +24,8 @@ namespace GW2EIEvtcParser.EIData
                 new Buff("Rebound",31337, Source.Tempest, BuffNature.DefensiveBuffTable, "https://wiki.guildwars2.com/images/0/03/%22Rebound%21%22.png"),
                 new Buff("Harmonious Conduit",31353, Source.Tempest, BuffNature.GraphOnlyBuff, "https://wiki.guildwars2.com/images/b/b3/Harmonious_Conduit.png", 0, 99526),
                 new Buff("Transcendent Tempest",31353, Source.Tempest, BuffNature.GraphOnlyBuff, "https://wiki.guildwars2.com/images/a/ac/Transcendent_Tempest_%28effect%29.png", 99526, ulong.MaxValue),
+                new Buff("Static Charge",31487, Source.Tempest, BuffNature.OffensiveBuffTable, "https://wiki.guildwars2.com/images/4/4b/Overload_Air.png"),
+                new Buff("Heat Sync",30462, Source.Tempest, BuffNature.SupportBuffTable, "https://wiki.guildwars2.com/images/d/d9/Heat_Sync.png"),
         };
 
     }
