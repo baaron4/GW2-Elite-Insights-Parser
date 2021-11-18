fcd4f8123cfef9f34ff4e5ec23c5f87558e41168
EliphasNUIT <bichon.jekfer@gmail.com>
2021-11-19 00:18:41 +0100

    added Caithe, Blighted Braham and Blighted Sieran ids

--- GW2EIEvtcParser/ParserHelpers/ParserHelper.cs
+++ GW2EIEvtcParser/ParserHelpers/ParserHelper.cs
@@ -680,23 +680,23 @@ namespace GW2EIEvtcParser
                     return "https://i.imgur.com/UuxqFko.png";
                 case ArcDPSEnums.TrashID.Braham:
                     return "https://i.imgur.com/IZwGr7N.png";
-                /*case ArcDPSEnums.TrashID.Caithe:
-                    return "https://i.imgur.com/gxBNudo.png";*/
+                case ArcDPSEnums.TrashID.Caithe:
+                    return "https://i.imgur.com/gxBNudo.png";
                 case ArcDPSEnums.TrashID.BlightedRytlock:
                     return "https://i.imgur.com/iDvXEZj.png";
                 /*case ArcDPSEnums.TrashID.BlightedCanach:
-                    return "https://i.imgur.com/ObZJXxd.png";
+                    return "https://i.imgur.com/ObZJXxd.png";*/
                 case ArcDPSEnums.TrashID.BlightedBraham:
-                    return "https://i.imgur.com/wcLwsIg.png";*/
+                    return "https://i.imgur.com/wcLwsIg.png";
                 case ArcDPSEnums.TrashID.BlightedMarjory:
                     return "https://i.imgur.com/SqKNAzN.png";
                 case ArcDPSEnums.TrashID.BlightedCaithe:
                     return "https://i.imgur.com/hruf4mI.png";
                 case ArcDPSEnums.TrashID.BlightedForgal:
                     return "https://i.imgur.com/LIioL0V.png";
-                /*case ArcDPSEnums.TrashID.BlightedSieran:
+                case ArcDPSEnums.TrashID.BlightedSieran:
                     return "https://i.imgur.com/8EaXVPP.png";
-                case ArcDPSEnums.TrashID.BlightedTybalt:
+                /*case ArcDPSEnums.TrashID.BlightedTybalt:
                     return "https://i.imgur.com/TgHJKB3.png";
                 case ArcDPSEnums.TrashID.BlightedPaleTree:
                     return "https://i.imgur.com/l6ADzRj.png";
