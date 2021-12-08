0d39dab30aa45901061240ee15a59359e4c3694c
EliphasNUIT <bichon.jekfer@gmail.com>
2021-12-08 12:52:52 +0100

    renaming

--- GW2EIEvtcParser/Extensions/ExtensionStatistics/BarrierStats/EXTFinalOutgoingBarrierStat.cs
+++ GW2EIEvtcParser/Extensions/ExtensionStatistics/BarrierStats/EXTFinalOutgoingBarrierStat.cs
@@ -14,27 +14,27 @@ namespace GW2EIEvtcParser.Extensions
         private int DownedBps { get; }
         private int DownedBarrier { get;}
 
-        public int ActorHps { get; }
-        public int ActorHealing { get; }
-        private int ActorDownedHps { get; }
-        private int ActorDownedHealing { get; }
+        public int ActorBps { get; }
+        public int ActorBarrier { get; }
+        private int ActorDownedBps { get; }
+        private int ActorDownedBarrier { get; }
 
         internal EXTFinalOutgoingBarrierStat(ParsedEvtcLog log, long start, long end, AbstractSingleActor actor, AbstractSingleActor target)
         {
-            (Barrier, DownedBarrier) = ComputeHealingFrom(log, actor.EXTBarrier.GetOutgoingBarrierEvents(target, log, start, end));
-            (ActorHealing, ActorDownedHealing) = ComputeHealingFrom(log, actor.EXTBarrier.GetJustActorOutgoingHealEvents(target, log, start, end));
+            (Barrier, DownedBarrier) = ComputeBarrierFrom(log, actor.EXTBarrier.GetOutgoingBarrierEvents(target, log, start, end));
+            (ActorBarrier, ActorDownedBarrier) = ComputeBarrierFrom(log, actor.EXTBarrier.GetJustActorOutgoingHealEvents(target, log, start, end));
             double phaseDuration = (end - start) / 1000.0;
             if (phaseDuration > 0)
             {
                 Bps = (int)Math.Round(Barrier / phaseDuration);
                 DownedBps = (int)Math.Round(DownedBarrier / phaseDuration);
 
-                ActorHps = (int)Math.Round(ActorHealing / phaseDuration);
-                ActorDownedHps = (int)Math.Round(ActorDownedHealing / phaseDuration);
+                ActorBps = (int)Math.Round(ActorBarrier / phaseDuration);
+                ActorDownedBps = (int)Math.Round(ActorDownedBarrier / phaseDuration);
             }
         }
 
-        private static (int healing, int downedHealing) ComputeHealingFrom(ParsedEvtcLog log, IReadOnlyList<EXTAbstractBarrierEvent> barrierEvents)
+        private static (int healing, int downedHealing) ComputeBarrierFrom(ParsedEvtcLog log, IReadOnlyList<EXTAbstractBarrierEvent> barrierEvents)
         {
             int healing = 0;
             int downedHealing = 0;
