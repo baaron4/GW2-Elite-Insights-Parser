a2f18eeab77257b489baed17de03f0866731dd15
EliphasNUIT <bichon.jekfer@gmail.com>
2021-12-08 11:36:02 +0100

    added Barrier combat data

---
+++ GW2EIEvtcParser/Extensions/ExtensionCombatData/EXTBarrierCombatData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser.Extensions
+{
+    public class EXTBarrierCombatData
+    {
+        private readonly Dictionary<AgentItem, List<EXTAbstractHealingEvent>> _barrierData;
+        private readonly Dictionary<AgentItem, List<EXTAbstractHealingEvent>> _barrierReceivedData;
+        private readonly Dictionary<long, List<EXTAbstractHealingEvent>> _barrierDataByID;
+
+        internal EXTBarrierCombatData(Dictionary<AgentItem, List<EXTAbstractHealingEvent>> barrierData, Dictionary<AgentItem, List<EXTAbstractHealingEvent>> barrierReceivedData, Dictionary<long, List<EXTAbstractHealingEvent>> barrierDataByID)
+        {
+            _barrierData = barrierData;
+            _barrierReceivedData = barrierReceivedData;
+            _barrierDataByID = barrierDataByID;
+        }
+
+        public IReadOnlyList<EXTAbstractHealingEvent> GetHealData(AgentItem key)
+        {
+            if (_barrierData.TryGetValue(key, out List<EXTAbstractHealingEvent> res))
+            {
+                return res;
+            }
+            return new List<EXTAbstractHealingEvent>();
+        }
+        public IReadOnlyList<EXTAbstractHealingEvent> GetHealReceivedData(AgentItem key)
+        {
+            if (_barrierReceivedData.TryGetValue(key, out List<EXTAbstractHealingEvent> res))
+            {
+                return res;
+            }
+            return new List<EXTAbstractHealingEvent>();
+        }
+
+        public IReadOnlyList<EXTAbstractHealingEvent> GetHealData(long key)
+        {
+            if (_barrierDataByID.TryGetValue(key, out List<EXTAbstractHealingEvent> res))
+            {
+                return res;
+            }
+            return new List<EXTAbstractHealingEvent>();
+        }
+    }
+}
