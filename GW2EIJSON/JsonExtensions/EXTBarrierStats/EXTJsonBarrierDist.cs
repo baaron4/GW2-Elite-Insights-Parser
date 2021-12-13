bf1fc86b27bc449e32bc6ace46baea30adcc7716
EliphasNUIT <bichon.jekfer@gmail.com>
2021-12-13 15:42:32 +0100

    added barrier stats classes to Json

---
+++ GW2EIJSON/JsonExtensions/EXTBarrierStats/EXTJsonBarrierDist.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace GW2EIJSON
+{
+    /// <summary>
+    /// Class corresponding a barrier distribution
+    /// </summary>
+    public class EXTJsonBarrierDist
+    {
+        
+        /// <summary>
+        /// Total barrier done
+        /// </summary>
+        public int TotalBarrier{ get; set; }
+
+        /// <summary>
+        /// Minimum barrier done
+        /// </summary>
+        public int Min { get; set; }
+
+        /// <summary>
+        /// Maximum barrier done
+        /// </summary>
+        public int Max { get; set; }
+        
+        /// <summary>
+        /// Number of hits
+        /// </summary>
+        public int Hits { get; set; }
+
+        /// <summary>
+        /// ID of the barrier skill
+        /// </summary>
+        /// <seealso cref="JsonLog.SkillMap"/>
+        /// <seealso cref="JsonLog.BuffMap"/>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// True if indirect barrier \n
+        /// If true, the id is a buff
+        /// </summary>
+        public bool IndirectBarrier { get; set; }
+
+        
+        public EXTJsonBarrierDist()
+        {
+
+        }  
+
+    }
+}
