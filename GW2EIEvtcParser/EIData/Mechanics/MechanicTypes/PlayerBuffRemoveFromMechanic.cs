99ce9243346697de72f449f29cc394d1486071f6
EliphasNUIT <bichon.jekfer@gmail.com>
2022-01-25 20:51:23 +0100

    added PlayerBuffApplyTo mechanic and PlayerBuffRemoveFrom mechanic

---
+++ GW2EIEvtcParser/EIData/Mechanics/MechanicTypes/PlayerBuffRemoveFromMechanic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser.EIData
+{
+
+    internal class PlayerBuffRemoveFromMechanic : BuffRemoveMechanic
+    {
+
+        public PlayerBuffRemoveFromMechanic(long skillId, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, int internalCoolDown, BuffRemoveChecker condition) : this(skillId, inGameName, plotlySetting, shortName, shortName, shortName, internalCoolDown, condition)
+        {
+        }
+
+        public PlayerBuffRemoveFromMechanic(long skillId, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown, BuffRemoveChecker condition) : base(skillId, inGameName, plotlySetting, shortName, description, fullName, internalCoolDown, condition)
+        {
+        }
+
+        public PlayerBuffRemoveFromMechanic(long skillId, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, int internalCoolDown) : this(skillId, inGameName, plotlySetting, shortName, shortName, shortName, internalCoolDown)
+        {
+        }
+
+        public PlayerBuffRemoveFromMechanic(long skillId, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown) : base(skillId, inGameName, plotlySetting, shortName, description, fullName, internalCoolDown)
+        {
+        }
+
+        internal override void CheckMechanic(ParsedEvtcLog log, Dictionary<Mechanic, List<MechanicEvent>> mechanicLogs, Dictionary<int, AbstractSingleActor> regroupedMobs)
+        {
+            foreach (Player p in log.PlayerList)
+            {
+                foreach (AbstractBuffEvent c in log.CombatData.GetBuffData(SkillId))
+                {
+                    if (c is BuffRemoveAllEvent rae && p.AgentItem == rae.CreditedBy && Keep(rae, log))
+                    {
+                        mechanicLogs[this].Add(new MechanicEvent(rae.Time, this, p));
+                    }
+                }
+            }
+        }
+    }
+}
