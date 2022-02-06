9190836c6c8934b012f2d8103e8d920d353dec67
EliphasNUIT <bichon.jekfer@gmail.com>
2022-02-06 02:32:28 +0100

    ground work for sub fight logic in InstanceLogic

--- GW2EIEvtcParser/EncounterLogic/Instance.cs
+++ GW2EIEvtcParser/EncounterLogic/Instance.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using GW2EIEvtcParser.EIData;
 using GW2EIEvtcParser.Extensions;
 using GW2EIEvtcParser.ParsedData;
 using static GW2EIEvtcParser.EncounterLogic.EncounterCategory;
@@ -10,17 +12,46 @@ namespace GW2EIEvtcParser.EncounterLogic
     {
         public bool StartedLate { get; private set; }
         public bool EndedBeforeExpectedEnd { get; private set; }
+        private readonly List<FightLogic> _subLogics = new List<FightLogic>();
         public Instance(int id) : base(id)
         {
             Extension = "instance";
-            Targetless = true;
             Icon = "https://wiki.guildwars2.com/images/d/d2/Guild_emblem_004.png";
             EncounterCategoryInformation.Category = FightCategory.UnknownEncounter;
             EncounterCategoryInformation.SubCategory = SubFightCategory.UnknownEncounter;
         }
 
+        private void FillSubLogics(AgentData agentData)
+        {
+            var allTargetIDs = Enum.GetValues(typeof(ArcDPSEnums.TargetID)).Cast<int>().ToList();
+            foreach (int targetID in allTargetIDs)
+            {
+                if (agentData.GetNPCsByID(targetID).Any())
+                {
+                    switch(targetID)
+                    {
+                        case (int)ArcDPSEnums.TargetID.AiKeeperOfThePeak:
+                            //_subLogics.Add(new AiKeeperOfThePeak(targetID));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
         internal override void EIEvtcParse(ulong gw2Build, FightData fightData, AgentData agentData, List<CombatItem> combatData, IReadOnlyDictionary<uint, AbstractExtensionHandler> extensions)
         {
+            FillSubLogics(agentData);
+            foreach (FightLogic logic in _subLogics)
+            {
+                logic.EIEvtcParse(gw2Build, fightData, agentData, combatData, extensions);
+                _targets.AddRange(logic.Targets);
+                _trashMobs.AddRange(logic.TrashMobs);
+                _nonPlayerFriendlies.AddRange(logic.NonPlayerFriendlies);
+            }
+            _targets.RemoveAll(x => x.ID == (int)ArcDPSEnums.TargetID.DummyTarget);
+            Targetless = !_targets.Any();
             AgentItem dummyAgent = agentData.AddCustomNPCAgent(0, fightData.FightEnd, "Dummy Instance Target", ParserHelper.Spec.NPC, (int)ArcDPSEnums.TargetID.Instance, true);
             ComputeFightTargets(agentData, combatData, extensions);
         }
@@ -38,10 +69,105 @@ namespace GW2EIEvtcParser.EncounterLogic
                 StartedLate = Math.Abs(evt.OffsetFromInstanceCreation - fightData.LogOffset) < 20000;
             }
         }
-
+        internal override string GetLogicName(CombatData combatData, AgentData agentData)
+        {
+            MapIDEvent mapID = combatData.GetMapIDEvents().LastOrDefault();
+            if (mapID == null)
+            {
+                return base.GetLogicName(combatData, agentData);
+            }
+            switch(mapID.MapID)
+            {
+                case 1384:
+                    EncounterCategoryInformation.Category = FightCategory.Fractal;
+                    EncounterCategoryInformation.SubCategory = SubFightCategory.SunquaPeak;
+                    Extension = "snqpeak";
+                    Icon = "https://i.imgur.com/3mlCdI9.png";
+                    return "Sunqua Peak";
+            }
+            return base.GetLogicName(combatData, agentData);
+        }
+        internal override List<InstantCastFinder> GetInstantCastFinders()
+        {
+            var res = new List<InstantCastFinder>();
+            foreach (FightLogic logic in _subLogics)
+            {
+                res.AddRange(logic.GetInstantCastFinders());
+            }
+            return res;
+        }
+        internal override List<ErrorEvent> GetCustomWarningMessages(FightData fightData, int arcdpsVersion)
+        {
+            var res = new List<ErrorEvent>();
+            foreach (FightLogic logic in _subLogics)
+            {
+                res.AddRange(logic.GetCustomWarningMessages(fightData, arcdpsVersion));
+            }
+            return res;
+        }
+        internal override void ComputePlayerCombatReplayActors(AbstractPlayer p, ParsedEvtcLog log, CombatReplay replay)
+        {
+            foreach (FightLogic logic in _subLogics)
+            {
+                logic.ComputePlayerCombatReplayActors(p, log, replay);
+            }
+        }
+        internal override List<AbstractBuffEvent> SpecialBuffEventProcess(CombatData combatData, SkillData skillData)
+        {
+            var res = new List<AbstractBuffEvent>();
+            foreach (FightLogic logic in _subLogics)
+            {
+                res.AddRange(logic.SpecialBuffEventProcess(combatData, skillData));
+            }
+            return res;
+        }
+        internal override List<AbstractCastEvent> SpecialCastEventProcess(CombatData combatData, SkillData skillData)
+        {
+            var res = new List<AbstractCastEvent>();
+            foreach (FightLogic logic in _subLogics)
+            {
+                res.AddRange(logic.SpecialCastEventProcess(combatData, skillData));
+            }
+            return res;
+        }
+        internal override List<AbstractHealthDamageEvent> SpecialDamageEventProcess(CombatData combatData, SkillData skillData)
+        {
+            var res = new List<AbstractHealthDamageEvent>();
+            foreach (FightLogic logic in _subLogics)
+            {
+                res.AddRange(logic.SpecialDamageEventProcess(combatData, skillData));
+            }
+            return res;
+        }
+        internal override void ComputeNPCCombatReplayActors(NPC target, ParsedEvtcLog log, CombatReplay replay)
+        {
+            foreach (FightLogic logic in _subLogics)
+            {
+                logic.ComputeNPCCombatReplayActors(target, log, replay);
+            }
+        }
+        protected override List<int> GetTargetsIDs()
+        {
+            if (!Targetless)
+            {
+                return new List<int>();
+            }
+            return new List<int>()
+            {
+                GenericTriggerID
+            };
+        }
+        protected override List<ArcDPSEnums.TrashID> GetTrashMobsIDs()
+        {
+            return new List<ArcDPSEnums.TrashID>();
+        }
         protected override HashSet<int> GetUniqueNPCIDs()
         {
             return new HashSet<int>();
+        }
+        protected override List<int> GetFriendlyNPCIDs()
+        {
+            return new List<int>();
         }
     }
 }
