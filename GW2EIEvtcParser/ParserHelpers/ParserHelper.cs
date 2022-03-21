6a1629ab280e4c76d7b4e866f653525abeb15b88
EliphasNUIT <bichon.jekfer@gmail.com>
2022-03-21 21:14:06 +0100

    fixed InternalSkillIDsChange value

--- GW2EIEvtcParser/ParserHelpers/ParserHelper.cs
+++ GW2EIEvtcParser/ParserHelpers/ParserHelper.cs
@@ -56,7 +56,7 @@ namespace GW2EIEvtcParser
             //
             internal const int ProperConfusionDamageSimulation = 20210529;
             internal const int DirectX11Updated = 20210923;
-            internal const int InternalSkillIDsChange = 20220307;
+            internal const int InternalSkillIDsChange = 20220304;
             internal const int BuffAttrFlatIncRemoved = 20220308;
             //
             internal const int EndOfLife = int.MaxValue;
