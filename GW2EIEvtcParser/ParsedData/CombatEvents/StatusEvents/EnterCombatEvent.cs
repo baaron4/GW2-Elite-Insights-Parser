e3045efde16efba39fe344c083159382ed193e78
EliphasNUIT <bichon.jekfer@gmail.com>
2022-04-26 00:53:44 +0200

    added Subgroup to EnterCombatEvent

--- GW2EIEvtcParser/ParsedData/CombatEvents/StatusEvents/EnterCombatEvent.cs
+++ GW2EIEvtcParser/ParsedData/CombatEvents/StatusEvents/EnterCombatEvent.cs
@@ -2,9 +2,10 @@
 {
     public class EnterCombatEvent : AbstractStatusEvent
     {
+        public int Subgroup { get; }
         internal EnterCombatEvent(CombatItem evtcItem, AgentData agentData) : base(evtcItem, agentData)
         {
-
+            Subgroup = (int)evtcItem.DstAgent;
         }
 
     }
