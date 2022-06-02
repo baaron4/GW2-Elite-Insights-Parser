f5961949900a55c77cd25916c259b87032f3284a
EliphasNUIT <bichon.jekfer@gmail.com>
2022-06-03 01:18:56 +0200

    created EffectGUIDs file

---
+++ GW2EIEvtcParser/ParserHelpers/EffectGUIDs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser
+{
+    public static class EffectGUIDs
+    {
+
+    }
+}
