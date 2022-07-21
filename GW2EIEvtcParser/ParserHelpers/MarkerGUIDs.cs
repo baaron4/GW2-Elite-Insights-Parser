3e11635f46c5604dbd7c95c846bd63b2994a845f
EliphasNUIT <bichon.jekfer@gmail.com>
2022-07-22 00:11:51 +0200

    added all catmander GUIDs

--- GW2EIEvtcParser/ParserHelpers/MarkerGUIDs.cs
+++ GW2EIEvtcParser/ParserHelpers/MarkerGUIDs.cs
@@ -11,6 +11,13 @@ namespace GW2EIEvtcParser
         public const string CommanderTag2 = "AF9442A290C6214596E0B339EB3BDE92";
         public const string CommanderTag3 = "1993FADB6FB70E4383A223A54D311F7D";
         public const string BlueCatmanderTag = "9B94F0FD616E7F4AA58EFDC8C59FB689";
+        public const string CyanCatmanderTag = "A8072D65CE35924BABBAC831B12019D7";
         public const string GreenCatmanderTag = "2CA226E07262C743BA193ACF6F9D0AF6";
+        public const string OrangeCatmanderTag = "9FDF03E9BA09A2458C1EDDA4D81BC34D";
+        public const string PinkCatmanderTag = "4387BE6146D43246AA7B333168EA58EA";
+        public const string PurpleCatmanderTag = "7224A4AF710E4243BFE032629E17CA6E";
+        public const string RedCatmanderTag = "CA76AB023593B0448F692FE29DF03D17";
+        public const string YellowCatmanderTag = "6BCE90E99016B448969EB317784A8334";
+        public const string WhiteCatmanderTag = "A0B0EC076BC83B40A293C1CDEC4A7DE7";
     }
 }
