e9e24e48c1b1b7971a393291aaf7beaacda7b910
EliphasNUIT <bichon.jekfer@gmail.com>
2022-08-08 01:58:12 +0200

    cleanup

--- GW2EIEvtcParser/EIData/ProfHelpers/Warrior/SpellbreakerHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Warrior/SpellbreakerHelper.cs
@@ -26,7 +26,7 @@ namespace GW2EIEvtcParser.EIData
             new BuffDamageModifierTarget(NumberOfBoons, "Pure Strike (no boons)", "14% crit damage", DamageSource.NoPets, 14.0, DamageType.Strike, DamageType.All, Source.Spellbreaker, ByAbsence, "https://wiki.guildwars2.com/images/7/76/Pure_Strike_%28trait%29.png", DamageModifierMode.All).UsingChecker( (x, log) => x.HasCrit),
             new BuffDamageModifierTarget(MagebaneTether, "Magebane Tether", "10% to tethered target", DamageSource.NoPets, 10.0, DamageType.Strike, DamageType.All, Source.Spellbreaker, ByPresence, "https://wiki.guildwars2.com/images/7/76/Big_Game_Hunter.png", DamageModifierMode.PvEInstanceOnly).UsingChecker((x, log) => {
                 AgentItem src = x.From;
-                  AgentItem dst = x.To;
+                AgentItem dst = x.To;
                 AbstractBuffEvent effectApply = log.CombatData.GetBuffData(MagebaneTether).Where(y => y is BuffApplyEvent bae && Math.Abs(bae.AppliedDuration - 8000) < ServerDelayConstant && bae.By == src && bae.To == dst).LastOrDefault(y => y.Time <= x.Time);
                 if (effectApply != null)
                 {
