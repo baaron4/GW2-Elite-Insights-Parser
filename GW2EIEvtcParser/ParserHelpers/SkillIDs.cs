efa30f3eb730db1348c5db63d321039f4475b235
EliphasNUIT <bichon.jekfer@gmail.com>
2022-11-30 17:38:45 +0100

    added Dormant buff skill ids

--- GW2EIEvtcParser/ParserHelpers/SkillIDs.cs
+++ GW2EIEvtcParser/ParserHelpers/SkillIDs.cs
@@ -1697,6 +1697,9 @@ namespace GW2EIEvtcParser
         public const long LeyWovenShielding = 68508;
         public const long ErgoShear = 68546;
         public const long FixatedOldLionsCourt = 68562;
+        public const long DormantCourage = 68585;
+        public const long DormantJustice = 68607;
+        public const long DormantResolve = 68640;
     }
 
 }
