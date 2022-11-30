f38626a5ac0a365a4e75d7ad0342e3982cf197d1
EliphasNUIT <bichon.jekfer@gmail.com>
2022-11-30 13:31:54 +0100

    updated guardian for balance patch

--- GW2EIEvtcParser/EIData/ProfHelpers/Guardian/DragonhunterHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Guardian/DragonhunterHelper.cs
@@ -43,13 +43,23 @@ namespace GW2EIEvtcParser.EIData
                 }
                 return false;
             }),
-            new BuffDamageModifierTarget(JusticeDragonhunter, "Big Game Hunter", "15% to tethered target", DamageSource.NoPets, 15.0, DamageType.Strike, DamageType.All, Source.Dragonhunter, ByPresence, "https://wiki.guildwars2.com/images/7/76/Big_Game_Hunter.png", DamageModifierMode.PvEInstanceOnly).WithBuilds(GW2Builds.February2020Balance).UsingChecker((x, log) => {
+            new BuffDamageModifierTarget(JusticeDragonhunter, "Big Game Hunter", "15% to tethered target", DamageSource.NoPets, 15.0, DamageType.Strike, DamageType.All, Source.Dragonhunter, ByPresence, "https://wiki.guildwars2.com/images/7/76/Big_Game_Hunter.png", DamageModifierMode.PvEInstanceOnly).WithBuilds(GW2Builds.February2020Balance, GW2Builds.November2022Balance).UsingChecker((x, log) => {
                 AgentItem src = x.From;
                 AgentItem dst = x.To;
                 AbstractBuffEvent effectApply = log.CombatData.GetBuffData(JusticeDragonhunter).Where(y => y is BuffApplyEvent bae && Math.Abs(bae.AppliedDuration - 10000) < ServerDelayConstant && bae.By == src && bae.To == dst).LastOrDefault(y => y.Time <= x.Time);
                 if (effectApply != null)
                 {
                    return x.Time - effectApply.Time < 10000;
+                }
+                return false;
+            }),
+            new BuffDamageModifierTarget(JusticeDragonhunter, "Big Game Hunter", "15% to tethered target", DamageSource.NoPets, 15.0, DamageType.Strike, DamageType.All, Source.Dragonhunter, ByPresence, "https://wiki.guildwars2.com/images/7/76/Big_Game_Hunter.png", DamageModifierMode.PvEInstanceOnly).WithBuilds(GW2Builds.November2022Balance).UsingChecker((x, log) => {
+                AgentItem src = x.From;
+                AgentItem dst = x.To;
+                AbstractBuffEvent effectApply = log.CombatData.GetBuffData(JusticeDragonhunter).Where(y => y is BuffApplyEvent bae && Math.Abs(bae.AppliedDuration - 12000) < ServerDelayConstant && bae.By == src && bae.To == dst).LastOrDefault(y => y.Time <= x.Time);
+                if (effectApply != null)
+                {
+                   return x.Time - effectApply.Time < 12000;
                 }
                 return false;
             }),
