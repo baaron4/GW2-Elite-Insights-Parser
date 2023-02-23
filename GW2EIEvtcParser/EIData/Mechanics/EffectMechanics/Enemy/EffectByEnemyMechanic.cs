db4e5d343332e95d39b2a5e2e4ae9dfdf6e7bcbc
EliphasNUIT <bichon.jekfer@gmail.com>
2023-02-23 14:28:35 +0100

    added effect tracking mechanics

---
+++ GW2EIEvtcParser/EIData/Mechanics/EffectMechanics/Enemy/EffectByEnemyMechanic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser.EIData
+{
+
+    internal class EffectByEnemyMechanic : EffectByMechanic
+    {
+
+        public EffectByEnemyMechanic(string effectGUID, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown, EffectChecker condition = null) : this(new string[] { effectGUID }, inGameName, plotlySetting, shortName, description, fullName, internalCoolDown, condition)
+        {
+        }
+
+        public EffectByEnemyMechanic(string[] effectGUIDs, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown, EffectChecker condition = null) : base(effectGUIDs, inGameName, plotlySetting, shortName, description, fullName, internalCoolDown, condition)
+        {
+            IsEnemyMechanic = true;
+        }
+
+        internal override void CheckMechanic(ParsedEvtcLog log, Dictionary<Mechanic, List<MechanicEvent>> mechanicLogs, Dictionary<int, AbstractSingleActor> regroupedMobs)
+        {
+            EnemyChecker(log, mechanicLogs, regroupedMobs);
+        }
+    }
+}
---
+++ GW2EIEvtcParser/EIData/Mechanics/EffectMechanics/Enemy/EffectOnEnemyMechanic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser.EIData
+{
+
+    internal class EffectOnEnemyMechanic : EffectOnMechanic
+    {
+
+        public EffectOnEnemyMechanic(string effectGUID, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown, EffectChecker condition = null) : this(new string[] { effectGUID }, inGameName, plotlySetting, shortName, description, fullName, internalCoolDown, condition)
+        {
+        }
+
+        public EffectOnEnemyMechanic(string[] effectGUIDs, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown, EffectChecker condition = null) : base(effectGUIDs, inGameName, plotlySetting, shortName, description, fullName, internalCoolDown, condition)
+        {
+            IsEnemyMechanic = true;
+        }
+
+        internal override void CheckMechanic(ParsedEvtcLog log, Dictionary<Mechanic, List<MechanicEvent>> mechanicLogs, Dictionary<int, AbstractSingleActor> regroupedMobs)
+        {
+            EnemyChecker(log, mechanicLogs, regroupedMobs);
+        }
+    }
+}
