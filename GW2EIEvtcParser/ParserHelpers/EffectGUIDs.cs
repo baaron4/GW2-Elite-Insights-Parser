7d6534f853d6f0131265f0a16aff58c2dc3adbb5
Linkaaaaa <christian.pagliarani@gmail.com>
2023-03-01 19:12:57 +0100

    added possible toxic sickness guids

--- GW2EIEvtcParser/ParserHelpers/EffectGUIDs.cs
+++ GW2EIEvtcParser/ParserHelpers/EffectGUIDs.cs
@@ -81,6 +81,10 @@ namespace GW2EIEvtcParser
         public const string UntamedUnnaturalTraversal = "8D36806A690A5442A983308EDCECB018";
         // Thief
         public const string DeadeyeMercy = "B59FCEFCF1D5D84B9FDB17F11E9B52E6";
+        // Nightmare Fractal
+        public const string ToxicSicknessIndicator = "3C98B00B9E795F4B8744E186EEEA7DF7";
+        public const string ToxicSicknessPuke1 = "B7DFF8C2A8DABD4C9C7F1D4CFC31FC8C";
+        public const string ToxicSicknessPuke2 = "E09CD66E417B59409401192201CE4B6E";
         // Vale Guardian
         public const string ValeGuardianDistributedMagic = "43FD739499BB6040BBF9EEF37781B2CE";
         public const string ValeGuardianMagicSpike = "55364633145D264A934935C3F026B19F";
