c9c24ad60611d71e1e11925ab72d446ce15bbc05
Zerthox <zerthox@gmail.com>
2023-05-27 19:55:07 +0200

    Add scourge sand swell decorations

--- GW2EIEvtcParser/EIData/ProfHelpers/Necromancer/ScourgeHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Necromancer/ScourgeHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using GW2EIEvtcParser.EIData.Buffs;
 using GW2EIEvtcParser.Extensions;
+using GW2EIEvtcParser.ParsedData;
 using static GW2EIEvtcParser.ArcDPSEnums;
 using static GW2EIEvtcParser.EIData.Buff;
 using static GW2EIEvtcParser.ParserHelper;
@@ -35,6 +37,27 @@ namespace GW2EIEvtcParser.EIData
 
          internal static void ComputeProfessionCombatReplayActors(AbstractPlayer player, ParsedEvtcLog log, CombatReplay replay)
         {
+            foreach (List<EffectEvent> group in log.CombatData.GetGroupedEffectEventsBySrcWithGUID(player.AgentItem, EffectGUIDs.ScourgeSandSwellPortal))
+            {
+                IconDecoration first = null;
+                for (int i = 0; i < group.Count; i++)
+                {
+                    EffectEvent effect = group[i];
+                    int start = (int)effect.Time;
+                    var remove = log.CombatData.GetBuffData(PathUses).OfType<BuffRemoveAllEvent>().FirstOrDefault(x => x.Time >= start);
+                    int end = (int?)remove?.Time ?? start + 8000;
+                    var decoration = new IconDecoration("https://wiki.guildwars2.com/images/2/20/Sand_Swell.png", 128, 0.5f, effect.Src, (start, end), new PositionConnector(effect.Position));
+                    replay.Decorations.Add(decoration);
+                    if (i == 0)
+                    {
+                        first = decoration;
+                    }
+                    else
+                    {
+                        replay.Decorations.Add(first.LineTo(decoration, 0, "rgba(30, 193, 110, 0.5)"));
+                    }
+                }
+            }
         }
     }
 }
