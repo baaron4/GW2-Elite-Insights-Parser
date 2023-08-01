63e66a69d200bc00ae2a46982f499a1ea074766e
EliphasNUIT <bichon.jekfer@gmail.com>
2023-08-01 21:00:35 +0200

    ground work for IconOverheadDecoration

---
+++ GW2EIEvtcParser/EIData/CombatReplay/Decorations/IconOverheadDecoration.cs
@@ -0,0 +1,22 @@
+﻿using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser.EIData
+{
+    internal class IconOverheadDecoration : IconDecoration
+    {
+
+        public IconOverheadDecoration(string icon, int pixelSize, float opacity, (int start, int end) lifespan, AgentConnector connector) : base(icon, pixelSize, (int)connector.Agent.HitboxWidth, opacity, lifespan, connector)
+        {
+        }
+
+        public override GenericAttachedDecoration UsingSkillMode(AbstractSingleActor owner, bool drawOnSelect = true)
+        {
+            return this;
+        }
+
+        public override GenericDecorationCombatReplayDescription GetCombatReplayDescription(CombatReplayMap map, ParsedEvtcLog log)
+        {
+            return new IconOverheadDecorationCombatReplayDescription(log, this, map);
+        }
+    }
+}
