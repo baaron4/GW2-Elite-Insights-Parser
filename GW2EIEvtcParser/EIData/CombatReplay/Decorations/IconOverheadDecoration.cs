0e31cfbbe99a5e6c02ae74e9eee49cf7a3df492d
EliphasNUIT <bichon.jekfer@gmail.com>
2023-08-05 01:15:05 +0200

    added max worldSize for OverheadIcons

--- GW2EIEvtcParser/EIData/CombatReplay/Decorations/IconOverheadDecoration.cs
+++ GW2EIEvtcParser/EIData/CombatReplay/Decorations/IconOverheadDecoration.cs
@@ -1,15 +1,16 @@
-﻿using GW2EIEvtcParser.ParsedData;
+﻿using System;
+using GW2EIEvtcParser.ParsedData;
 
 namespace GW2EIEvtcParser.EIData
 {
     internal class IconOverheadDecoration : IconDecoration
     {
 
-        public IconOverheadDecoration(string icon, int pixelSize, float opacity, (int start, int end) lifespan, AgentConnector connector) : base(icon, pixelSize, (int)connector.Agent.HitboxWidth, opacity, lifespan, connector)
+        public IconOverheadDecoration(string icon, int pixelSize, float opacity, (int start, int end) lifespan, AgentConnector connector) : base(icon, pixelSize, Math.Min((int)connector.Agent.HitboxWidth / 2, 100), opacity, lifespan, connector)
         {
         }
 
-        public IconOverheadDecoration(string icon, int pixelSize, float opacity, Segment lifespan, AgentConnector connector) : base(icon, pixelSize, (int)connector.Agent.HitboxWidth, opacity, lifespan, connector)
+        public IconOverheadDecoration(string icon, int pixelSize, float opacity, Segment lifespan, AgentConnector connector) : base(icon, pixelSize, Math.Min((int)connector.Agent.HitboxWidth / 2, 100), opacity, lifespan, connector)
         {
         }
 
