f1d2f958826319b85c1106fa5effac5f3b62aa52
EliphasNUIT <bichon.jekfer@gmail.com>
2023-08-22 18:49:37 +0200

    ground work for new strikes

---
+++ GW2EIEvtcParser/EncounterLogic/Strikes/SotO/CosmicObservatory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GW2EIEvtcParser.EIData;
+using GW2EIEvtcParser.Exceptions;
+using GW2EIEvtcParser.Extensions;
+using GW2EIEvtcParser.ParsedData;
+using GW2EIEvtcParser.ParserHelpers;
+using static GW2EIEvtcParser.ParserHelper;
+using static GW2EIEvtcParser.SkillIDs;
+using static GW2EIEvtcParser.EncounterLogic.EncounterLogicUtils;
+using static GW2EIEvtcParser.EncounterLogic.EncounterLogicPhaseUtils;
+using static GW2EIEvtcParser.EncounterLogic.EncounterLogicTimeUtils;
+using static GW2EIEvtcParser.EncounterLogic.EncounterImages;
+
+namespace GW2EIEvtcParser.EncounterLogic
+{
+    internal class CosmicObservatory : SecretOfTheObscureStrike
+    {
+        public CosmicObservatory(int triggerID) : base(triggerID)
+        {
+            MechanicList.AddRange(new List<Mechanic>
+            {
+            }
+            );
+            Extension = "cosobs";
+            EncounterCategoryInformation.InSubCategoryOrder = 0;
+            EncounterID |= 0x000001;
+        }
+    }
+}
---
+++ GW2EIEvtcParser/EncounterLogic/Strikes/SotO/TempleOfFebe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GW2EIEvtcParser.EIData;
+using GW2EIEvtcParser.Exceptions;
+using GW2EIEvtcParser.Extensions;
+using GW2EIEvtcParser.ParsedData;
+using GW2EIEvtcParser.ParserHelpers;
+using static GW2EIEvtcParser.ParserHelper;
+using static GW2EIEvtcParser.SkillIDs;
+using static GW2EIEvtcParser.EncounterLogic.EncounterLogicUtils;
+using static GW2EIEvtcParser.EncounterLogic.EncounterLogicPhaseUtils;
+using static GW2EIEvtcParser.EncounterLogic.EncounterLogicTimeUtils;
+using static GW2EIEvtcParser.EncounterLogic.EncounterImages;
+
+namespace GW2EIEvtcParser.EncounterLogic
+{
+    internal class TempleOfFebe : SecretOfTheObscureStrike
+    {
+        public TempleOfFebe(int triggerID) : base(triggerID)
+        {
+            MechanicList.AddRange(new List<Mechanic>
+            {
+            }
+            );
+            Extension = "tmpfeb";
+            EncounterCategoryInformation.InSubCategoryOrder = 0;
+            EncounterID |= 0x000002;
+        }
+    }
+}
