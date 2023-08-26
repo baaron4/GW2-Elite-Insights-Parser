953f7b49294781ff7e6638f1bd2e2807d59480e0
EliphasNUIT <bichon.jekfer@gmail.com>
2023-08-26 14:41:29 +0200

    added missing parameter

--- GW2EIEvtcParser/EIData/ProfHelpers/Mesmer/VirtuosoHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Mesmer/VirtuosoHelper.cs
@@ -121,7 +121,7 @@ namespace GW2EIEvtcParser.EIData
                 foreach (EffectEvent effect in thousandCuts)
                 {
                     (int, int) lifespan = ProfHelper.ComputeEffectLifespan(log, effect, 5000);
-                    var connector = (PositionConnector)new PositionConnector(effect.Position).WithOffset(effect.Orientation.Z, -600.0f);
+                    var connector = (PositionConnector)new PositionConnector(effect.Position).WithOffset(effect.Orientation.Z, -600.0f, true);
                     var rotationConnector = new AngleConnector(effect.Rotation.Z);
                     // 30 units width is a guess
                     replay.Decorations.Add(new RectangleDecoration(true, 0, 30, 1200, lifespan, color.WithAlpha(0.5f).ToString(), connector).UsingRotationConnector(rotationConnector).UsingSkillMode(player, Spec.Virtuoso, ThousandCuts));
