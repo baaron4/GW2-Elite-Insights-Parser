6dc9f4744d3b970a8b8feca92b387744015046d5
EliphasNUIT <bichon.jekfer@gmail.com>
2023-09-11 23:24:39 +0200

    added BuffSourceFinder20221018 with adjusted imperial impact extension (undocumented change)

---
+++ GW2EIEvtcParser/EIData/Buffs/BuffSourceFinders/BuffSourceFinder20221018.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser.EIData.BuffSourceFinders
+{
+    internal class BuffSourceFinder20221018 : BuffSourceFinder20210921
+    {
+        public BuffSourceFinder20221018(HashSet<long> boonIds) : base(boonIds)
+        {
+            ImperialImpactExtension = 1000;
+        }
+
+    }
+}
--- GW2EIEvtcParser/ParserHelpers/ArcDPSEnums.cs
+++ GW2EIEvtcParser/ParserHelpers/ArcDPSEnums.cs
@@ -51,6 +51,7 @@ namespace GW2EIEvtcParser
             internal const ulong August2022BalanceHotFix = 132359;
             internal const ulong August2022Balance = 133322;
             internal const ulong October2022Balance = 135242;
+            internal const ulong October2022BalanceHotFix = 135930;
             internal const ulong November2022Balance = 137943;
             internal const ulong February2023Balance = 141374;
             internal const ulong May2023Balance = 145038;
