3092ca167a7d0fb0066622034f884f5af5bf0228
EliphasNUIT <bichon.jekfer@gmail.com>
2023-12-24 00:16:45 +0100

    added Single remove mechanics

---
+++ GW2EIEvtcParser/EIData/Mechanics/BuffMechanics/Remove/Friendly/Single/PlayerBuffRemoveSingleMechanic.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser.EIData
+{
+
+    internal abstract class PlayerBuffRemoveSingleMechanic : PlayerBuffRemoveMechanic<AbstractBuffRemoveEvent>
+    {
+        public PlayerBuffRemoveSingleMechanic(long mechanicID, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName) : base(mechanicID, inGameName, plotlySetting, shortName, description, fullName, 0)
+        {
+        }
+
+        public PlayerBuffRemoveSingleMechanic(long[] mechanicIDs, string inGameName, MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName) : base(mechanicIDs, inGameName, plotlySetting, shortName, description, fullName, 0)
+        {
+        }
+
+        internal override void CheckMechanic(ParsedEvtcLog log, Dictionary<Mechanic, List<MechanicEvent>> mechanicLogs, Dictionary<int, AbstractSingleActor> regroupedMobs)
+        {
+            foreach (long mechanicID in MechanicIDs)
+            {
+                foreach (AbstractBuffEvent c in log.CombatData.GetBuffData(mechanicID))
+                {
+                    if (c is AbstractBuffRemoveEvent abre && Keep(abre, log))
+                    {
+                        AbstractSingleActor amp = GetActor(log, GetAgentItem(abre), regroupedMobs);
+                        if (amp != null)
+                        {
+                            if (abre is BuffRemoveAllEvent brae)
+                            {
+                                for (int i = 0; i < brae.RemovedStacks; i++)
+                                {
+                                    AddMechanic(log, mechanicLogs, brae, amp);
+                                }
+                            }
+                            else if (abre is BuffRemoveSingleEvent brse)
+                            {
+                                AddMechanic(log, mechanicLogs, brse, amp);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
