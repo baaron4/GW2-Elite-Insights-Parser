ac8edf7e974529331e475ec7b0821ff6440c7c59
EliphasNUIT <bichon.jekfer@gmail.com>
2024-03-19 19:38:02 +0100

    updated March2024BalanceAndCerusLegendary value

--- GW2EIEvtcParser/ParserHelpers/ArcDPSEnums.cs
+++ GW2EIEvtcParser/ParserHelpers/ArcDPSEnums.cs
@@ -68,7 +68,7 @@ namespace GW2EIEvtcParser
             internal const ulong January2024Balance = 157732;
             internal const ulong February2024NewWeapons = 158837;
             internal const ulong February2024CerusCMHPFix = 158968;
-            internal const ulong March2024BalanceAndCerusLegendary = ulong.MaxValue;
+            internal const ulong March2024BalanceAndCerusLegendary = 159951;
             //
             internal const ulong EndOfLife = ulong.MaxValue;
         }
