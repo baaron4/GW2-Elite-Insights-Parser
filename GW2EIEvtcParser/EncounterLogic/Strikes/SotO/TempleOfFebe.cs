62aa8724a29ccc6e1c02719f2d0714f5eaf4ae3b
Linkaaaaa <christian.pagliarani@gmail.com>
2024-05-08 21:20:27 +0200

    Added 50-10% subphase to Cerus

--- GW2EIEvtcParser/EncounterLogic/Strikes/SotO/TempleOfFebe.cs
+++ GW2EIEvtcParser/EncounterLogic/Strikes/SotO/TempleOfFebe.cs
@@ -177,7 +177,8 @@ namespace GW2EIEvtcParser.EncounterLogic
                 return phases;
             }
             // Invul check
-            phases.AddRange(GetPhasesByInvul(log, InvulnerabilityCerus, mainTarget, true, true));
+            List<PhaseData> invulnPhases = GetPhasesByInvul(log, InvulnerabilityCerus, mainTarget, true, true);
+            phases.AddRange(invulnPhases);
             for (int i = 1; i < phases.Count; i++)
             {
                 PhaseData phase = phases[i];
@@ -208,6 +209,16 @@ namespace GW2EIEvtcParser.EncounterLogic
                 var phase = new PhaseData(enragedSmash.Time, log.FightData.FightEnd, "Enraged Smash");
                 phase.AddTarget(mainTarget);
                 phases.Add(phase);
+                // Sub Phase for 50-10%
+                for (int i = 1; i <= invulnPhases.Count; i++)
+                {
+                    if (i == 5)
+                    {
+                        var phase50_10 = new PhaseData(invulnPhases[i - 1].Start, enragedSmash.Time, "50-10%");
+                        phase50_10.AddTarget(mainTarget);
+                        phases.Add(phase50_10);
+                    }
+                }
             }
             return phases;
         }
