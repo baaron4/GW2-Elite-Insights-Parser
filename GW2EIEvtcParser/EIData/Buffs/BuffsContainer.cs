92e18bb68f2d1a16059be2a9ff4fffce8ad424ec
EliphasNUIT <bichon.jekfer@gmail.com>
2024-06-09 20:29:02 +0200

    added a bandaid

--- GW2EIEvtcParser/EIData/Buffs/BuffsContainer.cs
+++ GW2EIEvtcParser/EIData/Buffs/BuffsContainer.cs
@@ -162,6 +162,37 @@ namespace GW2EIEvtcParser.EIData
             BuffsBySource = currentBuffs.GroupBy(x => x.Source).ToDictionary(x => x.Key, x => (IReadOnlyList<Buff>)x.ToList());
             //
             _buffSourceFinder = GetBuffSourceFinder(combatData, new HashSet<long>(BuffsByClassification[BuffClassification.Boon].Select(x => x.ID)));
+            // Band aid for the stack type 0 situation
+            if (combatData.HasStackIDs)
+            {
+                var stackType0Buffs = currentBuffs.Where(x => x.StackType == BuffStackType.StackingConditionalLoss).ToList();
+                foreach (Buff buff in stackType0Buffs)
+                {
+                    IReadOnlyList<AbstractBuffEvent> buffData = combatData.GetBuffData(buff.ID);
+                    var buffDataByDst = buffData.GroupBy(x => x.To).ToDictionary(x => x.Key, x => x.ToList());
+                    foreach (KeyValuePair<AgentItem, List<AbstractBuffEvent>> pair in buffDataByDst)
+                    {
+                        var appliesPerInstanceID = pair.Value.OfType<BuffApplyEvent>().GroupBy(x => x.BuffInstance).ToDictionary(x => x.Key, x => x.ToList());
+                        var removeSinglesPerInstanceID = pair.Value.OfType<BuffRemoveSingleEvent>().Where(x => !x.OverstackOrNaturalEnd).GroupBy(x => x.BuffInstance).ToDictionary(x => x.Key, x => x.ToList());
+                        foreach (KeyValuePair<uint, List<BuffRemoveSingleEvent>> removePair in removeSinglesPerInstanceID)
+                        {
+                            if (appliesPerInstanceID.TryGetValue(removePair.Key, out List<BuffApplyEvent> applyList))
+                            {
+                                foreach (BuffRemoveSingleEvent remove in removePair.Value)
+                                {
+                                    BuffApplyEvent apply = applyList.LastOrDefault(x => x.Time <= remove.Time);
+                                    if (apply != null && apply.OriginalAppliedDuration == remove.RemovedDuration)
+                                    {
+                                        int activeTime = apply.OriginalAppliedDuration - apply.AppliedDuration;
+                                        int elapsedTime = (int)(remove.Time - apply.Time);
+                                        remove.OverrideRemovedDuration(remove.RemovedDuration - activeTime - elapsedTime); 
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         public bool TryGetBuffByName(string name, out Buff buff)
--- GW2EIEvtcParser/ParsedData/CombatEvents/BuffEvents/BuffRemoves/AbstractBuffRemoveEvent.cs
+++ GW2EIEvtcParser/ParsedData/CombatEvents/BuffEvents/BuffRemoves/AbstractBuffRemoveEvent.cs
@@ -1,10 +1,11 @@
-﻿using static GW2EIEvtcParser.ArcDPSEnums;
+﻿using System;
+using static GW2EIEvtcParser.ArcDPSEnums;
 
 namespace GW2EIEvtcParser.ParsedData
 {
     public abstract class AbstractBuffRemoveEvent : AbstractBuffEvent
     {
-        public int RemovedDuration { get; }
+        public int RemovedDuration { get; private set; }
 
         internal AbstractBuffRemoveEvent(CombatItem evtcItem, AgentData agentData, SkillData skillData) : base(evtcItem, skillData)
         {
@@ -18,6 +19,11 @@ namespace GW2EIEvtcParser.ParsedData
             RemovedDuration = removedDuration;
             By = by;
             To = to;
+        }
+
+        internal void OverrideRemovedDuration(int removedDuration)
+        {
+            RemovedDuration = Math.Max(removedDuration, 0);
         }
 
         internal override void TryFindSrc(ParsedEvtcLog log)
