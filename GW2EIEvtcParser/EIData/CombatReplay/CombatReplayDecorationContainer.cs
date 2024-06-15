a462393ce7445fbf14c37112e56ec7911c99bcb5
EliphasNUIT <bichon.jekfer@gmail.com>
2024-06-16 00:21:11 +0200

    structure change to conserve decoration add order

--- GW2EIEvtcParser/EIData/CombatReplay/CombatReplayDecorationContainer.cs
+++ GW2EIEvtcParser/EIData/CombatReplay/CombatReplayDecorationContainer.cs
@@ -6,12 +6,12 @@ namespace GW2EIEvtcParser.EIData
     public class CombatReplayDecorationContainer
     {
         private Dictionary<string, ConstantGenericDecoration> DecorationCache { get; }
-        private Dictionary<ConstantGenericDecoration, List<VariableGenericDecoration>> Decorations { get; }
+        private List<(ConstantGenericDecoration constant, VariableGenericDecoration variable)> Decorations { get; }
 
         internal CombatReplayDecorationContainer(Dictionary<string, ConstantGenericDecoration> cache)
         {
             DecorationCache = cache;
-            Decorations = new Dictionary<ConstantGenericDecoration, List<VariableGenericDecoration>>();
+            Decorations = new List<(ConstantGenericDecoration constant, VariableGenericDecoration variable)>();
         }
 
         internal void Add(GenericDecoration decoration)
@@ -27,25 +27,15 @@ namespace GW2EIEvtcParser.EIData
                 cached = constantPart;
                 DecorationCache[id] = constantPart;
             }
-            if (!Decorations.TryGetValue(cached, out List<VariableGenericDecoration> list))
-            {
-                Decorations[cached] = new List<VariableGenericDecoration> { decoration.VariableDecoration };
-            }
-            else
-            {
-                list.Add(decoration.VariableDecoration);
-            }
+            Decorations.Add((cached, decoration.VariableDecoration));
         }
 
         internal IReadOnlyList<GenericDecoration> ToList()
         {
             var result = new List<GenericDecoration>();
-            foreach (KeyValuePair<ConstantGenericDecoration, List<VariableGenericDecoration>> pair in Decorations)
+            foreach ((ConstantGenericDecoration constant, VariableGenericDecoration variable) in Decorations)
             {
-                foreach (VariableGenericDecoration decoration in pair.Value)
-                {
-                    result.Add(pair.Key.GetDecorationFromVariable(decoration));
-                }
+                result.Add(constant.GetDecorationFromVariable(variable));
             }
             return result;
         }
