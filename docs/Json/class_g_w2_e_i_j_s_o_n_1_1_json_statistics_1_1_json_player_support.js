909c96349fd8eb61f9c4c44d2f492af2dd1edb06
EliphasNUIT <bichon.jekfer@gmail.com>
2024-08-05 10:13:18 +0200

    updated Json docs

--- docs/Json/class_g_w2_e_i_j_s_o_n_1_1_json_statistics_1_1_json_player_support.js
+++ docs/Json/class_g_w2_e_i_j_s_o_n_1_1_json_statistics_1_1_json_player_support.js
@@ -6,6 +6,8 @@ var class_g_w2_e_i_j_s_o_n_1_1_json_statistics_1_1_json_player_support =
     [ "CondiCleanseSelf", "class_g_w2_e_i_j_s_o_n_1_1_json_statistics_1_1_json_player_support.html#af18d4004a889cbdfcac78aad6b88df2d", null ],
     [ "CondiCleanseTime", "class_g_w2_e_i_j_s_o_n_1_1_json_statistics_1_1_json_player_support.html#a17bcfaa8333f23d928c890adfb116655", null ],
     [ "CondiCleanseTimeSelf", "class_g_w2_e_i_j_s_o_n_1_1_json_statistics_1_1_json_player_support.html#a3eee930a005173b8bd46cd6eabbfc2e3", null ],
+    [ "RemovedStunDuration", "class_g_w2_e_i_j_s_o_n_1_1_json_statistics_1_1_json_player_support.html#a53c8f79b25f38dada166c80bdb0596af", null ],
     [ "Resurrects", "class_g_w2_e_i_j_s_o_n_1_1_json_statistics_1_1_json_player_support.html#a6118e3a2b81a703f76f6390f88c1ca46", null ],
-    [ "ResurrectTime", "class_g_w2_e_i_j_s_o_n_1_1_json_statistics_1_1_json_player_support.html#aa8df84edc2ba638f3e9b16dc6196c09b", null ]
+    [ "ResurrectTime", "class_g_w2_e_i_j_s_o_n_1_1_json_statistics_1_1_json_player_support.html#aa8df84edc2ba638f3e9b16dc6196c09b", null ],
+    [ "StunBreak", "class_g_w2_e_i_j_s_o_n_1_1_json_statistics_1_1_json_player_support.html#a4dc248f3f96641f692f52fff36f28977", null ]
 ];
