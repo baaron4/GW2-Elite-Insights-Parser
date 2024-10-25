bfa85430459dc5b4be82a6ee4e08f2377c9060a2
EliphasNUIT <bichon.jekfer@gmail.com>
2024-10-25 19:53:11 +0200

    version update

--- GW2EIParser/Properties/AssemblyInfo.cs
+++ GW2EIParser/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 #if !DEBUG
-[assembly: AssemblyVersion("2.73.3.2")]
+[assembly: AssemblyVersion("3.0.0.0")]
 #else
-[assembly: AssemblyVersion("2.73.*")]
+[assembly: AssemblyVersion("3.0.*")]
 #endif
 //[assembly: AssemblyFileVersion("1.8.10.34877")]
