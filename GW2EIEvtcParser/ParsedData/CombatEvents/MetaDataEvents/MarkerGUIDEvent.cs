c568ac8c140d9f6b81f2a727ab219f9e6ad47686
EliphasNUIT <bichon.jekfer@gmail.com>
2024-11-01 09:16:24 +0100

    cleanup

--- GW2EIEvtcParser/ParsedData/CombatEvents/MetaDataEvents/MarkerGUIDEvent.cs
+++ GW2EIEvtcParser/ParsedData/CombatEvents/MetaDataEvents/MarkerGUIDEvent.cs
@@ -6,14 +6,13 @@ namespace GW2EIEvtcParser.ParsedData
     {
         internal static MarkerGUIDEvent DummyMarkerGUID = new MarkerGUIDEvent();
 
-        private readonly bool _isCommanderTag = false;
-
-        public bool IsCommanderTag => _isCommanderTag || MarkerGUIDs.CommanderTagMarkersHexGUIDs.Contains(HexContentGUID);
+        public bool IsCommanderTag { get; }
         internal MarkerGUIDEvent(CombatItem evtcItem, EvtcVersionEvent evtcVersion) : base(evtcItem)
         {
+            IsCommanderTag = MarkerGUIDs.CommanderTagMarkersHexGUIDs.Contains(HexContentGUID);
             if (evtcVersion.Build >= ArcDPSEnums.ArcDPSBuilds.ExtraDataInGUIDEvents)
             {
-                _isCommanderTag = evtcItem.SrcInstid == 1;
+                IsCommanderTag |= evtcItem.SrcInstid == 1;
             }
         }
         internal MarkerGUIDEvent() : base()
