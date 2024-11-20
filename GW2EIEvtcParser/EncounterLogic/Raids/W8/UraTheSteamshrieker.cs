b02be962e8001607213012c5ced4ba28a01e66a6
EliphasNUIT <bichon.jekfer@gmail.com>
2024-11-20 11:51:04 +0100

    added icons for Ura

--- GW2EIEvtcParser/EncounterLogic/Raids/W8/UraTheSteamshrieker.cs
+++ GW2EIEvtcParser/EncounterLogic/Raids/W8/UraTheSteamshrieker.cs
@@ -1,6 +1,12 @@
 ﻿
+using GW2EIEvtcParser.EIData;
+using GW2EIEvtcParser.Exceptions;
 using GW2EIEvtcParser.ParsedData;
+using GW2EIEvtcParser.ParserHelpers;
 using static GW2EIEvtcParser.EncounterLogic.EncounterCategory;
+using static GW2EIEvtcParser.EncounterLogic.EncounterImages;
+using static GW2EIEvtcParser.EncounterLogic.EncounterLogicPhaseUtils;
+using static GW2EIEvtcParser.SkillIDs;
 
 namespace GW2EIEvtcParser.EncounterLogic;
 
@@ -9,12 +15,8 @@ internal class UraTheSteamshrieker : MountBalrior
     public UraTheSteamshrieker(int triggerID) : base(triggerID)
     {
         Extension = "ura";
+        Icon = EncounterIconUra;
         EncounterCategoryInformation.InSubCategoryOrder = 2;
         EncounterID |= 0x000003;
-    }
-
-    internal override string GetLogicName(CombatData combatData, AgentData agentData)
-    {
-        return "Ura, the Steamshrieker";
     }
 }
