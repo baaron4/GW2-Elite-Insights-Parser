443b34c5c41113c58cb4dd6e4f8b528d55257028
EliphasNUIT <bichon.jekfer@gmail.com>
2024-11-21 11:01:59 +0100

    crash fix

--- GW2EIEvtcParser/EIData/CombatReplay/CombatReplayDescription/Connector/ConnectorDescription.cs
+++ GW2EIEvtcParser/EIData/CombatReplay/CombatReplayDescription/Connector/ConnectorDescription.cs
@@ -4,12 +4,10 @@ namespace GW2EIEvtcParser.EIData;
 
 [JsonPolymorphic(UnknownDerivedTypeHandling = JsonUnknownDerivedTypeHandling.FailSerialization)]
 [JsonDerivedType(typeof(SkillConnectorDescription))]
-[JsonDerivedType(typeof(RotationConnectorDescription))]
 [JsonDerivedType(typeof(AngleConnectorDescription))]
 [JsonDerivedType(typeof(AngleInterpolationConnectorDescription))]
 [JsonDerivedType(typeof(AgentFacingAgentConnectorDescription))]
 [JsonDerivedType(typeof(AgentFacingConnectorDescription))]
-[JsonDerivedType(typeof(GeographicalConnectorDescription))]
 [JsonDerivedType(typeof(PositionConnectorDescription))]
 [JsonDerivedType(typeof(InterpolationConnectorDescription))]
 [JsonDerivedType(typeof(AgentConnectorDescription))]
