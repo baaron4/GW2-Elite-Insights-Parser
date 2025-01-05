84ce702724c2789d476c4da5d95af63062629491
EliphasNUIT <bichon.jekfer@gmail.com>
2025-01-05 14:50:34 +0100

    added screen space connector

---
+++ GW2EIEvtcParser/EIData/CombatReplay/CombatReplayDescription/Connector/ScreenSpaceConnectorDescription.cs
@@ -0,0 +1,12 @@
+﻿namespace GW2EIEvtcParser.EIData;
+
+public class ScreenSpaceConnectorDescription : ConnectorDescription
+{
+    public readonly IReadOnlyList<float> Position;
+    public readonly bool IsScreenSpace;
+    internal ScreenSpaceConnectorDescription(ScreenSpaceConnector connector, CombatReplayMap map, ParsedEvtcLog log) : base(connector, map, log)
+    {
+        IsScreenSpace = true;
+        Position = [connector.Position.X, connector.Position.Y]; 
+    }
+}
---
+++ GW2EIEvtcParser/EIData/CombatReplay/Decorations/Connector/ScreenSpaceConnector.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace GW2EIEvtcParser.EIData;
+
+internal class ScreenSpaceConnector(in Vector2 position) : Connector
+{
+    public readonly Vector2 Position = position;
+
+    public override ConnectorDescription GetConnectedTo(CombatReplayMap map, ParsedEvtcLog log)
+    {
+        return new ScreenSpaceConnectorDescription(this, map, log);
+    }
+}
