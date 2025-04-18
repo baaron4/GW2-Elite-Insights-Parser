aa0f3e103c342da116ee8eedc95a361c864a6003
EliphasNUIT <bichon.jekfer@gmail.com>
2025-04-18 15:19:46 +0200

    restored spotter url

--- GW2EIEvtcParser/ParserHelpers/Images/TraitImages.cs
+++ GW2EIEvtcParser/ParserHelpers/Images/TraitImages.cs
@@ -155,7 +155,7 @@ internal static class TraitImages
     public const string OakheartSalve = "https://wiki.guildwars2.com/images/4/4d/Oakheart_Salve.png";
     public const string SecondSkin = "https://wiki.guildwars2.com/images/5/51/Second_Skin.png";
     public const string ImplacableFoe = "https://wiki.guildwars2.com/images/7/79/Implacable_Foe.png";
-    public const string Spotter = "https://render.guildwars2.com/file/F56EB274FA516609317A6540ABC20B6116C8051D/3613096.png";
+    public const string Spotter = "https://wiki.guildwars2.com/images/b/b0/Spotter.png";
     public const string FangAndClaw = "https://render.guildwars2.com/file/F56EB274FA516609317A6540ABC20B6116C8051D/3613096.png";
     public const string Wolfsong = "https://render.guildwars2.com/file/55D102432B71E1E26280DF6118AE5663650FEAA3/3613094.png";
     public const string BeastlyWarden = "https://render.guildwars2.com/file/EF61D1061135B1EC57FDFD94795ED125D43962E1/1012612.png";
