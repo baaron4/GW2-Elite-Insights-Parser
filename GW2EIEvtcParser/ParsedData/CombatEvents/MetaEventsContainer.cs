e9fda6bc5d26f2f108e4f02580b5e3413ef0d36e
EliphasNUIT <bichon.jekfer@gmail.com>
2025-05-06 11:14:37 +0300

    moved some files around

--- GW2EIEvtcParser/ParsedData/CombatEvents/MetaEventsContainer.cs
+++ GW2EIEvtcParser/ParsedData/CombatEvents/MetaEventsContainer.cs
@@ -16,17 +16,25 @@ internal class MetaEventsContainer
     public PointOfViewEvent? PointOfViewEvent { get; set; }
     public FractalScaleEvent? FractalScaleEvent { get; set; }
     public EvtcVersionEvent? EvtcVersionEvent { get; set; }
+
     public readonly Dictionary<AgentItem, List<GuildEvent>> GuildEvents = [];
+
     public readonly Dictionary<long, BuffInfoEvent> BuffInfoEvents = [];
     public readonly Dictionary<byte, List<BuffInfoEvent>> BuffInfoEventsByCategory = [];
+
     public readonly Dictionary<long, SkillInfoEvent> SkillInfoEvents = [];
+
     public readonly List<ErrorEvent> ErrorEvents = [];
+
     public readonly Dictionary<long, EffectGUIDEvent> EffectGUIDEventsByEffectID = [];
     public readonly Dictionary<GUID, EffectGUIDEvent> EffectGUIDEventsByGUID = [];
+
     public readonly Dictionary<long, MarkerGUIDEvent> MarkerGUIDEventsByMarkerID = [];
     public readonly Dictionary<GUID, MarkerGUIDEvent> MarkerGUIDEventsByGUID = [];
+
     public readonly Dictionary<long, SpeciesGUIDEvent> SpeciesGUIDEventsBySpeciesID = [];
     public readonly Dictionary<GUID, SpeciesGUIDEvent> SpeciesGUIDEventsByGUID = [];
+
     public readonly Dictionary<long, SkillGUIDEvent> SkillGUIDEventsBySkillID = [];
     public readonly Dictionary<GUID, SkillGUIDEvent> SkillGUIDEventsByGUID = [];
 }
