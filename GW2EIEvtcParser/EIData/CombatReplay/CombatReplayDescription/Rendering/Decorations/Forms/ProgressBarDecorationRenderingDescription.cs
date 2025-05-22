f14a37ee814462bf5697781eea797bb105110805
EliphasNUIT <bichon.jekfer@gmail.com>
2025-05-22 19:38:18 +0200

    fixed progress bars

--- GW2EIEvtcParser/EIData/CombatReplay/CombatReplayDescription/Rendering/Decorations/Forms/ProgressBarDecorationRenderingDescription.cs
+++ GW2EIEvtcParser/EIData/CombatReplay/CombatReplayDescription/Rendering/Decorations/Forms/ProgressBarDecorationRenderingDescription.cs
@@ -6,12 +6,12 @@ namespace GW2EIEvtcParser.EIData;
 public class ProgressBarDecorationRenderingDescription : RectangleDecorationRenderingDescription
 {
 
-    public readonly IReadOnlyList<(long, double)> Progress;
+    public readonly IReadOnlyList<double[]> Progress;
     public int InterpolationMethod { get; private set; }
     internal ProgressBarDecorationRenderingDescription(ParsedEvtcLog log, ProgressBarDecorationRenderingData decoration, CombatReplayMap map, Dictionary<long, SkillItem> usedSkills, Dictionary<long, Buff> usedBuffs, string metadataSignature) : base(log, decoration, map, usedSkills, usedBuffs, metadataSignature)
     {
         Type = Types.ProgressBar;
-        Progress = decoration.Progress;
+        Progress = decoration.Progress.Select(x => new double[] { x.Item1, x.Item2 }).ToList();
         InterpolationMethod = (int)decoration.Method;
     }
 }
