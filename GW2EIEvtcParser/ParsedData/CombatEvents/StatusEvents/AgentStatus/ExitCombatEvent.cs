c731643b2104ea9a44cc2399aa8773f1a3edfc87
EliphasNUIT <bichon.jekfer@gmail.com>
2025-06-04 20:08:13 +0200

    added subgroup, spec and basespec to exit combat

--- GW2EIEvtcParser/ParsedData/CombatEvents/StatusEvents/AgentStatus/ExitCombatEvent.cs
+++ GW2EIEvtcParser/ParsedData/CombatEvents/StatusEvents/AgentStatus/ExitCombatEvent.cs
@@ -1,10 +1,17 @@
-﻿namespace GW2EIEvtcParser.ParsedData;
+﻿using static GW2EIEvtcParser.ParserHelper;
+
+namespace GW2EIEvtcParser.ParsedData;
 
 public class ExitCombatEvent : StatusEvent
 {
+    public readonly int Subgroup;
+    public readonly Spec Spec = Spec.Unknown;
+    public readonly Spec BaseSpec = Spec.Unknown;
     internal ExitCombatEvent(CombatItem evtcItem, AgentData agentData) : base(evtcItem, agentData)
     {
-
+        Subgroup = (int)evtcItem.DstAgent;
+        Spec = ProfToSpec(agentData.GetSpec((uint)evtcItem.Value, (uint)evtcItem.BuffDmg));
+        BaseSpec = SpecToBaseSpec(Spec);
     }
 
 }
