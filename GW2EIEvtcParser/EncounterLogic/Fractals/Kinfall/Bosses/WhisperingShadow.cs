40f2e616e568d001fcb531dba4821ed79f6cdc05
Linkaaaaa <christian.pagliarani@gmail.com>
2025-06-29 09:53:38 +0200

    Adjust Undying Light post patch

--- GW2EIEvtcParser/EncounterLogic/Fractals/Kinfall/Bosses/WhisperingShadow.cs
+++ GW2EIEvtcParser/EncounterLogic/Fractals/Kinfall/Bosses/WhisperingShadow.cs
@@ -4,7 +4,6 @@ using GW2EIEvtcParser.ParsedData;
 using static GW2EIEvtcParser.EncounterLogic.EncounterLogicUtils;
 using static GW2EIEvtcParser.EncounterLogic.EncounterLogicPhaseUtils;
 using static GW2EIEvtcParser.ParserHelpers.EncounterImages;
-using static GW2EIEvtcParser.ParserHelper;
 using static GW2EIEvtcParser.SpeciesIDs;
 using static GW2EIEvtcParser.SkillIDs;
 using GW2EIEvtcParser.ParserHelpers;
@@ -305,19 +304,11 @@ internal class WhisperingShadow : Kinfall
         
         if (log.FightData.IsCM && log.CombatData.GetBuffData(AchievementEligibilityUndyingLight).Any())
         {
-            int counter = 0;
-            foreach (Player p in log.PlayerList)
-            {
-                if (p.HasBuff(log, AchievementEligibilityUndyingLight, log.FightData.FightEnd - ServerDelayConstant))
-                {
-                    counter++;
-                }
-            }
-            // The achievement requires 5 players alive with the buff, if the instance has only 4 players inside, you cannot get it.
-            if (counter == 5)
-            {
-                InstanceBuffs.Add((log.Buffs.BuffsByIds[AchievementEligibilityUndyingLight], 1));
-            }
+            // The achievement requires 5 players alive and in the instance from the moment challenge mode is activated until the end.
+            // The buff is present only on the players that do not have the achievement yet.
+            // If any player dies, the buff is removed from everyone.
+            // We don't check if players died during the encounter because the elibility is valid for the entire fractal.
+            InstanceBuffs.Add((log.Buffs.BuffsByIds[AchievementEligibilityUndyingLight], 1));
         }
     }
 }
