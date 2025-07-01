7fa0483fadcd655a89902f1e6e5523fd6ec2f757
EliphasNUIT <bichon.jekfer@gmail.com>
2025-07-01 11:20:09 +0200

    w7 instance ground work

--- GW2EIEvtcParser/EncounterLogic/Raids/W7/TheKeyOfAhdashimInstance.cs
+++ GW2EIEvtcParser/EncounterLogic/Raids/W7/TheKeyOfAhdashimInstance.cs
@@ -1,8 +1,14 @@
-﻿using GW2EIEvtcParser.EIData;
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using GW2EIEvtcParser.EIData;
+using GW2EIEvtcParser.Extensions;
 using GW2EIEvtcParser.ParsedData;
 using static GW2EIEvtcParser.ArcDPSEnums;
 using static GW2EIEvtcParser.EncounterLogic.EncounterLogicPhaseUtils;
 using static GW2EIEvtcParser.EncounterLogic.EncounterLogicTimeUtils;
+using static GW2EIEvtcParser.EncounterLogic.EncounterLogicUtils;
+using static GW2EIEvtcParser.ParserHelper;
 using static GW2EIEvtcParser.ParserHelpers.EncounterImages;
 using static GW2EIEvtcParser.SpeciesIDs;
 
@@ -10,11 +16,25 @@ namespace GW2EIEvtcParser.EncounterLogic;
 
 internal class TheKeyOfAhdashimInstance : TheKeyOfAhdashim
 {
+    private readonly Adina _adina;
+    private readonly Sabir _sabir;
+    private readonly PeerlessQadim _peerlessQadim;
+
+    private readonly IReadOnlyList<TheKeyOfAhdashim> _subLogics;
     public TheKeyOfAhdashimInstance(int triggerID) : base(triggerID)
     {
         EncounterID = EncounterIDs.EncounterMasks.Unsupported;
         Icon = InstanceIconTheKeyOfAhdashim;
-        Extension = "keyadash";
+        Extension = "keyadash"; 
+        
+        _adina = new Adina((int)TargetID.Adina);
+        _sabir = new Sabir((int)TargetID.Sabir);
+        _peerlessQadim = new PeerlessQadim((int)TargetID.PeerlessQadim);
+        _subLogics = [_adina, _sabir, _peerlessQadim];
+
+        MechanicList.Add(_adina.Mechanics);
+        MechanicList.Add(_sabir.Mechanics);
+        MechanicList.Add(_peerlessQadim.Mechanics);
     }
 
     internal override string GetLogicName(CombatData combatData, AgentData agentData)
@@ -22,13 +42,122 @@ internal class TheKeyOfAhdashimInstance : TheKeyOfAhdashim
         return "The Key Of Ahdashim";
     }
 
+    internal override List<PhaseData> GetPhases(ParsedEvtcLog log, bool requirePhases)
+    {
+        return base.GetPhases(log, requirePhases);
+    }
+
+    internal override List<InstantCastFinder> GetInstantCastFinders()
+    {
+        List<InstantCastFinder> finders = [
+            .. _adina.GetInstantCastFinders(),
+            .. _sabir.GetInstantCastFinders(),
+            .. _peerlessQadim.GetInstantCastFinders()
+        ];
+        return finders;
+    }
+
+    internal override IReadOnlyList<TargetID> GetTrashMobsIDs()
+    {
+        List<TargetID> trashes = [
+            .. _adina.GetTrashMobsIDs(),
+            .. _sabir.GetTrashMobsIDs(),
+            .. _peerlessQadim.GetTrashMobsIDs()
+        ];
+        return trashes.Distinct().ToList();
+    }
     internal override IReadOnlyList<TargetID> GetTargetsIDs()
     {
-        return
-        [
-            TargetID.Adina,
-            TargetID.Sabir,
-            TargetID.PeerlessQadim,
+        List<TargetID> targets = [
+            .. _adina.GetTargetsIDs(),
+            .. _sabir.GetTargetsIDs(),
+            .. _peerlessQadim.GetTargetsIDs()
         ];
+        targets.Add(TargetID.Instance);
+        return targets.Distinct().ToList();
+    }
+
+    internal override IReadOnlyList<TargetID> GetFriendlyNPCIDs()
+    {
+        List<TargetID> friendlies = [
+            .. _adina.GetFriendlyNPCIDs(),
+            .. _sabir.GetFriendlyNPCIDs(),
+            .. _peerlessQadim.GetFriendlyNPCIDs()
+        ];
+        return friendlies.Distinct().ToList();
+    }
+
+    internal override void EIEvtcParse(ulong gw2Build, EvtcVersionEvent evtcVersion, FightData fightData, AgentData agentData, List<CombatItem> combatData, IReadOnlyDictionary<uint, ExtensionHandler> extensions)
+    {
+        base.EIEvtcParse(gw2Build, evtcVersion, fightData, agentData, combatData, extensions);
+    }
+
+    internal override List<BuffEvent> SpecialBuffEventProcess(CombatData combatData, SkillData skillData)
+    {
+        var res = new List<BuffEvent>();
+        foreach (var subLogic in _subLogics)
+        {
+            res.AddRange(subLogic.SpecialBuffEventProcess(combatData, skillData));
+        }
+        return res;
+    }
+
+    internal override List<CastEvent> SpecialCastEventProcess(CombatData combatData, SkillData skillData)
+    {
+        var res = new List<CastEvent>();
+        foreach (var subLogic in _subLogics)
+        {
+            res.AddRange(subLogic.SpecialCastEventProcess(combatData, skillData));
+        }
+        return res;
+    }
+
+    internal override List<HealthDamageEvent> SpecialDamageEventProcess(CombatData combatData, AgentData agentData, SkillData skillData)
+    {
+        var res = new List<HealthDamageEvent>();
+        foreach (var subLogic in _subLogics)
+        {
+            res.AddRange(subLogic.SpecialDamageEventProcess(combatData, agentData, skillData));
+        }
+        return res;
+    }
+
+    // TODO: handle duplicates due multiple base method calls in Combat Replay methods
+    internal override void ComputeNPCCombatReplayActors(NPC target, ParsedEvtcLog log, CombatReplay replay)
+    {
+        base.ComputeNPCCombatReplayActors(target, log, replay);
+        foreach (var logic in _subLogics)
+        {
+            logic.ComputeNPCCombatReplayActors(target, log, replay);
+        }
+    }
+
+    internal override void ComputePlayerCombatReplayActors(PlayerActor p, ParsedEvtcLog log, CombatReplay replay)
+    {
+        base.ComputePlayerCombatReplayActors(p, log, replay);
+        foreach (var logic in _subLogics)
+        {
+            logic.ComputePlayerCombatReplayActors(p, log, replay);
+        }
+    }
+
+    internal override void ComputeEnvironmentCombatReplayDecorations(ParsedEvtcLog log, CombatReplayDecorationContainer environmentDecorations)
+    {
+        base.ComputeEnvironmentCombatReplayDecorations(log, environmentDecorations);
+        foreach (var logic in _subLogics)
+        {
+            logic.ComputeEnvironmentCombatReplayDecorations(log, environmentDecorations);
+        }
+    }
+
+    internal override Dictionary<TargetID, int> GetTargetsSortIDs()
+    {
+        var sortIDs = new Dictionary<TargetID, int>();
+        int offset = 0;
+        foreach (var logic in _subLogics)
+        {
+            offset = AddSortIDWithOffset(sortIDs, logic.GetTargetsSortIDs(), offset);
+        }
+        return sortIDs;
     }
 }
