baa51463f62e9aceadaf8062863ae09e76bc4f13
EliphasNUIT <bichon.jekfer@gmail.com>
2025-07-29 10:17:26 +0200

    merge fix

--- GW2EIEvtcParser/ParsedEvtcLog.cs
+++ GW2EIEvtcParser/ParsedEvtcLog.cs
@@ -77,7 +77,7 @@ public class ParsedEvtcLog
             {
                 if (CombatData.GetDamageTakenData(p.AgentItem).Any(x => !x.ToFriendly) ||
                     CombatData.GetDamageData(p.AgentItem).Any(x => !x.ToFriendly) ||
-                    CombatData.GetBuffDataBySrc(p.AgentItem).Any(x => x.To.GetFinalMaster() != p.AgentItem))
+                    CombatData.GetBuffDataBySrc(p.AgentItem).Any(x => x.To.GetFinalMaster() != p.EnglobingAgentItem))
                 {
                     activePlayers.Add(p);
                 }
