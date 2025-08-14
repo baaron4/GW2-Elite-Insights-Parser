f20eee2dc0616aeb5ff8ac4911dca29d5fa66fe5
EliphasNUIT <bichon.jekfer@gmail.com>
2025-08-14 18:03:38 +0200

    restored UpdatePlayersSpecAndGroup for instance and wvw logs

--- GW2EIEvtcParser/LogLogic/LogLogic.cs
+++ GW2EIEvtcParser/LogLogic/LogLogic.cs
@@ -286,6 +286,20 @@ public abstract class LogLogic
     internal virtual void UpdatePlayersSpecAndGroup(IReadOnlyList<Player> players, CombatData combatData, LogData logData)
     {
         //
+        if (IsInstance || ParseMode == ParseModeEnum.WvW)
+        {
+            foreach (Player p in players)
+            {
+                // We get the first enter combat for the player, we ignore it however if there was an exit combat before it as that means the player was already in combat at log start
+                var enterCombat = combatData.GetEnterCombatEvents(p.AgentItem).FirstOrDefault(x => x.Spec != Spec.Unknown);
+                if (enterCombat != null && enterCombat.Subgroup > 0 && !combatData.GetExitCombatEvents(p.AgentItem).Any(x => x.Time < enterCombat.Time))
+                {
+                    p.AgentItem.OverrideSpec(enterCombat.Spec);
+                    p.OverrideGroup(enterCombat.Subgroup);
+                }
+            }
+            return;
+        }
         long threshold = logData.LogStart + 5000;
         foreach (Player p in players)
         {
