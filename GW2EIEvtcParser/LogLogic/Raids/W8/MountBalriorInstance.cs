2d1dde3e8e078e4cd2809d0ba3482517f4502d18
EliphasNUIT <bichon.jekfer@gmail.com>
2025-09-04 17:27:32 +0200

    removed useless empty space

--- GW2EIEvtcParser/LogLogic/Raids/W8/MountBalriorInstance.cs
+++ GW2EIEvtcParser/LogLogic/Raids/W8/MountBalriorInstance.cs
@@ -82,7 +82,7 @@ internal class MountBalriorInstance : MountBalrior
                     success = true;
                 }
                 var isCM = greer.GetHealth(log.CombatData) > 35e6;
-                var name = (isCM ? "Godspoil Greer " : "Greer, the Blightbringer ") + (greers.Count > 0 ? (offset) : "");
+                var name = (isCM ? "Godspoil Greer" : "Greer, the Blightbringer") + (greers.Count > 0 ? " " + (offset) : "");
                 greer.OverrideName(name);
                 AddInstanceEncounterPhase(log, phases, encounterPhases, [greer], [..greeAndRegs, ..protoGreelings], eregs, mainPhase, name, start, end, success, _greer, isCM ? LogData.LogMode.CMNoName : LogData.LogMode.Normal);
             }
@@ -184,7 +184,7 @@ internal class MountBalriorInstance : MountBalrior
                 encounterThreshold = end;
                 var maxHP = ura.GetHealth(log.CombatData);
                 var isCM = maxHP > 70e6;
-                var name = (isCM ? "Godscream Ura " : "Ura, the Steamshrieker ") + (uras.Count > 0 ? (offset) : "");
+                var name = (isCM ? "Godscream Ura" : "Ura, the Steamshrieker") + (uras.Count > 0 ? " " + (offset) : "");
                 ura.OverrideName(name);
                 AddInstanceEncounterPhase(log, phases, encounterPhases, [ura], [], [], mainPhase, name, start, end, success, _ura, isCM ? (maxHP > 100e6 ? LogData.LogMode.LegendaryCM : LogData.LogMode.CMNoName) : LogData.LogMode.Normal);
             }
