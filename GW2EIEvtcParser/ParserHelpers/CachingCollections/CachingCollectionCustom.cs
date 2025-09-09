092c29371039eeddbc0c804270a5f5fe148b1aa1
EliphasNUIT <bichon.jekfer@gmail.com>
2025-09-09 23:08:08 +0200

    added TryGetEnglobingValue to caching collections

--- GW2EIEvtcParser/ParserHelpers/CachingCollections/CachingCollectionCustom.cs
+++ GW2EIEvtcParser/ParserHelpers/CachingCollections/CachingCollectionCustom.cs
@@ -29,6 +29,26 @@ public class CachingCollectionCustom<Q, T>(ParsedEvtcLog log, Q nullValue, int i
         return false;
     }
 
+    public bool TryGetEnglobingValue(long start, long end, Q? q, [NotNullWhen(true)] out T? value)
+    {
+        (start, end) = SanitizeTimes(start, end);
+        q = q == null ? _nullValue : q;
+        var englobingStart = _cache.Keys.Where(x => x <= start).DefaultIfEmpty(0).Max();
+        if (_cache.TryGetValue(englobingStart, out var subCache))
+        {
+            var englobingEnd = subCache.Keys.Where(x => x >= end).DefaultIfEmpty(0).Min();
+            if (subCache.TryGetValue(englobingEnd, out var subSubCache))
+            {
+                if (subSubCache.TryGetValue(q, out value!))
+                {
+                    return true;
+                }
+            }
+        }
+        value = default;
+        return false;
+    }
+
     public void Set(long start, long end, Q? q, T value)
     {
         (start, end) = SanitizeTimes(start, end);
