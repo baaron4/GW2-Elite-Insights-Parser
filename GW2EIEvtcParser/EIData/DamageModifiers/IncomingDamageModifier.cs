d937c9a5811bb3f99ff9c160dc34cf96ecadf539
EliphasNUIT <bichon.jekfer@gmail.com>
2025-09-09 23:08:54 +0200

    return type change

--- GW2EIEvtcParser/EIData/DamageModifiers/IncomingDamageModifier.cs
+++ GW2EIEvtcParser/EIData/DamageModifiers/IncomingDamageModifier.cs
@@ -32,7 +32,7 @@ public class IncomingDamageModifier : DamageModifier
         };
     }
 
-    public override IEnumerable<HealthDamageEvent> GetHitDamageEvents(SingleActor actor, ParsedEvtcLog log, SingleActor? t, long start, long end)
+    public override IReadOnlyList<HealthDamageEvent> GetHitDamageEvents(SingleActor actor, ParsedEvtcLog log, SingleActor? t, long start, long end)
     {
         return actor.GetHitDamageTakenEvents(t, log, start, end, SrcType);
     }
