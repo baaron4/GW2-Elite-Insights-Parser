3ed432fb2d1846358b724366d3e53fe9e7affddb
EliphasNUIT <bichon.jekfer@gmail.com>
2025-09-12 15:15:04 +0200

    added LonelyTowerInstance file

---
+++ GW2EIEvtcParser/LogLogic/Fractals/TheLonelyTower/LonelyTowerInstance.cs
@@ -0,0 +1,37 @@
+﻿using GW2EIEvtcParser.EIData;
+using GW2EIEvtcParser.ParsedData;
+using static GW2EIEvtcParser.ArcDPSEnums;
+using static GW2EIEvtcParser.LogLogic.LogLogicPhaseUtils;
+using static GW2EIEvtcParser.LogLogic.LogLogicTimeUtils;
+using static GW2EIEvtcParser.ParserHelpers.LogImages;
+using static GW2EIEvtcParser.SpeciesIDs;
+
+namespace GW2EIEvtcParser.LogLogic;
+
+internal class LonelyTowerInstance : LonelyTower
+{
+    public LonelyTowerInstance(int triggerID) : base(triggerID)
+    {
+        LogID = LogIDs.LogMasks.Unsupported;
+        Extension = "lnlytw";
+    }
+
+    internal override string GetLogicName(CombatData combatData, AgentData agentData)
+    {
+        return "Lonely Tower";
+    }
+
+    internal override IReadOnlyList<TargetID> GetTargetsIDs()
+    {
+        return
+        [
+            TargetID.CerusLonelyTower,
+            TargetID.DeimosLonelyTower,
+            TargetID.EparchLonelyTower,
+        ];
+    }
+    internal override void CheckSuccess(CombatData combatData, AgentData agentData, LogData logData, IReadOnlyCollection<AgentItem> playerAgents)
+    {
+        logData.SetSuccess(true, logData.LogEnd);
+    }
+}
