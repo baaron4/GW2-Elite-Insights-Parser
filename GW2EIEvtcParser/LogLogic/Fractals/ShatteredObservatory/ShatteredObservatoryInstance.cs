2e191406d0e86740a6cf3074eaa90335513459bc
EliphasNUIT <bichon.jekfer@gmail.com>
2026-01-10 17:52:37 +0100

    added ComputeAchievementEligibilityEvents to all supported instance log logics

--- GW2EIEvtcParser/LogLogic/Fractals/ShatteredObservatory/ShatteredObservatoryInstance.cs
+++ GW2EIEvtcParser/LogLogic/Fractals/ShatteredObservatory/ShatteredObservatoryInstance.cs
@@ -337,6 +337,14 @@ internal class ShatteredObservatoryInstance : ShatteredObservatory
             logic.SetInstanceBuffs(log, instanceBuffs);
         }
     }
+    internal override void ComputeAchievementEligibilityEvents(ParsedEvtcLog log, Player p, List<AchievementEligibilityEvent> achievementEligibilityEvents)
+    {
+        base.ComputeAchievementEligibilityEvents(log, p, achievementEligibilityEvents);
+        foreach (var logic in _subLogics)
+        {
+            logic.ComputeAchievementEligibilityEvents(log, p, achievementEligibilityEvents);
+        }
+    }
 
     internal override Dictionary<TargetID, int> GetTargetsSortIDs()
     {
