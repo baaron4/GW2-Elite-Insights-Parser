7a06986f7cf30752cc0af17bf9986406cb14606f
EliphasNUIT <bichon.jekfer@gmail.com>
2026-01-27 00:16:30 +0100

    added GainComputerByStackPlusConstant

---
+++ GW2EIEvtcParser/EIData/DamageModifiers/GainComputers/GainComputerByStackPlusConstant.cs
@@ -0,0 +1,15 @@
+﻿namespace GW2EIEvtcParser.EIData;
+
+internal class GainComputerByStackPlusConstant : GainComputerByStack
+{
+    private readonly double ConstantGain = 0;
+    public GainComputerByStackPlusConstant(double constant) : base()
+    {
+        ConstantGain = constant;
+    }
+
+    public override double ComputeGain(double gainPerStack, int stack)
+    {
+        return (gainPerStack * stack + ConstantGain) / (100 + (stack * gainPerStack + ConstantGain));
+    }
+}
