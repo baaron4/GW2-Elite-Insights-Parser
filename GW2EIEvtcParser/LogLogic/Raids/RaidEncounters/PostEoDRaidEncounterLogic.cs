6632725bfaae90ff5116aa5a1e31d2b5e47251d1
EliphasNUIT <bichon.jekfer@gmail.com>
2026-02-02 16:18:08 +0100

    small refactoring

---
+++ GW2EIEvtcParser/LogLogic/Raids/RaidEncounters/PostEoDRaidEncounterLogic.cs
@@ -0,0 +1,33 @@
+﻿using GW2EIEvtcParser.EIData;
+using GW2EIEvtcParser.ParsedData;
+using static GW2EIEvtcParser.ArcDPSEnums;
+using static GW2EIEvtcParser.LogLogic.LogLogicTimeUtils;
+
+namespace GW2EIEvtcParser.LogLogic;
+
+internal abstract class PostEoDRaidEncounterLogic : RaidEncounterLogic
+{
+
+    protected PostEoDRaidEncounterLogic(int triggerID) : base(triggerID)
+    {
+    }
+
+    internal override void CheckSuccess(CombatData combatData, AgentData agentData, LogData logData, IReadOnlyCollection<AgentItem> playerAgents, LogData.LogSuccessHandler successHandler)
+    {
+        if (IsInstance)
+        {
+            successHandler.SetSuccess(true, GetFinalMapChangeTime(logData, combatData));
+            return;
+        }
+        IReadOnlyList<RewardEvent> rewards = combatData.GetRewardEvents();
+        RewardEvent? reward = rewards.FirstOrDefault(x => x.RewardType == RewardTypes.PostEoDRaidEncounterReward && x.Time > logData.LogStart);
+        if (reward != null)
+        {
+            successHandler.SetSuccess(true, reward.Time);
+        }
+        else
+        {
+            NoBouncyChestGenericCheckSucess(combatData, agentData, logData, playerAgents, successHandler);
+        }
+    }
+}
--- GW2EIEvtcParser/LogLogic/Raids/RaidEncounters/SotO/SecretOfTheObscureRaidEncounter.cs
+++ GW2EIEvtcParser/LogLogic/Raids/RaidEncounters/SotO/SecretOfTheObscureRaidEncounter.cs
@@ -5,30 +5,11 @@ using static GW2EIEvtcParser.LogLogic.LogLogicTimeUtils;
 
 namespace GW2EIEvtcParser.LogLogic;
 
-internal abstract class SecretOfTheObscureRaidEncounter : RaidEncounterLogic
+internal abstract class SecretOfTheObscureRaidEncounter : PostEoDRaidEncounterLogic
 {
     public SecretOfTheObscureRaidEncounter(int triggerID) : base(triggerID)
     {
         LogCategoryInformation.SubCategory = SubLogCategory.SotO;
         LogID |= LogIDs.RaidEncounterMasks.SotOMask;
-    }
-
-    internal override void CheckSuccess(CombatData combatData, AgentData agentData, LogData logData, IReadOnlyCollection<AgentItem> playerAgents, LogData.LogSuccessHandler successHandler)
-    {
-        if (IsInstance)
-        {
-            successHandler.SetSuccess(true, GetFinalMapChangeTime(logData, combatData));
-            return;
-        }
-        IReadOnlyList<RewardEvent> rewards = combatData.GetRewardEvents();
-        RewardEvent? reward = rewards.FirstOrDefault(x => x.RewardType == RewardTypes.PostEoDRaidEncounterReward && x.Time > logData.LogStart);
-        if (reward != null)
-        {
-            successHandler.SetSuccess(true, reward.Time);
-        }
-        else
-        {
-            NoBouncyChestGenericCheckSucess(combatData, agentData, logData, playerAgents, successHandler);
-        }
     }
 }
