c826be6020b53bac7af50e691121c8d188b99308
EliphasNUIT <bichon.jekfer@gmail.com>
2026-02-17 15:08:37 +0100

    added weighted breakbar damage mechanic

---
+++ GW2EIEvtcParser/EIData/Mechanics/WeightedMechanics/CombatEventListMechanics/DamageMechanics/WeightedBreakbarDamageMechanic.cs
@@ -0,0 +1,11 @@
+﻿using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser.EIData;
+
+internal abstract class WeightedBreakbarDamageMechanic : WeightedCombatEventListMechanic<BreakbarDamageEvent>
+{
+
+    public WeightedBreakbarDamageMechanic(MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown, CombatEventsGetter getter) : base(plotlySetting, shortName, description, fullName, internalCoolDown, getter)
+    {
+    }
+}
---
+++ GW2EIEvtcParser/EIData/Mechanics/WeightedMechanics/CombatEventListMechanics/WeightedCombatEventListMechanic.cs
@@ -0,0 +1,25 @@
+﻿using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser.EIData;
+
+internal abstract class WeightedCombatEventListMechanic<T> : WeightedCheckedMechanic<T> where T : TimeCombatEvent
+{
+
+    public delegate IEnumerable<T> CombatEventsGetter(ParsedEvtcLog log, AgentItem agent);
+
+    private readonly CombatEventsGetter _getter;
+
+    public IEnumerable<T> GetEvents(ParsedEvtcLog log, AgentItem a)
+    {
+        return _getter(log, a);
+    }
+
+    public WeightedCombatEventListMechanic(MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown, CombatEventsGetter getter) : base(plotlySetting, shortName, description, fullName, internalCoolDown)
+    {
+        _getter = getter;
+        if (_getter == null)
+        {
+            throw new InvalidOperationException("Missing getter in CombatEventListMechanic");
+        }
+    }
+}
