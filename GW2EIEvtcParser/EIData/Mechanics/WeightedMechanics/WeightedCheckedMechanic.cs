33967abd1cdcf9ca0bda128c66d0ed7d2c688be2
EliphasNUIT <bichon.jekfer@gmail.com>
2026-02-17 14:54:51 +0100

    added WeightedMechanic

---
+++ GW2EIEvtcParser/EIData/Mechanics/WeightedMechanics/WeightedCheckedMechanic.cs
@@ -0,0 +1,18 @@
+﻿using GW2EIEvtcParser.ParsedData;
+
+namespace GW2EIEvtcParser.EIData;
+
+public abstract class WeightedCheckedMechanic<Checkable> : CheckedMechanic<Checkable>
+{
+
+    protected WeightedCheckedMechanic(MechanicPlotlySetting plotlySetting, string shortName, string description, string fullName, int internalCoolDown) : base(plotlySetting, shortName, description, fullName, internalCoolDown)
+    {
+    }
+
+    protected void InsertMechanic(ParsedEvtcLog log, Dictionary<Mechanic, List<MechanicEvent>> mechanicLogs, long time, SingleActor actor, double weight)
+    {
+        (long timeToUse, actor) = ComputeTimeAndActor(log, actor, time);
+        InsertMechanicWithSubMechanics(log, mechanicLogs, time, actor, new CounterMechanicEvent(timeToUse, this, actor));
+    }
+
+}
