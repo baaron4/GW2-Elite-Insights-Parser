ed60f01412947c94d13e8ac55af1dbcfbbf8f6a2
EliphasNUIT <bichon.jekfer@gmail.com>
2026-07-15 18:57:36 +0200

    updated build value

--- GW2EIEvtcParser/ParserHelpers/GW2Builds.cs
+++ GW2EIEvtcParser/ParserHelpers/GW2Builds.cs
@@ -261,9 +261,9 @@ public static class GW2Builds
 
     // https://wiki.guildwars2.com/wiki/Game_updates/2026-06-02
     public const long June2026HotFix = 201671;
-    // https://wiki.guildwars2.com/wiki/Game_updates/2026-06-02
 
-    public const long July2026Balance = 999999;
+    // https://wiki.guildwars2.com/wiki/Game_updates/2026-06-02
+    public const long July2026Balance = 203989;
 
     public const ulong EndOfLife = ulong.MaxValue;
 }
