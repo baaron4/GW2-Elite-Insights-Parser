f14d16ed7ee79bf0382df2dd24071da7ba9748d3
EliphasNUIT <bichon.jekfer@gmail.com>
2026-07-23 15:32:31 +0200

    general cleanup

--- GW2EIBuilders/Resources/JS/mixins.js
+++ GW2EIBuilders/Resources/JS/mixins.js
@@ -40,7 +40,7 @@ var numberComponent = {
             if (isNaN(value) || !isFinite(value)) {
                 return 0;
             }
-            return value.toLocaleString(undefined, {
+            return value.toLocaleString('en', {
                 minimumFractionDigits,
                 maximumFractionDigits
             });
@@ -63,7 +63,7 @@ var numberComponent = {
             if (isNaN(value) || !isFinite(value)) {
                 return '0%';
             }
-            return value.toLocaleString(undefined, {
+            return value.toLocaleString('en', {
                 style: "percent",
                 minimumFractionDigits,
                 maximumFractionDigits
