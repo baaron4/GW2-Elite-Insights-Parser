7c0a7b8ee6f39ffd656b0acb4788c9c5c906459b
EliphasNUIT <bichon.jekfer@gmail.com>
2026-09-06 17:31:39 +0200

    ApiSkillInfo -> ApiSkill

--- GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/ParsedData/Skills/SkillItem.cs
+++ GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/ParsedData/Skills/SkillItem.cs
@@ -48,8 +48,7 @@ public class SkillItem
     public string Icon { get; private set; } = "";
     private readonly WeaponDescriptor? _weaponDescriptor;
     public bool IsWeaponSkill => _weaponDescriptor != null;
-    internal readonly GW2APISkill? ApiSkill;
-    public GW2APISkill? ApiSkillInfo => ApiSkill;
+    public readonly GW2APISkill? ApiSkill;
     public bool CanHeal => ApiSkill != null && ApiSkill.Facts != null && ApiSkill.Facts.Any(x => x.Target == "Healing");
     private SkillInfoEvent? _skillInfo;
 
