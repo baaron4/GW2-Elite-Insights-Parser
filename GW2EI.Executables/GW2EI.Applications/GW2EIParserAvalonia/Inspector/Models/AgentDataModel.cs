7bc39f19b05a347231febb14dcf4d9dc64846470
EliphasNUIT <bichon.jekfer@gmail.com>
2026-09-09 13:36:31 +0200

    renaming

--- GW2EI.Executables/GW2EI.Applications/GW2EIParserAvalonia/Inspector/Models/AgentDataModel.cs
+++ GW2EI.Executables/GW2EI.Applications/GW2EIParserAvalonia/Inspector/Models/AgentDataModel.cs
@@ -7,42 +7,42 @@ namespace GW2EIParserAvalonia.Models;
 
 public sealed class AgentDataModel
 {
-    public ulong Agent => AgentItem.Agent;
-    public ushort InstID => AgentItem.InstID;
-    public string Name => AgentItem.Name;
-    public int ID => AgentItem.ID;
+    public ulong Agent => _agentItem.Agent;
+    public ushort InstID => _agentItem.InstID;
+    public string Name => _agentItem.Name;
+    public int ID => _agentItem.ID;
     // This is EI Logic
     //public int UniqueID => AgentItem.UniqueID;
-    public AgentItem.AgentType Type => AgentItem.Type;
-    public ParserHelper.Spec Spec => AgentItem.Spec;
-    public ParserHelper.Spec BaseSpec => AgentItem.BaseSpec;
+    public AgentItem.AgentType Type => _agentItem.Type;
+    public ParserHelper.Spec Spec => _agentItem.Spec;
+    public ParserHelper.Spec BaseSpec => _agentItem.BaseSpec;
 
     public readonly AgentDataModel? Master;
-    public long FirstAware => AgentItem.FirstAware;
-    public long LastAware => AgentItem.LastAware;
-    public ushort Toughness => AgentItem.Toughness;
-    public ushort Healing => AgentItem.Healing;
-    public ushort Condition => AgentItem.Condition;
-    public ushort Concentration => AgentItem.Concentration;
+    public long FirstAware => _agentItem.FirstAware;
+    public long LastAware => _agentItem.LastAware;
+    public ushort Toughness => _agentItem.Toughness;
+    public ushort Healing => _agentItem.Healing;
+    public ushort Condition => _agentItem.Condition;
+    public ushort Concentration => _agentItem.Concentration;
     // Height is defunct, it also never worked according to deltaconnected
-    public uint HitboxWidth => AgentItem.HitboxWidth;
-    public bool IsPlayer => AgentItem.IsPlayer;
-    public bool IsNPC => AgentItem.IsNPC;
-    public bool IsFake => AgentItem.IsFake;
-    public bool IsUnknown => AgentItem.IsUnknown;
-    public bool IsEnglobedAgent => AgentItem.IsEnglobedAgent;
-    public bool IsEnglobingAgent => AgentItem.IsEnglobingAgent;
-    public int EnglobedAgentCount => AgentItem.EnglobedAgentItems.Count;
-    public bool IsNotInSquadFriendlyPlayer => AgentItem.IsNotInSquadFriendlyPlayer;
-    public int MergeCount => AgentItem.Merges.Count;
+    public uint HitboxWidth => _agentItem.HitboxWidth;
+    public bool IsPlayer => _agentItem.IsPlayer;
+    public bool IsNPC => _agentItem.IsNPC;
+    public bool IsFake => _agentItem.IsFake;
+    public bool IsUnknown => _agentItem.IsUnknown;
+    public bool IsEnglobedAgent => _agentItem.IsEnglobedAgent;
+    public bool IsEnglobingAgent => _agentItem.IsEnglobingAgent;
+    public int EnglobedAgentCount => _agentItem.EnglobedAgentItems.Count;
+    public bool IsNotInSquadFriendlyPlayer => _agentItem.IsNotInSquadFriendlyPlayer;
+    public int MergeCount => _agentItem.Merges.Count;
     public readonly List<AgentDataModel>? Merges;
-    public int RegroupedCount => AgentItem.Regrouped.Count;
+    public int RegroupedCount => _agentItem.Regrouped.Count;
     public readonly List<AgentDataModel>? Regrouped;
-    private readonly AgentItem AgentItem;
+    private readonly AgentItem _agentItem;
 
     public AgentDataModel(AgentItem agent)
     {
-        AgentItem = agent;
+        _agentItem = agent;
         if (agent.Master != null)
         {
             Master = new AgentDataModel(agent.Master);
