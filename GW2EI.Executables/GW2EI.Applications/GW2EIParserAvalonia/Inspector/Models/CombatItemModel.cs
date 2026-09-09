43b839512d2c9b416ac2031f866e0686bc8b5ddb
EliphasNUIT <bichon.jekfer@gmail.com>
2026-09-09 13:38:26 +0200

    reworked combat item model so that certain enums are only show if relevant

--- GW2EI.Executables/GW2EI.Applications/GW2EIParserAvalonia/Inspector/Models/CombatItemModel.cs
+++ GW2EI.Executables/GW2EI.Applications/GW2EIParserAvalonia/Inspector/Models/CombatItemModel.cs
@@ -5,79 +5,42 @@ namespace GW2EIParserAvalonia.Models;
 
 public sealed class CombatItemModel
 {
-    public long Time { get; }
-    public ulong SrcAgent { get; }
-    public ulong DstAgent { get; }
-    public int Value { get; }
-    public int BuffDmg { get; }
-    public uint OverstackValue { get; }
-    public uint SkillID { get; }
-    public ushort SrcInstid { get; }
-    public ushort DstInstid { get; }
-    public ushort SrcMasterInstid { get; }
-    public ushort DstMasterInstid { get; }
-    public byte IFFByte { get; }
-    public IFF IFF { get; }
-    public byte IsBuff { get; }
-    public byte Result { get; }
-    public byte IsActivationByte { get; }
-    public Activation IsActivation { get; }
-    public byte IsBuffRemoveByte { get; }
-    public BuffRemove IsBuffRemove { get; }
-    public byte IsNinety { get; }
-    public byte IsFifty { get; }
-    public byte IsMoving { get; }
-    public StateChange IsStateChange { get; }
-    public byte IsFlanking { get; }
-    public byte IsShields { get; }
-    public byte IsOffcycle { get; }
-    public uint Pad { get; }
-    public byte Pad1 { get; }
-    public byte Pad2 { get; }
-    public byte Pad3 { get; }
-    public byte Pad4 { get; }
+    public long Time => _combatItem.Time;
+    public ulong SrcAgent => _combatItem.SrcAgent;
+    public ulong DstAgent => _combatItem.DstAgent;
+    public int Value => _combatItem.Value;
+    public int BuffDmg => _combatItem.BuffDmg;
+    public uint OverstackValue => _combatItem.OverstackValue;
+    public uint SkillID => _combatItem.SkillID;
+    public ushort SrcInstid => _combatItem.SrcInstid;
+    public ushort DstInstid => _combatItem.DstInstid;
+    public ushort SrcMasterInstid => _combatItem.SrcMasterInstid;
+    public ushort DstMasterInstid => _combatItem.DstMasterInstid;
+    public byte IFF => _combatItem.IFFByte;
+    public IFF? IFFEnum => _combatItem.IsStateChange == StateChange.Combat ? _combatItem.IFF : null;
+    public byte IsBuff => _combatItem.IsBuff;
+    public byte Result => _combatItem.Result;
+    public byte IsActivation => _combatItem.IsActivationByte;
+    public Activation? IsActivationEnum => _combatItem.IsCastEvent() ? _combatItem.IsActivation : null;
+    public byte IsBuffRemove => _combatItem.IsBuffRemoveByte;
+    public BuffRemove? IsBuffRemoveEnum => _combatItem.IsBuffApplyOrRemoveEvent() ? _combatItem.IsBuffRemove : null;
+    public byte IsNinety => _combatItem.IsNinety;
+    public byte IsFifty => _combatItem.IsFifty;
+    public byte IsMoving => _combatItem.IsMoving;
+    public StateChange IsStateChange => _combatItem.IsStateChange;
+    public byte IsFlanking => _combatItem.IsFlanking;
+    public byte IsShields => _combatItem.IsShields;
+    public byte IsOffcycle => _combatItem.IsOffcycle;
+    public uint Pad => _combatItem.Pad;
+    public byte Pad1 => _combatItem.Pad1;
+    public byte Pad2 => _combatItem.Pad2;
+    public byte Pad3 => _combatItem.Pad3;
+    public byte Pad4 => _combatItem.Pad4;
+
+    private readonly CombatItem _combatItem;
 
     public CombatItemModel(CombatItem item)
     {
-        Time = item.Time;
-        SrcAgent = item.SrcAgent;
-        DstAgent = item.DstAgent;
-        Value = item.Value;
-        BuffDmg = item.BuffDmg;
-        OverstackValue = item.OverstackValue;
-        SkillID = item.SkillID;
-
-        SrcInstid = item.SrcInstid;
-        DstInstid = item.DstInstid;
-        SrcMasterInstid = item.SrcMasterInstid;
-        DstMasterInstid = item.DstMasterInstid;
-
-        IFFByte = item.IFFByte;
-        IFF = item.IFF;
-
-        IsBuff = item.IsBuff;
-        Result = item.Result;
-
-        IsActivationByte = item.IsActivationByte;
-        IsActivation = item.IsActivation;
-
-        IsBuffRemoveByte = item.IsBuffRemoveByte;
-        IsBuffRemove = item.IsBuffRemove;
-
-        IsNinety = item.IsNinety;
-        IsFifty = item.IsFifty;
-        IsMoving = item.IsMoving;
-
-        IsStateChange = item.IsStateChange;
-
-        IsFlanking = item.IsFlanking;
-        IsShields = item.IsShields;
-        IsOffcycle = item.IsOffcycle;
-
-        Pad = item.Pad;
-        Pad1 = item.Pad1;
-        Pad2 = item.Pad2;
-        Pad3 = item.Pad3;
-        Pad4 = item.Pad4;
+        _combatItem = item;
     }
 }
