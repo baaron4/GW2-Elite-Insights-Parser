d7670a0c1e1fc9e573ba9bc79887e7b444fefeaf
EliphasNUIT <bichon.jekfer@gmail.com>
2026-09-15 20:08:43 +0200

    adjust GUIDWrapper for endianness

--- GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/ParserHelpers/GuidExt.cs
+++ GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/ParserHelpers/GuidExt.cs
@@ -1,7 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System.Buffers.Binary;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
 namespace GW2EIEvtcParser;
 
 [StructLayout(LayoutKind.Explicit, Size = 16)]
@@ -9,16 +8,22 @@ namespace GW2EIEvtcParser;
 public readonly struct GUIDWrapper
 #pragma warning restore CA1815 // Override equals and operator equals on value types
 {
-    [FieldOffset(0)] public readonly ulong First8;
-    [FieldOffset(8)] public readonly ulong Last8;
+    [FieldOffset(0)] public readonly uint ZeroToFour;
+    [FieldOffset(4)] public readonly ushort FourToSix;
+    [FieldOffset(6)] public readonly ushort SixToEight;
+    [FieldOffset(8)] public readonly ulong EightToSixteen;
 
     [FieldOffset(0)] public readonly Guid GUID;
 
     public GUIDWrapper(ulong first8, ulong last8)
     {
         GUID = default;
-        First8 = first8;
-        Last8 = last8;
+        first8 = BinaryPrimitives.ReverseEndianness(first8);
+        var last4OfFirst8 = (uint)(first8 & 0xFFFFFFFF);
+        ZeroToFour = (uint)(first8 >> 32);
+        FourToSix = (ushort)(last4OfFirst8 >> 16);
+        SixToEight = (ushort)(last4OfFirst8 & 0xFFFF);
+        EightToSixteen = last8;
     }
 }
 public static class GuidExt
