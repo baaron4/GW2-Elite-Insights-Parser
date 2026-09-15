2ce4dbc279323b9d80ceb9c393da7b7b73b87c6d
EliphasNUIT <bichon.jekfer@gmail.com>
2026-09-15 17:12:47 +0200

    added some filtering + DataGridCollectionView useage

--- GW2EI.Executables/GW2EI.Applications/GW2EIParserAvalonia/Inspector/ViewModels/InspectorViewModel.cs
+++ GW2EI.Executables/GW2EI.Applications/GW2EIParserAvalonia/Inspector/ViewModels/InspectorViewModel.cs
@@ -7,6 +7,7 @@ using GW2EIEvtcParser.ParsedData;
 using GW2EIParserAvalonia.Services;
 using GW2EIParserAvalonia.Models;
 using GW2EIEvtcParser.Extensions;
+using Avalonia.Collections;
 
 namespace GW2EIParserAvalonia.ViewModels;
 
@@ -15,21 +16,133 @@ public partial class InspectorViewModel : ObservableObject
     [ObservableProperty]
     private EventModel? selectedEvent;
     [ObservableProperty]
-    private AgentDataModel? selectedAgent;
-    [ObservableProperty]
     private SkillDataModel? selectedSkill;
-
-    // Events tab filters
-    [ObservableProperty]
-    private string? agentSearchText;
-    [ObservableProperty]
-    private AgentFilterItem? selectedAgentFilter;
     [ObservableProperty]
     private string? skillIdFilter;
     [ObservableProperty]
     private string? skillNameFilter;
     [ObservableProperty]
     private string? guidFilter;
+    #region AGENT EVENTS
+    // Events tab filters
+    [ObservableProperty]
+    private string? agentSearchText;
+    public IReadOnlyList<AgentFilterItem> AgentFilterItems { get; }
+    [ObservableProperty]
+    private AgentFilterItem? selectedAgentFilter;
+    #endregion AGENT EVENTS
+
+    private readonly IReadOnlyList<TimeCombatEvent> _allTimeEvents;
+    private readonly IReadOnlyList<NonTimeCombatEvent> _allNonTimeEvents;
+    private readonly IReadOnlyList<EXTHealingExtensionEvent> _allHealingExtensionEvents;
+    public IReadOnlyList<SkillDataModel> SkillsData { get; } = [];
+
+    public IReadOnlyList<EventModel> Events { get; }
+    public BulkObservableCollection<EventModel> VisibleEvents { get; } = [];
+    public IReadOnlyList<EventTypeFilterNodeModel> EventTypeFilterRoots { get; }
+
+    public BulkObservableCollection<EventPropertyModel> SelectedEventProperties { get; } = [];
+    public BulkObservableCollection<EventPropertyModel> SelectedAgentProperties { get; } = [];
+    public BulkObservableCollection<EventPropertyModel> SelectedSkillProperties { get; } = [];
+
+    #region AGENTS
+
+    [ObservableProperty]
+    private AgentDataModel? selectedAgent;
+    public DataGridCollectionView AgentsDataView { get; }
+    [ObservableProperty]
+    private string? agentSpeciesFilter;
+    partial void OnAgentSpeciesFilterChanged(string? value)
+    {
+        AgentsDataView.Refresh();
+        OnPropertyChanged(nameof(AgentCount));
+    }
+    [ObservableProperty]
+    private string? agentNameFilter;
+    partial void OnAgentNameFilterChanged(string? value)
+    {
+        AgentsDataView.Refresh();
+        OnPropertyChanged(nameof(AgentCount));
+    }
+    [ObservableProperty]
+    private string? agentTypeFilter;
+    partial void OnAgentTypeFilterChanged(string? value)
+    {
+        AgentsDataView.Refresh();
+        OnPropertyChanged(nameof(AgentCount));
+    }
+    [ObservableProperty]
+    private string? agentSpecFilter;
+    partial void OnAgentSpecFilterChanged(string? value)
+    {
+        AgentsDataView.Refresh();
+        OnPropertyChanged(nameof(AgentCount));
+    }
+    [ObservableProperty]
+    private string? agentBaseSpecFilter;
+    partial void OnAgentBaseSpecFilterChanged(string? value)
+    {
+        AgentsDataView.Refresh();
+        OnPropertyChanged(nameof(AgentCount));
+    }
+    private bool FilterAgentModels(object item)
+    {
+        if (item is not AgentDataModel agent)
+        {
+            return false;
+        }
+
+
+        if (!string.IsNullOrWhiteSpace(AgentSpeciesFilter) && 
+            int.TryParse(AgentSpeciesFilter, out var id) && 
+            agent.ID != id)
+        {
+            return false;
+        }
+
+        if (!string.IsNullOrWhiteSpace(AgentNameFilter) && 
+            !agent.Name.Contains(AgentNameFilter, StringComparison.OrdinalIgnoreCase))
+        {
+            return false;
+        }
+
+        if (!string.IsNullOrWhiteSpace(AgentTypeFilter) &&
+            Enum.TryParse<AgentItem.AgentType>(AgentTypeFilter, true, out var type) &&
+            agent.Type != type)
+        {
+            return false;
+        }
+
+        if (!string.IsNullOrWhiteSpace(AgentSpecFilter) &&
+            Enum.TryParse<ParserHelper.Spec>(AgentTypeFilter, true, out var spec) &&
+            agent.Spec != spec)
+        {
+            return false;
+        }
+
+        if (!string.IsNullOrWhiteSpace(AgentBaseSpecFilter) &&
+            Enum.TryParse<ParserHelper.Spec>(AgentBaseSpecFilter, true, out var baseSpec) &&
+            agent.BaseSpec != baseSpec)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public int AgentCount => AgentsDataView.Count;
+
+    #endregion AGENTS
+
+    #region GUIDS
+
+    public DataGridCollectionView SkillGUIDsView { get; }
+    public DataGridCollectionView EffectGUIDsView { get; }
+    public DataGridCollectionView MarkerGUIDsView { get; }
+    public DataGridCollectionView SpeciesGUIDsView { get; }
+    public DataGridCollectionView TeamGUIDsView { get; }
+    public DataGridCollectionView EmoteGUIDsView { get; }
+    public DataGridCollectionView TransformationGUIDsView { get; }
 
     // Content GUID tab filters
     [ObservableProperty]
@@ -37,66 +150,145 @@ public partial class InspectorViewModel : ObservableObject
     [ObservableProperty]
     private string? contentGuidFilter;
 
-    private readonly IReadOnlyList<TimeCombatEvent> _allTimeEvents;
-    private readonly IReadOnlyList<NonTimeCombatEvent> _allNonTimeEvents;
-    private readonly IReadOnlyList<EXTHealingExtensionEvent> _allHealingExtensionEvents;
+    partial void OnContentIdFilterChanged(string? value) 
+    {
+        SkillGUIDsView.Refresh();
+        EffectGUIDsView.Refresh();
+        MarkerGUIDsView.Refresh();
+        SpeciesGUIDsView.Refresh();
+        TeamGUIDsView.Refresh();
+        EmoteGUIDsView.Refresh();
+        TransformationGUIDsView.Refresh();
+    }
 
-    public IReadOnlyList<CombatItemModel> CombatItems { get; } = [];
-    public IReadOnlyList<AgentDataModel> AgentsData { get; } = [];
-    public IReadOnlyList<SkillDataModel> SkillsData { get; } = [];
+    partial void OnContentGuidFilterChanged(string? value)
+    {
+        SkillGUIDsView.Refresh();
+        EffectGUIDsView.Refresh();
+        MarkerGUIDsView.Refresh();
+        SpeciesGUIDsView.Refresh();
+        TeamGUIDsView.Refresh();
+        EmoteGUIDsView.Refresh();
+        TransformationGUIDsView.Refresh();
+    }
 
-    public IReadOnlyList<EventModel> Events { get; }
-    public BulkObservableCollection<EventModel> VisibleEvents { get; } = [];
-    public IReadOnlyList<EventTypeFilterNodeModel> EventTypeFilterRoots { get; }
+    private bool FilterContentGUIDs(object item)
+    {
+        if (item is not ContentGUIDModel evt)
+        {
+            return false;
+        }
 
-    public IReadOnlyList<AgentFilterItem> AgentFilterItems { get; }
+        if (!string.IsNullOrWhiteSpace(ContentIdFilter) && !evt.ContentID.ToString().Contains(ContentIdFilter, StringComparison.OrdinalIgnoreCase))
+        {
+            return false;
+        }
 
-    public BulkObservableCollection<EventPropertyModel> SelectedEventProperties { get; } = [];
-    public BulkObservableCollection<EventPropertyModel> SelectedAgentProperties { get; } = [];
-    public BulkObservableCollection<EventPropertyModel> SelectedSkillProperties { get; } = [];
+        if (!string.IsNullOrWhiteSpace(ContentGuidFilter) && !evt.GUID.ToString().Contains(ContentGuidFilter, StringComparison.OrdinalIgnoreCase))
+        {
+            return false;
+        }
+        return true;
+    }
 
-    public IReadOnlyList<ContentGUIDModel> Skills { get; }
-    public IReadOnlyList<ContentGUIDModel> Effects { get; }
-    public IReadOnlyList<ContentGUIDModel> Markers { get; }
-    public IReadOnlyList<ContentGUIDModel> Species { get; }
-    public IReadOnlyList<ContentGUIDModel> Teams { get; }
-    public IReadOnlyList<ContentGUIDModel> Emotes { get; }
-    public IReadOnlyList<ContentGUIDModel> Transformations { get; }
-
-    public BulkObservableCollection<ContentGUIDModel> VisibleSkills { get; } = [];
-    public BulkObservableCollection<ContentGUIDModel> VisibleEffects { get; } = [];
-    public BulkObservableCollection<ContentGUIDModel> VisibleMarkers { get; } = [];
-    public BulkObservableCollection<ContentGUIDModel> VisibleSpecies { get; } = [];
-    public BulkObservableCollection<ContentGUIDModel> VisibleTeams { get; } = [];
-    public BulkObservableCollection<ContentGUIDModel> VisibleEmotes { get; } = [];
-    public BulkObservableCollection<ContentGUIDModel> VisibleTransformations { get; } = [];
-
-    public int CombatItemCount => CombatItems.Count;
-    public int AgentCount => AgentsData.Count;
+    #endregion GUIDS
     public int SkillCount => SkillsData.Count;
+
+    #region COMBAT ITEMS
+
+    public DataGridCollectionView CombatItemsView { get; }
+    public int CombatItemCount => CombatItemsView.Count;
+
+    [ObservableProperty]
+    private string? stateChangeFilter;
+    partial void OnStateChangeFilterChanged(string? value) { 
+        CombatItemsView.Refresh();
+    }
+
+    private bool FilterCombatItems(object item)
+    {
+        if (item is not CombatItemModel combatItem)
+        {
+            return false;
+        }
+        if (Enum.TryParse<ArcDPSEnums.StateChange>(StateChangeFilter, true, out var stateChange))
+        {
+            if (stateChange != combatItem.IsStateChange)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    #endregion COMBAT ITEMS
 
     public InspectorViewModel(RawEvtcLog log)
     {
-        CombatItems = log.CombatItems.Select(item => new CombatItemModel(item)).ToList();
-        AgentsData = log.AgentData.AllAgents.Select(agent => new AgentDataModel(agent)).OrderBy(agent => agent.ID).ToList();
+        var combatItems = log.CombatItems.Select(item => new CombatItemModel(item));
+        CombatItemsView = new(combatItems)
+        {
+            Filter = FilterCombatItems
+        };
+        #region AGENTS
+        var agentsData = log.AgentData.AllAgents.Select(agent => new AgentDataModel(agent)).OrderBy(agent => agent.ID).ToList();
+        AgentsDataView = new(agentsData)
+        {
+            Filter = FilterAgentModels
+        };
+        AgentFilterItems = agentsData.Select(agent => new AgentFilterItem(agent)).ToList();
+        #endregion AGENTS
         SkillsData = log.SkillData.AllSkills.Select(skill => new SkillDataModel(skill, log.SkillData)).OrderBy(skill => skill.ID).ToList();
-
-        AgentFilterItems = AgentsData.Select(agent => new AgentFilterItem(agent)).ToList();
 
         _allTimeEvents = log.CombatData.GetAllTimeCombatEvents();
         _allNonTimeEvents = log.CombatData.GetAllNonTimeCombatEvents();
         _allHealingExtensionEvents = log.CombatData.GetAllHealingExtensionCombatEvents();
-
+        #region GUIDS
         var contentGUIDEvents = _allNonTimeEvents.OfType<IDToGUIDEvent>().Where(x => x.IsValid).ToList();
-        Skills = contentGUIDEvents.OfType<SkillGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(skill => skill.ContentID).ToList();
-        Effects = contentGUIDEvents.OfType<EffectGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(effect => effect.ContentID).ToList();
-        Markers = contentGUIDEvents.OfType<MarkerGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(marker => marker.ContentID).ToList();
-        Species = contentGUIDEvents.OfType<SpeciesGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(species => species.ContentID).ToList();
-        Teams = contentGUIDEvents.OfType<TeamGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(team => team.ContentID).ToList();
-        Emotes = contentGUIDEvents.OfType<EmoteGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(emote => emote.ContentID).ToList();
-        Transformations = contentGUIDEvents.OfType<TransformationGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(transformation => transformation.ContentID).ToList();
 
-        Events = _allTimeEvents.OrderBy(x => x.Time).Cast<CombatEvent>().Concat(_allNonTimeEvents).Concat(_allHealingExtensionEvents).Select(x => new EventModel(x)).ToList();
+        var skills = contentGUIDEvents.OfType<SkillGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(skill => skill.ContentID);
+        SkillGUIDsView = new(skills)
+        {
+            Filter = FilterContentGUIDs
+        };
+
+        var effects = contentGUIDEvents.OfType<EffectGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(effect => effect.ContentID);
+        EffectGUIDsView = new(effects)
+        {
+            Filter = FilterContentGUIDs
+        };
+
+        var markers = contentGUIDEvents.OfType<MarkerGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(marker => marker.ContentID);
+        MarkerGUIDsView = new(markers)
+        {
+            Filter = FilterContentGUIDs
+        };
+
+        var species = contentGUIDEvents.OfType<SpeciesGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(species => species.ContentID);
+        SpeciesGUIDsView = new(species)
+        {
+            Filter = FilterContentGUIDs
+        };
+
+        var teams = contentGUIDEvents.OfType<TeamGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(team => team.ContentID);
+        TeamGUIDsView = new(teams)
+        {
+            Filter = FilterContentGUIDs
+        };
+
+        var emotes = contentGUIDEvents.OfType<EmoteGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(emote => emote.ContentID);
+        EmoteGUIDsView = new(emotes)
+        {
+            Filter = FilterContentGUIDs
+        };
+
+        var transformations = contentGUIDEvents.OfType<TransformationGUIDEvent>().Select(x => new ContentGUIDModel(x)).OrderBy(transformation => transformation.ContentID).ToList();
+        TransformationGUIDsView = new(transformations)
+        {
+            Filter = FilterContentGUIDs
+        };
+        #endregion GUIDS
+        Events = _allTimeEvents.OrderBy(x => x.Time).Cast<CombatEvent>().Concat(_allNonTimeEvents).Concat(_allHealingExtensionEvents.OrderBy(x => x.Time)).Select(x => new EventModel(x)).ToList();
         EventTypeFilterRoots = EventTypeFilterNodeModel.BuildRoots(_allTimeEvents, _allNonTimeEvents, _allHealingExtensionEvents);
 
         foreach (var root in EventTypeFilterRoots)
@@ -105,7 +297,6 @@ public partial class InspectorViewModel : ObservableObject
         }
 
         RefreshVisibleEvents();
-        RefreshVisibleContentGUIDs();
     }
 
     internal void SetCheckStateOnAllRoots(bool state)
@@ -125,8 +316,6 @@ public partial class InspectorViewModel : ObservableObject
     partial void OnGuidFilterChanged(string? value) => RefreshVisibleEvents();
 
     partial void OnSelectedAgentFilterChanged(AgentFilterItem? value) => RefreshVisibleEvents();
-
-    partial void OnAgentSearchTextChanged(string? value) => OnPropertyChanged(nameof(FilteredAgentFilterItems));
 
     private void RefreshVisibleEvents()
     {
@@ -211,48 +400,4 @@ public partial class InspectorViewModel : ObservableObject
     {
         return EventTypeFilterRoots.Any(root => root.IsEventVisible(eventType));
     }
-
-    partial void OnContentIdFilterChanged(string? value) => RefreshVisibleContentGUIDs();
-
-    partial void OnContentGuidFilterChanged(string? value) => RefreshVisibleContentGUIDs();
-
-    private void RefreshVisibleContentGUIDs()
-    {
-        bool hasContentIdFilter = !string.IsNullOrWhiteSpace(ContentIdFilter);
-        bool hasGuidFilter = !string.IsNullOrWhiteSpace(ContentGuidFilter);
-
-        VisibleSkills.ReplaceRange(FilterContentGUIDs(Skills, hasContentIdFilter, hasGuidFilter));
-        VisibleEffects.ReplaceRange(FilterContentGUIDs(Effects, hasContentIdFilter, hasGuidFilter));
-        VisibleMarkers.ReplaceRange(FilterContentGUIDs(Markers, hasContentIdFilter, hasGuidFilter));
-        VisibleSpecies.ReplaceRange(FilterContentGUIDs(Species, hasContentIdFilter, hasGuidFilter));
-        VisibleTeams.ReplaceRange(FilterContentGUIDs(Teams, hasContentIdFilter, hasGuidFilter));
-        VisibleEmotes.ReplaceRange(FilterContentGUIDs(Emotes, hasContentIdFilter, hasGuidFilter));
-        VisibleTransformations.ReplaceRange(FilterContentGUIDs(Transformations, hasContentIdFilter, hasGuidFilter));
-    }
-
-    private IEnumerable<ContentGUIDModel> FilterContentGUIDs(IEnumerable<ContentGUIDModel> source, bool hasContentIdFilter, bool hasGuidFilter)
-    {
-        return source.Where(model =>
-        {
-            if (hasContentIdFilter && !model.ContentID.ToString().Contains(ContentIdFilter!, StringComparison.OrdinalIgnoreCase))
-            {
-                return false;
-            }
-
-            if (hasGuidFilter && !model.GUID.ToString().Contains(ContentGuidFilter!, StringComparison.OrdinalIgnoreCase))
-            {
-                return false;
-            }
-
-            return true;
-        });
-    }
-    
-    public IEnumerable<AgentFilterItem> FilteredAgentFilterItems =>
-    string.IsNullOrWhiteSpace(AgentSearchText)
-        ? AgentFilterItems
-        : AgentFilterItems.Where(agent =>
-            agent.DisplayName.Contains(
-                AgentSearchText,
-                StringComparison.OrdinalIgnoreCase));
 }
