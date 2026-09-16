32bde6c0ed97e0ce75ad0789b4b4bce83d37041d
EliphasNUIT <bichon.jekfer@gmail.com>
2026-09-16 17:19:22 +0200

    updated gadget model info event

--- GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/ParsedData/CombatEvents/StatusEvents/GadgetModelInfoEvent.cs
+++ GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/ParsedData/CombatEvents/StatusEvents/GadgetModelInfoEvent.cs
@@ -6,55 +6,19 @@ public class GadgetModelInfoEvent : StatusEvent
 {
     public readonly byte HidingBits;
 
-    public readonly string Model;
+    public readonly ulong Model;
+    public readonly ulong PropID;
 
-    internal GadgetModelInfoEvent(CombatItem evtcItem, AgentData agentData) : base(evtcItem, agentData)
+    internal unsafe GadgetModelInfoEvent(CombatItem evtcItem, AgentData agentData) : base(evtcItem, agentData)
     {
         HidingBits = evtcItem.IsFlanking;
-        try
-        {
-            var bytes = new ByteBuffer(stackalloc byte[40]);
-            // 8 bytes
-            bytes.PushNative(evtcItem.DstAgent);
-            // 4 bytes
-            bytes.PushNative(evtcItem.Value);
-            // 4 bytes
-            bytes.PushNative(evtcItem.BuffDmg);
-            // 4 bytes
-            bytes.PushNative(evtcItem.OverstackValue);
-            // 4 bytes
-            bytes.PushNative(evtcItem.SkillID);
-            // 2 bytes
-            bytes.PushNative(evtcItem.SrcInstid);
-            // 2 bytes
-            bytes.PushNative(evtcItem.DstInstid);
-            // 2 bytes
-            bytes.PushNative(evtcItem.SrcMasterInstid);
-            // 2 bytes
-            bytes.PushNative(evtcItem.DstMasterInstid);
-            // 1 byte
-            bytes.PushNative(evtcItem.IFFByte);
-            // 1 byte
-            bytes.PushNative(evtcItem.IsBuff);
-            // 1 byte
-            bytes.PushNative(evtcItem.Result);
-            // 1 byte
-            bytes.PushNative(evtcItem.IsActivationByte);
-            // 1 byte
-            bytes.PushNative(evtcItem.IsBuffRemoveByte);
-            // 1 byte
-            bytes.PushNative(evtcItem.IsNinety);
-            // 1 byte
-            bytes.PushNative(evtcItem.IsFifty);
-            // 1 byte
-            bytes.PushNative(evtcItem.IsMoving);
+        Model = evtcItem.DstAgent;
 
-            Model = System.Text.Encoding.UTF8.GetString(bytes).TrimEnd('\0');
-        } 
-        catch (Exception) 
-        {
-            Model = string.Empty;
-        }
+        var propID = stackalloc byte[8];
+
+        *(Int32*)(propID) = evtcItem.Value;
+        *(Int32*)(propID + sizeof(Int32)) = evtcItem.BuffDmg;
+        PropID = *(UInt64*)(propID);
     }
 
 }
