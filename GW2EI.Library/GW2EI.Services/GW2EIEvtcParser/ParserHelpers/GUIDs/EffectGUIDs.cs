d3a83846a17273170f3516e6b5f47025bf2dec4a
EliphasNUIT <bichon.jekfer@gmail.com>
2026-09-17 14:15:21 +0200

    changed GUID

--- GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/ParserHelpers/GUIDs/EffectGUIDs.cs
+++ GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/ParserHelpers/GUIDs/EffectGUIDs.cs
@@ -1147,6 +1147,6 @@ public static class EffectGUIDs
     public static readonly Guid NexusOfEternityVloxxSwordSwing = new("40EF37BFC16E2744865BBFDD8DC748DF"); // src vloxx, duration 1000
     public static readonly Guid NexusOfEternityVloxxLargeRangedSwordSwing = new("B486884192085E4BB45FF076F994B0B6"); // src vloxx, duration 833
     public static readonly Guid NexusOfEternityVloxxStaffBigConeIndicator = new("4B6532EC3792184DA4CAF258E1E9E45D"); // src vloxx, duration 8000, has dynamic end
-    public static readonly Guid NexusOfEternityVloxxHostilePortAndSuckAoE = new("E8EB2CDF97F34C42A8AAC0D3BA6551D0"); // dst vloxx, no src, duration 1666, incomplete? Could be just an indicator
+    public static readonly Guid NexusOfEternityVloxxHostilePortAndSuckAoE = new("BA8654BD3D252C4B9A170EE404FBEA15"); // src vloxx, duration 10000, has dynamic end, could be just an indicator
     #endregion Raid Encounters
 }
