6d3746993b779e2a9458743ca185b1a6b4b2d11a
EliphasNUIT <bichon.jekfer@gmail.com>
2026-09-17 15:08:26 +0200

    added aspects to trash mobs

--- GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/LogLogic/Raids/RaidEncounters/VisionsOfEternity/Bosses/NexusOfEternity.cs
+++ GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/LogLogic/Raids/RaidEncounters/VisionsOfEternity/Bosses/NexusOfEternity.cs
@@ -119,6 +119,8 @@ internal class NexusOfEternity : VisionsOfEternityRaidEncounter
         [
             TargetID.EliteCosmicPiercer,
             TargetID.EliteCosmicBulwark,
+            TargetID.AspectOfTheSpear,
+            TargetID.AspectOfTheStaff,
         ];
     }
 
