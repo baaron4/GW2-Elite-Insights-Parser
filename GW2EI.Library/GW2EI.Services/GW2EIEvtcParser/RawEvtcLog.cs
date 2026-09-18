19a5d156b541d993369a80ccab18ea7dba682f46
EliphasNUIT <bichon.jekfer@gmail.com>
2026-09-18 15:42:59 +0200

    adjust buff is also done for RawEvtcLog + info events public on SkillItem

--- GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/RawEvtcLog.cs
+++ GW2EI.Library/GW2EI.Services/GW2EIEvtcParser/RawEvtcLog.cs
@@ -18,5 +18,16 @@ public class RawEvtcLog : EvtcLog
 
         _operation.UpdateProgressWithCancellationCheck("Parsing: Creating GW2EI Log Meta Data");
         LogMetadata = new LogMetadata(evtcVersion, CombatData, logData.EvtcLogEnd - logData.EvtcLogStart, playerList, extensions, operation);
+
+        _operation.UpdateProgressWithCancellationCheck("Parsing: Adjusting Buffs");
+        var buffsByIDs = BuffsContainer.GetCurrentBuffs(CombatData).GroupBy(x => x.ID).ToDictionary(x => x.Key, x =>
+        {
+            if (x.Count() > 1 && x.Key != SkillIDs.NoBuff && x.Key != SkillIDs.Unknown)
+            {
+                throw new InvalidDataException("Same id present multiple times in buffs - " + x.First().ID);
+            }
+            return x.First();
+        });
+        BuffInfoSolver.AdjustBuffs(CombatData, buffsByIDs, operation);
     }
 }
